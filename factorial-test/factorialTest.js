a7af148190ea87e62e6cdb2ba93e18d7ad6cc689
Abdul Khuddus <khuddus1@gmail.com>
2019-06-12 05:17:17 +0530

    Complete factorial test

--- factorial-test/factorialTest.js
+++ factorial-test/factorialTest.js
@@ -1,6 +1,12 @@
 
-function factorialTest(...args) {
-  return args;
+function factorialTest(number) {
+  let fact = 1;
+  while (number !== 0) {
+    fact *= number;
+    // eslint-disable-next-line
+      --number;
+  }
+  return fact;
 }
 
 export {
