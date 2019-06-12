63f967c9d1143e42204188919e7fb257e1b6edc8
Abdul Khuddus <khuddus1@gmail.com>
2019-06-13 04:40:50 +0530

    Complete isPalindrome

--- is-palindrome/isPalindrome.js
+++ is-palindrome/isPalindrome.js
@@ -1,6 +1,15 @@
 
-function isPalindrome(...args) {
-  return args;
+function isPalindrome(arg) {
+  let str = String(arg);
+  str = str.replace(/[^a-zA-Z0-9]/g, '');
+  str = str.toLowerCase();
+  for (let i = 0; i < str.length / 2; i += 1) {
+    const lastCharIndex = str.length - 1 - i;
+    if (str.charAt(i) !== str.charAt(lastCharIndex)) {
+      return false;
+    }
+  }
+  return true;
 }
 
 export {
