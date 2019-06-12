e68d7bb09ecbae1662d0cbfe8393b6af5e49e8f1
Kanav Arora <kanav_arora@statusbrew.com>
2019-06-13 01:40:12 +0530

    [WIP] solve problem sumPrimes

--- sum-primes/sumPrimes.js
+++ sum-primes/sumPrimes.js
@@ -1,6 +1,19 @@
 
+function isPrime(num) {
+  for (let i = 2, s = Math.sqrt(num); i <= s; i += 1) {
+    if (num % i === 0) return false;
+  }
+  return num > 1;
+}
+
 function sumPrimes(...args) {
-  return args;
+  let sum = 0;
+  for (let i = 0; i <= args; i += 1) {
+    if (isPrime(i)) {
+      sum += i;
+    }
+  }
+  return sum;
 }
 
 export {
