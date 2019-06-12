f94a4d598eaba38b32538f1196b9ca98ab37eda0
Sebin Benjamin <sebinbenjamin@gmail.com>
2019-06-13 05:16:02 +0530

    completed exercise 'binary-agent'

--- binary-agent/binaryAgent.js
+++ binary-agent/binaryAgent.js
@@ -1,6 +1,14 @@
+const getIntFromBinary = binary => parseInt(binary, 2);
+const getASCIIFromIntegers = integers => String.fromCharCode(integers);
+const getEngStrFromBinary = binaryString => getASCIIFromIntegers(getIntFromBinary(binaryString));
+// eslint-disable-next-line arrow-body-style
+const getEngStrArray = (binaryStrArray) => {
+  return binaryStrArray.map(binaryString => getEngStrFromBinary(binaryString));
+};
 
-function binaryAgent(...args) {
-  return args;
+function binaryAgent(inputString) {
+  const binaryStrArray = inputString.split(' ');
+  return getEngStrArray(binaryStrArray).join('');
 }
 
 export {
