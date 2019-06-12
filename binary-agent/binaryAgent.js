753e941d654937ca58aa0f8b3844a2e47431b6a7
sourabhmodi20051 <sourabhmodi20051@gmail.com>
2019-06-13 04:55:16 +0530

    Solved binary Agent

--- binary-agent/binaryAgent.js
+++ binary-agent/binaryAgent.js
@@ -1,6 +1,13 @@
+function getCharacterFromCode(binaryCharacter) {
+  return String.fromCharCode(parseInt(binaryCharacter, 2));
+}
 
-function binaryAgent(...args) {
-  return args;
+function binaryAgent(binaryString) {
+  const textRepresentaionArray = binaryString
+    .split(' ')
+    .map(characterCode => getCharacterFromCode(characterCode));
+  const textRepresentaionString = textRepresentaionArray.join('');
+  return textRepresentaionString;
 }
 
 export {
