13a73e4ccf19c5496d6f4a0fe87eae02bbdbff99
Jaspreet Singh <erjaspreet57@gmail.com>
2019-06-13 00:53:04 +0530

    Solve factorial

--- factorial/factorial.js
+++ factorial/factorial.js
@@ -1,6 +1,9 @@
 
-function factorial(...args) {
-  return args;
+function factorial(number) {
+  if (number === 0 || number === 1) {
+    return 1;
+  }
+  return number * factorial(number - 1);
 }
 
 export {
