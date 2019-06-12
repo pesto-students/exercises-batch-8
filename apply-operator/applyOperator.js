878711ad691c3eda2fb37fb03ad7d4764f4a6eaa
aayushtiwari <arsenicaayush@gmail.com>
2019-06-13 00:24:40 +0530

    W.I.P apply operator

--- apply-operator/applyOperator.js
+++ apply-operator/applyOperator.js
@@ -1,5 +1,16 @@
 
 function applyOperator(...args) {
+        if(args.length===0){
+                throw "error";
+        }
+        if(args.length===1){
+                return 0;
+        }
+        if(args[1]==='+'){
+                const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+               return args.shift().reduce(reducer);
+        }
   return args;
 }
 
