e63197fc8b02600d894b0638df3280396c3f6631
Jaspreet Singh <erjaspreet57@gmail.com>
2019-06-12 14:41:56 +0530

    Solve factorial-test

--- factorial-test/factorialTest.js
+++ factorial-test/factorialTest.js
@@ -1,8 +1,14 @@
 
-function factorialTest(...args) {
-  return args;
+function factorial(number) {
+  let fact = 1;
+  while (number !== 0) {
+    fact *= number;
+    // eslint-disable-next-line
+    --number;
+  }
+  return fact;
 }
 
 export {
-  factorialTest,
+  factorial,
 };
