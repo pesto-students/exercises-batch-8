217e4a7668e60ec7d230df3066bec58f5ff9a301
Pradyumna Newalkar <contactpradyumna1@gmail.com>
2019-06-12 19:09:17 +0000

    Added sumPrimes function + fixed it's test

--- sum-primes/sumPrimes.js
+++ sum-primes/sumPrimes.js
@@ -1,6 +1,18 @@
 
-function sumPrimes(...args) {
-  return args;
+function sumPrimes(maxNum) {
+  const sieve = new Array(maxNum + 1);
+  sieve.fill(1);
+  sieve[0] = 0;
+  sieve[1] = 0;
+  for (let i = 2; i < Math.sqrt(maxNum); i += 1) {
+    if (sieve[i]) {
+      for (let j = i * 2; j <= maxNum; j += i) {
+        sieve[j] = 0;
+      }
+    }
+  }
+  const sum = sieve.reduce((acc, val, index) => acc + (val * index));
+  return sum;
 }
 
 export {
