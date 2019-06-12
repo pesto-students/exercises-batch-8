65af54756e6187d26edac7f2a185326c0ddcf926
saurabh shetty <saurabh.shetty100@gmail.com>
2019-06-13 01:04:45 +0530

    Solved factorial

--- factorial/factorial.js
+++ factorial/factorial.js
@@ -1,6 +1,10 @@
 
-function factorial(...args) {
-  return args;
+function factorial(arg) {
+  let fact = 1;
+  for (let i = 1; i <= arg; i += 1) {
+    fact *= i;
+  }
+  return fact;
 }
 
 export {
