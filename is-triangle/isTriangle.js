0107753ff5333cb6795a73c454dba66bce9da44b
saurabh shetty <saurabh.shetty100@gmail.com>
2019-06-13 01:33:06 +0530

    Solved isTriangle

--- is-triangle/isTriangle.js
+++ is-triangle/isTriangle.js
@@ -1,6 +1,10 @@
 
 function isTriangle(...args) {
-  return args;
+  const sum = args.reduce((a, b) => a + b);
+  for (let i = 0; i < args.length; i += 1) {
+    if (sum - args[i] <= args[i]) return false;
+  }
+  return true;
 }
 
 export {
