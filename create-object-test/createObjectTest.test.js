68ca25d4a559a101d715f3d1443c1add74cae86e
Rakesh Tembhurne <tj2point0@gmail.com>
2019-06-12 15:28:09 +0530

    Complete create-object-tet

--- create-object-test/createObjectTest.test.js
+++ create-object-test/createObjectTest.test.js
@@ -1,9 +1,7 @@
-function createObject(a, b) {
-  return { fname: a, lname: b };
-}
+import { createObjectTest as createObject } from './createObjectTest';
 
 describe('createObject', () => {
   test('should return an object with fname and lname as key with values as passed to function', () => {
-    expect(createObject('anshu', 'rathee')).toBe({ fname: 'anshu', lname: 'rathee' });
+    expect(createObject('anshu', 'rathee')).toMatchObject({ fname: 'anshu', lname: 'rathee' });
   });
 });
