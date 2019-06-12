7d21e7da8dffc5ff5776d6725fb083cbf69afca0
sourabhmodi20051 <sourabhmodi20051@gmail.com>
2019-06-13 00:38:13 +0530

    Solved Apply Operator problem

--- apply-operator/applyOperator.js
+++ apply-operator/applyOperator.js
@@ -1,6 +1,26 @@
-
 function applyOperator(...args) {
-  return args;
+  const [operator, ...operands] = args;
+  if (operator === undefined) {
+    throw new Error();
+  }
+  if (operands.length === 0) {
+    return 0;
+  }
+  switch (operator) {
+    case '+':
+      return operands.reduce((a, b) => a + b);
+    case '-':
+      operands[0] = -operands[0];
+      return operands.reduce((a, b) => a - b);
+    case '*':
+      return operands.reduce((a, b) => a * b);
+    case '/':
+      return Math.round(operands.reduce((a, b) => a / b) * 10000) / 10000;
+    case '%':
+      return operands.reduce((a, b) => a % b);
+    default:
+      throw new Error();
+  }
 }
 
 export {
