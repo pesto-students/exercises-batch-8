b343a1fe14e20b9e26122a8dfbcb43caab3f362a
Abdul Khuddus <khuddus1@gmail.com>
2019-06-13 00:54:31 +0530

    Complete applyOperator

--- apply-operator/applyOperator.js
+++ apply-operator/applyOperator.js
@@ -1,6 +1,25 @@
 
-function applyOperator(...args) {
-  return args;
+function applyOperator(operator, ...values) {
+  if (operator === undefined) { throw Error('No operator specified'); }
+  if (values.length === 0) { return 0; }
+  // eslint-disable-next-line no-param-reassign
+  if (operator === '-') values[0] = -values[0];
+  return values.reduce((acc, val) => {
+    switch (operator) {
+      case '+':
+        return acc + val;
+      case '-':
+        return acc - val;
+      case '*':
+        return acc * val;
+      case '/':
+        return Number((acc / val).toFixed(4));
+      case '%':
+        return acc % val;
+      default:
+        throw Error('Not a valid operation');
+    }
+  });
 }
 
 export {
