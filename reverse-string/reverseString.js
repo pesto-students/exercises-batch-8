ad8ff03f4cb4558171a1567e75f64146940adf1a
Rakesh Tembhurne <tj2point0@gmail.com>
2019-06-13 04:35:45 +0530

    Complete reverse-string

--- reverse-string/reverseString.js
+++ reverse-string/reverseString.js
@@ -1,6 +1,12 @@
 
-function reverseString(...args) {
-  return args;
+function reverseString(stringToReverse) {
+  let reversedString = '';
+
+  for (let index = stringToReverse.length - 1; index >= 0; index -= 1) {
+    reversedString += stringToReverse[index];
+  }
+
+  return reversedString;
 }
 
 export {
