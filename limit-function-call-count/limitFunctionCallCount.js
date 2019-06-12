c407f191cff5b9ad634bad414aa990a0f0e5952a
saurabh shetty <saurabh.shetty100@gmail.com>
2019-06-13 01:39:47 +0530

    Solved limitFunctionCallCount

--- limit-function-call-count/limitFunctionCallCount.js
+++ limit-function-call-count/limitFunctionCallCount.js
@@ -1,6 +1,13 @@
 
-function limitFunctionCallCount(...args) {
-  return args;
+function limitFunctionCallCount(fun, count) {
+  let remainingCount = count;
+  return (...args) => {
+    if (remainingCount) {
+      remainingCount -= 1;
+      return fun(...args);
+    }
+    return null;
+  };
 }
 
 export {
