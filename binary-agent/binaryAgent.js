8c7d21c9555a7b9bfd5a574403f46b302fffcb79
Jaspreet Singh <erjaspreet57@gmail.com>
2019-06-13 04:41:24 +0530

    Solve binary-agent

--- binary-agent/binaryAgent.js
+++ binary-agent/binaryAgent.js
@@ -1,6 +1,18 @@
+function binaryToEnglish(binaryCharacter) {
+  return String.fromCharCode(parseInt(binaryCharacter, 2));
+}
 
-function binaryAgent(...args) {
-  return args;
+function binaryAgent(binaryString) {
+  if (!binaryString) {
+    return '';
+  }
+  const listOfBinaryCharacters = binaryString.split(' ');
+
+  const listOfEnglishCharacters = listOfBinaryCharacters.map(
+    binaryCharacter => binaryToEnglish(binaryCharacter),
+  );
+
+  return listOfEnglishCharacters.join('');
 }
 
 export {
