2ad0981e4702692882942d195ce6cb6dd31f6bea
saurabh shetty <saurabh.shetty100@gmail.com>
2019-06-13 00:31:41 +0530

    Solved applyOperator

--- apply-operator/applyOperator.js
+++ apply-operator/applyOperator.js
@@ -1,8 +1,21 @@
 
-function applyOperator(...args) {
-  return args;
-}
+function applyOperator(operator, ...args) {
+  let x;
+  if (operator === '-') x = -args[0];
+  else [x] = args;
 
+  if (!operator) { throw Error('No Values'); }
+  if (args.length === 0) return 0;
+  for (let i = 1; i < args.length; i += 1) {
+    if (operator === '+') x += args[i];
+    else if (operator === '-') x -= args[i];
+    else if (operator === '*') x *= args[i];
+    else if (operator === '/') x /= args[i];
+    else if (operator === '%') x %= args[i];
+  }
+  if (operator === '/') x = Number(x.toFixed(4));
+  return x;
+}
 export {
   applyOperator,
 };
