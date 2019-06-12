9494f717b62a7d6ea38a740d373b7199b2a615b1
Abdul Khuddus <khuddus1@gmail.com>
2019-06-13 05:14:23 +0530

    Complete sumAll

--- sum-all/sumAll.js
+++ sum-all/sumAll.js
@@ -1,6 +1,12 @@
 
-function sumAll(...args) {
-  return args;
+function sumAll([num1, num2]) {
+  const biggerNumber = num1 > num2 ? num1 : num2;
+  const smallerNumber = num1 < num2 ? num1 : num2;
+  let sum = 0;
+  for (let i = smallerNumber; i <= biggerNumber; i += 1) {
+    sum += i;
+  }
+  return sum;
 }
 
 export {
