97587ac57001de8ca5fc0522e31558ee1425638c
Sebin Benjamin <sebinbenjamin@gmail.com>
2019-06-13 01:36:52 +0530

    completed apply-operator exercise

--- apply-operator/applyOperator.js
+++ apply-operator/applyOperator.js
@@ -1,6 +1,21 @@
-
-function applyOperator(...args) {
-  return args;
+function applyOperator(operator, ...nums) {
+  if (operator) {
+    if (nums.length === 0) return 0;
+    // eslint-disable-next-line default-case
+    switch (operator) {
+      case '+':
+        return nums.reduce((res, num) => res + num, 0);
+      case '-':
+        return nums.reduce((res, num) => res - num, 0);
+      case '*':
+        return nums.reduce((res, num) => res * num, 1);
+      case '/':
+        return Number(nums.slice(1).reduce((res, num) => res / num, nums[0]).toFixed(4));
+      case '%':
+        return nums.slice(1).reduce((res, num) => res % num, nums[0]);
+    }
+  }
+  throw new Error('Unexpected Operator');
 }
 
 export {
