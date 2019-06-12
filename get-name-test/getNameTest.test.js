d5ccfee49da3cd3c0087de5b481e6a5c4ffee3f2
Abdul Khuddus <khuddus1@gmail.com>
2019-06-12 06:55:34 +0530

    Complete getNameTest

--- get-name-test/getNameTest.test.js
+++ get-name-test/getNameTest.test.js
@@ -1,18 +1,15 @@
-function getName(name) {
-  if (typeof name === 'string') return `You entered: ${name}`;
-  throw new Error('name should be of type string');
-}
+import { getName } from './getNameTest';
 
 
 describe('throwError test', () => {
   test('should return string with given name', () => {
     const actual = getName('Martin Garrix');
-    const expected = /Ramu/;
+    const expected = /\bMartin Garrix\b/;
     expect(actual).toMatch(expected);
   });
 
   test('should throw error if argument is not a string', () => {
-    const expected = 'there was an error';
+    const expected = 'name should be of type string';
     expect(() => getName(123)).toThrow(expected);
   });
 });
