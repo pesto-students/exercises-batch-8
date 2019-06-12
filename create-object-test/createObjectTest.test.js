7631ab7353df07493bc4af24d65bb63dfb1fe4db
Rakesh Tembhurne <tj2point0@gmail.com>
2019-06-12 15:39:14 +0530

    Split longer test into two separate tests

--- create-object-test/createObjectTest.test.js
+++ create-object-test/createObjectTest.test.js
@@ -1,7 +1,11 @@
 import { createObjectTest as createObject } from './createObjectTest';
 
 describe('createObject', () => {
-  test('should return an object with fname and lname as key with values as passed to function', () => {
-    expect(createObject('anshu', 'rathee')).toMatchObject({ fname: 'anshu', lname: 'rathee' });
+  test('should return an object with fname', () => {
+    expect(createObject('anshu', 'rathee').fname).toBe('anshu');
+  });
+
+  test('should return an object with fname', () => {
+    expect(createObject('anshu', 'rathee').lname).toBe('rathee');
   });
 });
