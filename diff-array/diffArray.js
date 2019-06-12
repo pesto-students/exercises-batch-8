028f3fc080273ebfd9dbf86a4e0be352401f0709
saurabh shetty <saurabh.shetty100@gmail.com>
2019-06-13 04:57:48 +0530

    Solved diffArray

--- diff-array/diffArray.js
+++ diff-array/diffArray.js
@@ -1,6 +1,19 @@
 
-function diffArray(...args) {
-  return args;
+function diffArray(array1, array2) {
+  const resultArray = [];
+  array1.map((array1val) => {
+    if (!array2.includes(array1val)) {
+      resultArray.push(array1val);
+    }
+    return null;
+  });
+  array2.map((array2val) => {
+    if (!array1.includes(array2val)) {
+      resultArray.push(array2val);
+    }
+    return null;
+  });
+  return resultArray;
 }
 
 export {
