d66366f56429058be51c82b4aef91b5089481776
aayushtiwari <arsenicaayush@gmail.com>
2019-06-13 00:55:05 +0530

    booWho done

--- boo-who/booWho.js
+++ boo-who/booWho.js
@@ -1,6 +1,16 @@
 
 function booWho(...args) {
-  return args;
+        if (typeof (args[0]) === 'boolean') {
+                return true;
+        }
+        if (typeof (args[0]) === 'object') {
+                return false;
+        } if (typeof (args[0]) === 'number') {
+                return false;
+        }
+        if (typeof (args[0]) === 'string') {
+                return false;
+        }
 }
 
 export {
