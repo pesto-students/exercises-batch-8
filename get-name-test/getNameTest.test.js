b119e05d7ba3b45aee156280af3333a3e284b854
Jaspreet Singh <erjaspreet57@gmail.com>
2019-06-12 14:46:35 +0530

    Solve get-name-test

--- get-name-test/getNameTest.test.js
+++ get-name-test/getNameTest.test.js
@@ -1,18 +1,14 @@
-function getName(name) {
-  if (typeof name === 'string') return `You entered: ${name}`;
-  throw new Error('name should be of type string');
-}
-
+import { getName } from './getNameTest';
 
 describe('throwError test', () => {
   test('should return string with given name', () => {
     const actual = getName('Martin Garrix');
-    const expected = /Ramu/;
+    const expected = 'You entered: Martin Garrix';
     expect(actual).toMatch(expected);
   });
 
   test('should throw error if argument is not a string', () => {
-    const expected = 'there was an error';
+    const expected = 'name should be of type string';
     expect(() => getName(123)).toThrow(expected);
   });
 });
