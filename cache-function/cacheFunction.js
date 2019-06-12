9442fdd6c76ce9f94e87bf4c82581fa4089f8a3d
saurabh shetty <saurabh.shetty100@gmail.com>
2019-06-13 00:50:53 +0530

    Solved cacheFunction

--- cache-function/cacheFunction.js
+++ cache-function/cacheFunction.js
@@ -1,8 +1,13 @@
 
-function cacheFunction(...args) {
-  return args;
+function cacheFunction(fun) {
+  const data = {};
+  return (arg) => {
+    // eslint-disable-next-line no-prototype-builtins
+    if (data.hasOwnProperty(arg)) return data[arg];
+    data[arg] = fun(arg);
+    return data[arg];
+  };
 }
-
 export {
   cacheFunction,
 };
