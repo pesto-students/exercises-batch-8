400df422a2909353427e0ea21719f597aa229ec2
Sebin Benjamin <sebinbenjamin@gmail.com>
2019-06-14 00:56:09 +0530

    Completed 'map-object' exercise

--- map-object/mapObject.js
+++ map-object/mapObject.js
@@ -1,6 +1,13 @@
 
-function mapObject(...args) {
-  return args;
+function mapObject(obj, mapFn) {
+  const resultObject = {};
+  for (const key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      const mappedProperty = mapFn(obj[key]);
+      resultObject[key] = mappedProperty;
+    }
+  }
+  return resultObject;
 }
 
 export {
