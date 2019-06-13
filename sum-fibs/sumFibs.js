f41b1d5fccac2e90a23f1d729a4b6f65b19df81e
Abhinav Sharma <abhi18av@outlook.com>
2019-06-13 19:25:38 +0530

    completed the sum-fibs exercise

--- sum-fibs/sumFibs.js
+++ sum-fibs/sumFibs.js
@@ -1,6 +1,25 @@
-
 function sumFibs(...args) {
-  return args;
+  const fibSeriesLimit = args[0];
+  function fibonacci(aNumber) {
+    if (aNumber === 0) {
+      throw new Error('Fibonacci starts from number 1');
+    } else if (aNumber === 1) {
+      return 1;
+    } else if (aNumber === 2) {
+      return 1;
+    } else {
+      return fibonacci(aNumber - 1) + fibonacci(aNumber - 2);
+    }
+  }
+
+  const fibOddNumSeries = [];
+  for (let i = 1; fibonacci(i) <= fibSeriesLimit; i++) {
+    if (fibonacci(i) % 2 === 1) {
+      fibOddNumSeries.push(fibonacci(i));
+    }
+  }
+
+  return fibOddNumSeries.reduce((a, b) => a + b, 0);
 }
 
 export {
