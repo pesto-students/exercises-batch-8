ee0b375cf8be8d93faf0c4be5d88625673e6b2a3
Kanav Arora <kanav_arora@statusbrew.com>
2019-06-14 00:49:22 +0530

    [WIP] Complet Object Defaults

--- object-defaults/objectDefaults.js
+++ object-defaults/objectDefaults.js
@@ -1,6 +1,14 @@
+const fillObject = (newObject, defaultObject) => {
+  Object.keys(defaultObject)
+    .forEach((key) => {
+      // eslint-disable-next-line no-param-reassign
+      newObject[key] = newObject[key] || defaultObject[key];
+    });
+  return newObject;
+};
 
-function objectDefaults(...args) {
-  return args;
+function objectDefaults(object, defaultObject) {
+  return fillObject(object, defaultObject);
 }
 
 export {
