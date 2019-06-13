f2c83acb802c6d6c2e0630bbdf9b8a3e86a81dca
saurabh shetty <saurabh.shetty100@gmail.com>
2019-06-13 19:52:37 +0530

    Fixed diffArray

--- diff-array/diffArray.js
+++ diff-array/diffArray.js
@@ -1,9 +1,8 @@
 
 function diffArray(array1, array2) {
-  const resultArray = [];
-  resultArray.concat(array1.filter(array1val => !array2.includes(array1val)));
-  resultArray.concat(array2.filter(array2val => !array1.includes(array2val)));
-  return resultArray;
+  const array1UniqueValues = array1.filter(array1val => !array2.includes(array1val));
+  const array2UniqueValues = array2.filter(array2val => !array1.includes(array2val));
+  return array1UniqueValues.concat(array2UniqueValues);
 }
 
 export {
