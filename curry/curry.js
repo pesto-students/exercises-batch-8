af74c6f991b6aad6a7653ef4e94dc6d410b08b84
kunnu01 <kunalbhatia2409@gmail.com>
2019-06-14 04:32:16 +0530

    Solved Curry

--- curry/curry.js
+++ curry/curry.js
@@ -1,6 +1,14 @@
 
-function curry(...args) {
-  return args;
+function curry(func) {
+  return function curried(...args) {
+    if (args.length >= func.length) {
+      return func.apply(this, args);
+    }
+    // eslint-disable-next-line func-names
+    return function (...args2) {
+      return curried.apply(this, args.concat(args2));
+    };
+  };
 }
 
 export {
