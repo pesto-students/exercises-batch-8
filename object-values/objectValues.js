11e5c005219c55d8e8338f4e4ef7e60e7d6093cf
saurabh shetty <saurabh.shetty100@gmail.com>
2019-06-14 00:45:23 +0530

    solved object-values

--- object-values/objectValues.js
+++ object-values/objectValues.js
@@ -1,6 +1,12 @@
 
-function objectValues(...args) {
-  return args;
+function objectValues(object) {
+  const resultArray = [];
+  // eslint-disable-next-line guard-for-in
+  for (const key in object) {
+    resultArray.push(object[key]);
+  }
+
+  return resultArray;
 }
 
 export {
