9b86f8d2d3445ddb1703ce223fcd05441f2884ee
Abhinav Sharma <abhi18av@outlook.com>
2019-06-13 20:45:33 +0530

    tweaked reverse-string problem

--- reverse-string/reverseString.js
+++ reverse-string/reverseString.js
@@ -2,6 +2,7 @@
 function reverseString(...args) {
   const originalString = args[0];
   let finalReversedString = '';
+  // Just iterate over the array from the back and add the characters to the finalReversedString array
   for (let i = originalString.length - 1; i >= 0; i--) {
     finalReversedString += originalString[i];
   }
