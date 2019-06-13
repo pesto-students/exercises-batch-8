39c04206e2eb26eac5c94a66c13f407eb64b18cb
saurabh shetty <saurabh.shetty100@gmail.com>
2019-06-13 09:23:23 +0530

    solved sumall

--- sum-all/sumAll.js
+++ sum-all/sumAll.js
@@ -1,6 +1,13 @@
 
-function sumAll(...args) {
-  return args;
+function sumAll(values) {
+  const [firstValue, lastValue] = values.sort((a, b) => a - b);
+  let sum = 0;
+  let index = firstValue;
+  while (index <= lastValue) {
+    sum += index;
+    index += 1;
+  }
+  return sum;
 }
 
 export {
