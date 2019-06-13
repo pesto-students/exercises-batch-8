a2117646fefff8c1ad1602e093e3c13b5c6ce296
Abdul Khuddus <khuddus1@gmail.com>
2019-06-14 00:46:46 +0530

    Complete chunkArrayInGroups

--- chunk-array-in-groups/chunkArrayInGroups.js
+++ chunk-array-in-groups/chunkArrayInGroups.js
@@ -1,6 +1,12 @@
 
-function chunkArrayInGroups(...args) {
-  return args;
+function chunkArrayInGroups(arr, size) {
+  return arr.reduce((acc, element, i, array) => {
+    const isNewArrayNeeded = i % size === 0;
+    if (isNewArrayNeeded) {
+      acc.push(array.slice(i, i + size));
+    }
+    return acc;
+  }, []);
 }
 
 export {
