8c79850038aff1212c3a09f9d96c8b141eb0272b
sourabhmodi20051 <sourabhmodi20051@gmail.com>
2019-06-14 05:23:56 +0530

    Solved account dataset problem

--- account-dataset/accountDataset.js
+++ account-dataset/accountDataset.js
@@ -1,8 +1,87 @@
+const accounts = require('./dataset.json').bankBalances;
 
-function accountDataset(...args) {
-  return args;
+const INTEREST_RATE = 18.9;
+
+const STATES = ['WI', 'WY', 'OH', 'GA', 'DE', 'IL'];
+
+function round(number, precision) {
+  const roundingFactor = 10 ** precision;
+  return Math.round(number * roundingFactor) / roundingFactor;
 }
 
+function filterAccounts(predicate) {
+  return accounts
+    .filter(account => predicate(account.amount));
+}
+
+function hundredThousandairs() {
+  return filterAccounts(n => n > 100000);
+}
+
+function datasetWithRoundedDollar() {
+  return accounts
+    .map(account => { 
+      const updatedAccount = { ...account, rounded: round(account.amount, 0) };
+      return updatedAccount;
+    });
+}
+
+function sumOfBankBalancesForAccounts(accounts) {
+  const sum = accounts
+    .reduce((sumAccumulator, account) => sumAccumulator + Number(account.amount), 0);
+  return round(sum, 2);
+}
+
+function sumOfBankBalances() {
+  return sumOfBankBalancesForAccounts(accounts);
+}
+
+function sumOfInterests() {
+  const interestSum = accounts
+    .reduce((interestSumAccumulator, account) => {
+      if( STATES.includes(account.state) ){
+        const interest = round(account.amount * INTEREST_RATE / 100, 2);
+        return interestSumAccumulator + interest;
+      }
+      else{
+        return interestSumAccumulator;
+      }
+    }, 0);
+  return round(interestSum, 2);
+}
+
+function higherStateSums() {
+  const amountSumHashTable = accounts.reduce((accumulatedHashTable, account) => {
+    const accumulatedHashTableCopy = {...accumulatedHashTable}
+    const state = account.state;
+    const amount = account.amount;
+    if(accumulatedHashTableCopy[state] == undefined){
+      accumulatedHashTableCopy[state] = Number(amount)
+    }
+    else{
+      accumulatedHashTableCopy[state] += Number(amount)
+    }
+    return accumulatedHashTableCopy
+  },{});
+
+  const aggregatedSum =  Object.keys(amountSumHashTable).reduce((acc, key) => {
+    if(amountSumHashTable[key] > 1000000){
+      return acc + amountSumHashTable[key];
+    }
+    else{
+      return acc;
+    }
+  },0);
+  return round(aggregatedSum, 2);
+}
+
+
+
+
 export {
-  accountDataset,
+  hundredThousandairs,
+  datasetWithRoundedDollar,
+  sumOfBankBalances,
+  sumOfInterests,
+  higherStateSums
 };
