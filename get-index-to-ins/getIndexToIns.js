6993d90ed7cc31db33a2e7ea25977fbcf079975a
sourabhmodi20051 <sourabhmodi20051@gmail.com>
2019-06-13 10:45:02 +0530

    Get Index to Insert Solved

--- get-index-to-ins/getIndexToIns.js
+++ get-index-to-ins/getIndexToIns.js
@@ -1,6 +1,12 @@
 
-function getIndexToIns(...args) {
-  return args;
+function getIndexToIns(array, itemToInsert) {
+  const sortedArray = array.sort((a, b) => a - b);
+  for (let i = 0; i < sortedArray.length; i += 1) {
+    if (sortedArray[i] >= itemToInsert) {
+      return i;
+    }
+  }
+  return sortedArray.length;
 }
 
 export {
