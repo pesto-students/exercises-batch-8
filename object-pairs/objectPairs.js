7340e2e5dffcc6534e76ad474a1f7872709ae328
Kanav Arora <kanav_arora@statusbrew.com>
2019-06-14 01:34:53 +0530

    [FIX] fix logic ObjectParis

--- object-pairs/objectPairs.js
+++ object-pairs/objectPairs.js
@@ -1,9 +1,6 @@
 
 function objectPairs(object) {
-  return Object.keys(object).reduce((result = [], key) => {
-    result.push([key, object[key]]);
-    return result;
-  }, []);
+  return Object.entries(object);
 }
 
 export {
