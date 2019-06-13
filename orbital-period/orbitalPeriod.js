4580a368b7744f9dc75a7118225fd1d5bbd5c3e4
Rakesh Tembhurne <tj2point0@gmail.com>
2019-06-14 01:50:42 +0530

    Complete orbital-period

--- orbital-period/orbitalPeriod.js
+++ orbital-period/orbitalPeriod.js
@@ -1,6 +1,19 @@
 
-function orbitalPeriod(...args) {
-  return args;
+function orbitalPeriod(altitudeDataList) {
+  const GM = 398600.4418; // in km3s-2
+  const earthRadius = 6367.4447; // in km
+
+  const orbitalPeriodsList = altitudeDataList.map((altData) => {
+    const height = earthRadius + altData.avgAlt;
+    const period = 2 * Math.PI * Math.sqrt(((height ** 3) / GM));
+
+    return {
+      name: altData.name,
+      orbitalPeriod: Math.round(period),
+    };
+  });
+
+  return orbitalPeriodsList;
 }
 
 export {
