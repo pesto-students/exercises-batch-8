d843617ecb1871da7e2e20e9cfea12fc6b41c9c3
kunnu01 <kunalbhatia2409@gmail.com>
2019-06-14 04:52:44 +0530

    Solved memoize and squareNumbersArray

--- square-numbers-array/squareNumbersArray.js
+++ square-numbers-array/squareNumbersArray.js
@@ -1,6 +1,10 @@
 
-function squareNumbersArray(...args) {
-  return args;
+function squareNumbersArray(...array) {
+  const hasNonNumericalArgs = array.filter(ele => Number.isNaN(parseInt(ele, 10)));
+  if (hasNonNumericalArgs.length) {
+    throw new Error('My custom error');
+  }
+  return arguments.map(ele => ele ** 2);
 }
 
 export {
