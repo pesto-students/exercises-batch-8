80e740f3509c8095ee8496fe76b9918257a5ee75
kunnu01 <kunalbhatia2409@gmail.com>
2019-06-13 20:50:15 +0530

    refactor is-triangle

--- is-triangle/isTriangle.js
+++ is-triangle/isTriangle.js
@@ -1,13 +1,8 @@
 
-function isTriangle(...args) {
-  const checkOne = args[0] + args[1] > args[2];
-  const checkTwo = args[1] + args[2] > args[0];
-  const checkThree = args[2] + args[0] > args[1];
-
-  if (checkOne && checkTwo && checkThree) {
-    return true;
-  }
-  return false;
+function isTriangle(...sides) {
+  sides.sort((a, b) => a - b);
+  const [smallSide, shortSide, longerSide] = sides;
+  return longerSide < shortSide + smallSide;
 }
 
 export {
