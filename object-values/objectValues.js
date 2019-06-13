15a8bf01d9eabe1281a1e2b784762e6340a6e03c
Abdul Khuddus <khuddus1@gmail.com>
2019-06-14 01:04:29 +0530

    Complete objectValues

--- object-values/objectValues.js
+++ object-values/objectValues.js
@@ -1,6 +1,9 @@
 
-function objectValues(...args) {
-  return args;
+function objectValues(obj) {
+  return Object.keys(obj).reduce((acc, key) => {
+    acc.push(obj[key]);
+    return acc;
+  }, []);
 }
 
 export {
