a56a70c82f813599c37515412b612b182f94e67f
Pradyumna Newalkar <contactpradyumna1@gmail.com>
2019-06-13 23:12:15 +0000

    Added alternating-submissions

--- alternating-characters/alternatingCharacters.js
+++ alternating-characters/alternatingCharacters.js
@@ -1,6 +1,14 @@
+function countAlternateSameChar(str) {
+  return str.split('').reduce((count, char, index, arr) => {
+    if (index < arr.length - 1 && arr[index] === arr[index + 1]) {
+      return count + 1;
+    }
+    return count;
+  }, 0);
+}
 
-function alternatingCharacters(...args) {
-  return args;
+function alternatingCharacters(strings) {
+  return strings.map(countAlternateSameChar);
 }
 
 export {
