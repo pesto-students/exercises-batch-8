6996c8b93daa5bfe652316402eaf5fc1de96dc6a
saurabh shetty <saurabh.shetty100@gmail.com>
2019-06-13 08:20:39 +0530

    solved repeatString

--- repeat-string-num-times/repeatStringNumTimes.js
+++ repeat-string-num-times/repeatStringNumTimes.js
@@ -1,6 +1,12 @@
 
-function repeatStringNumTimes(...args) {
-  return args;
+function repeatStringNumTimes(string, repeatations) {
+  let noOfRepetations = repeatations;
+  let resultString = '';
+  while (noOfRepetations > 0) {
+    resultString += string;
+    noOfRepetations -= 1;
+  }
+  return resultString;
 }
 
 export {
