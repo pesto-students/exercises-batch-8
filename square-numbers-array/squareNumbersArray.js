878a4ab2525cefc698c6324406713bb8147b9998
Kanav Arora <kanav_arora@statusbrew.com>
2019-06-14 04:44:39 +0530

    [TASK] solved square Number and added test case

--- square-numbers-array/squareNumbersArray.js
+++ square-numbers-array/squareNumbersArray.js
@@ -1,6 +1,12 @@
 
+const arraySquare = arr => arr.map(a => a * a);
+
 function squareNumbersArray(...args) {
-  return args;
+  if (args.some(isNaN)) {
+    throw new Error('this function can only square numbers');
+  } else {
+    return arraySquare(args);
+  }
 }
 
 export {
