4d7922fcdf9593e5f422a6db29073b2e33f16ab0
prateekinstillion <Prateek Madaan>
2019-06-14 00:34:18 +0530

    Solved the objectkeys problem

--- object-keys/objectKeys.js
+++ object-keys/objectKeys.js
@@ -1,6 +1,12 @@
 
-function objectKeys(...args) {
-  return args;
+function objectKeys(object) {
+  let keysOfObject = []
+  for (const key in object) {
+    if (object.hasOwnProperty(key)) {
+      keysOfObject.push(key);
+    }
+  }
+  return keysOfObject;
 }
 
 export {
