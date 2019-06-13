182084fa3781e6f44facc1b995f0066cf44b991c
prateekinstillion <Prateek Madaan>
2019-06-13 18:38:42 +0530

    Solved is-palindrome,large-of-four, reverse-string, sum-all

--- bouncer/bouncer.js
+++ bouncer/bouncer.js
@@ -1,6 +1,15 @@
+/* eslint-disable no-trailing-spaces */
+const retainTruthy = value => value !== undefined
+  && value !== null 
+  && value !== false 
+  && value !== 0 
+  && value !== '' 
+  // eslint-disable-next-line no-restricted-globals
+  && !isNaN(value);
 
-function bouncer(...args) {
-  return args;
+function bouncer(...values) {
+  const truthyValues = values.filter(retainTruthy);
+  return truthyValues;
 }
 
 export {
