d0effc02b148d14f6fc7b10debd1ba60ce2dc441
Sebin Benjamin <sebinbenjamin@gmail.com>
2019-06-14 04:14:43 +0530

    completed 'object-keys' exercise

--- object-keys/objectKeys.js
+++ object-keys/objectKeys.js
@@ -1,6 +1,11 @@
-
-function objectKeys(...args) {
-  return args;
+function objectKeys(obj) {
+  const resultArray = [];
+  for (const key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      resultArray.push(key);
+    }
+  }
+  return resultArray;
 }
 
 export {
