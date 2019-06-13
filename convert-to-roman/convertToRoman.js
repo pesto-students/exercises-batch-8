c6b0f4742cfe04f0f76e57d2315064c48984e5b6
Sebin Benjamin <sebinbenjamin@gmail.com>
2019-06-13 20:54:19 +0530

    completed 'convert-to-roman' exercise

--- convert-to-roman/convertToRoman.js
+++ convert-to-roman/convertToRoman.js
@@ -1,6 +1,17 @@
+const romanNumMapping = new Map([[1000, 'M'], [900, 'CM'], [500, 'D'], [400, 'CD'], [100, 'C'], [90, 'XC'], [50, 'L'], [40, 'XL'], [10, 'X'], [9, 'IX'], [5, 'V'], [4, 'IV'], [1, 'I']]);
 
-function convertToRoman(...args) {
-  return args;
+function convertToRoman(...indoArabicNum) {
+  let indoArabicNumeral = indoArabicNum;
+  let romanEquivalent = '';
+  for (const [indoArabicSymbol, romanSymbol] of romanNumMapping.entries()) {
+    let romanSymbolRepetitions = Math.floor(indoArabicNumeral / indoArabicSymbol);
+    while (romanSymbolRepetitions > 0) {
+      romanEquivalent += romanSymbol;
+      indoArabicNumeral -= indoArabicSymbol;
+      romanSymbolRepetitions -= 1;
+    }
+  }
+  return romanEquivalent;
 }
 
 export {
