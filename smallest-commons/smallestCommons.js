29564c499c4a3b67100fbe592fffcb9c9aca87d7
Jaspreet Singh <erjaspreet57@gmail.com>
2019-06-14 01:50:11 +0530

    Solve smallest-commons

--- smallest-commons/smallestCommons.js
+++ smallest-commons/smallestCommons.js
@@ -1,6 +1,38 @@
+function range(min, max) {
+  const arr = [];
+  for (let i = min; i <= max; i += 1) {
+    arr.push(i);
+  }
+  return arr;
+}
 
-function smallestCommons(...args) {
-  return args;
+function gcd(a, b) {
+  return !b ? a : gcd(b, a % b);
+}
+
+
+function lcm(a, b) {
+  return (a * b) / gcd(a, b);
+}
+
+
+function smallestCommons([number1, number2] = []) {
+  let min;
+  let max;
+  if (number1 > number2) {
+    max = number1;
+    min = number2;
+  } else {
+    max = number2;
+    min = number1;
+  }
+
+  let multiple = min;
+  range(min, max).forEach((num) => {
+    multiple = lcm(multiple, num);
+  });
+
+  return multiple;
 }
 
 export {
