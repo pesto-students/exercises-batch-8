1cf7835181a72c3c384f9a364ae18bddeda4891f
saurabh shetty <saurabh.shetty100@gmail.com>
2019-06-13 06:43:14 +0530

    Solved isPalindrome

--- is-palindrome/isPalindrome.js
+++ is-palindrome/isPalindrome.js
@@ -1,6 +1,12 @@
 
-function isPalindrome(...args) {
-  return args;
+function isPalindrome(string) {
+  const cleanString = String(string).replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+  for (let i = 0; i < cleanString.length / 2; i += 1) {
+    if (cleanString[i] !== cleanString[cleanString.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 export {
