0a1333534e1dd2e7c8484503f456013aeaff5728
Radheshyam Etikala <radhey1851@gmail.com>
2019-06-14 00:22:36 +0530

    Completed first one

--- chunk-array-in-groups/chunkArrayInGroups.js
+++ chunk-array-in-groups/chunkArrayInGroups.js
@@ -1,6 +1,13 @@
 
-function chunkArrayInGroups(...args) {
-  return args;
+function chunkArrayInGroups(array, chunkSize) {
+  const arrayOfChunks = [];
+
+  for (let i = 0; i < array.length; i += chunkSize) {
+    const chunk = array.slice(i, i + chunkSize);
+    arrayOfChunks.push(chunk);
+  }
+
+  return arrayOfChunks;
 }
 
 export {
