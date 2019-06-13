f51c43e25f4e7b76113c03eb8fa0981fa1febec0
Abdul Khuddus <khuddus1@gmail.com>
2019-06-13 10:21:43 +0530

    Complete getIndexToIns

--- get-index-to-ins/getIndexToIns.js
+++ get-index-to-ins/getIndexToIns.js
@@ -1,6 +1,12 @@
 
-function getIndexToIns(...args) {
-  return args;
+function getIndexToIns([...numbers], insertingNumber) {
+  return numbers.reduce((acc, num) => {
+    if (num < insertingNumber) {
+      // eslint-disable-next-line no-param-reassign
+      acc += 1;
+    }
+    return acc;
+  }, 0);
 }
 
 export {
