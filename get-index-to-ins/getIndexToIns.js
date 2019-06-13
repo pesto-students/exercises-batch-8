1c697a9fd6af14c52db6005976164835497a149c
Kanav Arora <kanav_arora@statusbrew.com>
2019-06-13 16:37:15 +0530

    [WIP] Completed getIndexToIns

--- get-index-to-ins/getIndexToIns.js
+++ get-index-to-ins/getIndexToIns.js
@@ -1,6 +1,15 @@
 
-function getIndexToIns(...args) {
-  return args;
+function getIndexToIns(array, valueToInsert) {
+  array.sort((a, b) => a - b);
+  for (let i = 0; i < array.length; i += 1) {
+    if (i === 0 && array[i] >= valueToInsert) { // case if valueToInsert is smallest
+      return 0;
+    } else if (array[i] < valueToInsert && valueToInsert <= array[i + 1]) { // case if valueToInsert is between two index
+      return i + 1;
+    } else if (i === array.length - 1 ) { // case if valueToInsert is largest
+      return i + 1;
+    }
+  }
 }
 
 export {
