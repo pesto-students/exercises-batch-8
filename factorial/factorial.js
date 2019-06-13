b4e4b290de02025b087c4f527d807a2c44140fdf
Abhinav Sharma <abhi18av@outlook.com>
2019-06-13 18:33:55 +0530

    completed the factorial exercise

--- factorial/factorial.js
+++ factorial/factorial.js
@@ -1,19 +1,12 @@
 
 function factorial(...args) {
-
-if (typeof aNumber === Number) {
   const aNumber = args[0];
-} else {
-  return "Not a number"
-}
-
-
-  if(aNumber === 0){
-    return 1 
-  } else if (aNumber === 1){
-    return 1 
+  if (aNumber === 0) {
+    return 1;
+  } else if (aNumber === 1) {
+    return 1;
   } else {
-    return aNumber * factorial(aNumber - 1)
+    return aNumber * factorial(aNumber - 1);
   }
 }
 
