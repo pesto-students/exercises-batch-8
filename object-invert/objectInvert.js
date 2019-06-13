5da8c3bda388a4212f20f746613e3437a4720a43
Radheshyam Etikala <radhey1851@gmail.com>
2019-06-14 00:57:43 +0530

    completed objectInvert

--- object-invert/objectInvert.js
+++ object-invert/objectInvert.js
@@ -1,6 +1,13 @@
 
-function objectInvert(...args) {
-  return args;
+function objectInvert(obj) {
+  const invertedObj = {};
+
+  const keyList = Object.keys(obj);
+  keyList.forEach((key) => {
+    const value = obj[key].toString();
+    invertedObj[value] = key;
+  });
+  return invertedObj;
 }
 
 export {
