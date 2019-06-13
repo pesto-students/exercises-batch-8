0e770604167a00fba9b95e70940bf3748189551a
Abhinav Sharma <abhi18av@outlook.com>
2019-06-13 19:28:55 +0530

    made some tweaks in sum-fibs

--- sum-fibs/sumFibs.js
+++ sum-fibs/sumFibs.js
@@ -1,5 +1,6 @@
 function sumFibs(...args) {
   const fibSeriesLimit = args[0];
+  // Calculating fibonacci value of a number
   function fibonacci(aNumber) {
     if (aNumber === 0) {
       throw new Error('Fibonacci starts from number 1');
@@ -13,12 +14,14 @@ function sumFibs(...args) {
   }
 
   const fibOddNumSeries = [];
+  // Calculating the odd numbers whose fibonacci value is less than a given number
   for (let i = 1; fibonacci(i) <= fibSeriesLimit; i++) {
     if (fibonacci(i) % 2 === 1) {
       fibOddNumSeries.push(fibonacci(i));
     }
   }
 
+  // Returning the sum of the calculated odd fibonacci values
   return fibOddNumSeries.reduce((a, b) => a + b, 0);
 }
 
