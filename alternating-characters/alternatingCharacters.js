cf8fffee071a38729ddab1c78b6b80e8dfc08e61
saurabh shetty <saurabh.shetty100@gmail.com>
2019-06-14 04:47:01 +0530

    solved alternatingCharacters

--- alternating-characters/alternatingCharacters.js
+++ alternating-characters/alternatingCharacters.js
@@ -1,6 +1,21 @@
-
-function alternatingCharacters(...args) {
-  return args;
+/* eslint-disable no-param-reassign */
+// eslint-disable-next-line no-unused-vars
+function alternatingCharacters(strings) {
+  const totalLettersToRemove = [];
+  strings = strings.map((string) => {
+    let previousLetter = null;
+    totalLettersToRemove.push(string.split('').map((letter) => {
+      if (letter === previousLetter) {
+        previousLetter = letter;
+        letter = null;
+      } else {
+        previousLetter = letter;
+      }
+      return letter;
+    }).filter(letter => letter === null).length);
+    return null;
+  });
+  return totalLettersToRemove;
 }
 
 export {
