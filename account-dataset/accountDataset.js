4b6bcccb5130f38d3b6c7b4a9f04287836c9450f
kunnu01 <kunalbhatia2409@gmail.com>
2019-06-14 05:18:15 +0530

    Solved 3 subQuestions of account-details

--- account-dataset/accountDataset.js
+++ account-dataset/accountDataset.js
@@ -1,8 +1,42 @@
+const dataset = require('./dataset.json');
 
-function accountDataset(...args) {
-  return args;
+const { bankBalances } = dataset;
+
+function hundredThousandairs() {
+  return bankBalances.filter(account => account.amount > 100000);
 }
 
+function datasetWithRoundedDollar() {
+  return bankBalances.map((account) => {
+    const { amount, ...rest } = account;
+    return {
+      amount,
+      rounded: Math.floor(amount),
+      ...rest,
+    };
+  });
+}
+
+function sumOfBankBalances() {
+  const sum = bankBalances.reduce((total, { amount }) => total + parseFloat(amount), 0);
+  return Math.round(sum * 100) / 100;
+}
+
+// function sumOfInterests() {
+//   const interestPercebtage = 18.9;
+//   const states = ['WI', 'IL', 'WY', 'OH', 'GA', 'DE'];
+
+//   const accountsWithSelectedStates = bankBalances
+//     .filter(account => states.includes(account.state));
+
+//   const totalInterest = accountsWithSelectedStates
+//     .reduce((total, { amount }) => {
+
+//     })
+// }
+
 export {
-  accountDataset,
+  hundredThousandairs,
+  datasetWithRoundedDollar,
+  sumOfBankBalances,
 };
