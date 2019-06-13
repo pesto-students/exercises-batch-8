10d294d4685c8a0dbe5090c0cbc8b2d20d48b94d
Sebin Benjamin <sebinbenjamin@gmail.com>
2019-06-14 01:51:26 +0530

    completed exercise 'object-invert'

--- object-invert/objectInvert.js
+++ object-invert/objectInvert.js
@@ -1,6 +1,12 @@
 
-function objectInvert(...args) {
-  return args;
+function objectInvert(obj) {
+  const resultObject = {};
+  for (const key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      resultObject[obj[key]] = key;
+    }
+  }
+  return resultObject;
 }
 
 export {
