e497367a6407c9f77633c3c14cf702377592fda0
Abhinav Sharma <abhi18av@outlook.com>
2019-06-13 18:24:13 +0530

    completed the rectangleDiagonal exercise

--- apply-operator/applyOperator.js
+++ apply-operator/applyOperator.js
@@ -1,12 +1,6 @@
 
 function applyOperator(...args) {
-
-  if (args[0] == "+"){
-    for(var i=1; i++; i<args.length){
-      let addedValue=       
-    }
-  }
-
+  return args;
 }
 
 export {
--- rectangle-diagonal/rectangleDiagonal.js
+++ rectangle-diagonal/rectangleDiagonal.js
@@ -1,7 +1,10 @@
-
 function rectangleDiagonal(...args) {
-
+  const length = args[0];
+  const width = args[1];
+  const square = aNumber => aNumber ** 2;
+  return (Math.sqrt(square(length) + square(width)));
 }
+
 
 export {
   rectangleDiagonal,
