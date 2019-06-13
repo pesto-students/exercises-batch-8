7f8559a439bbf2babe21854a842fdf2e1ec20d7e
Sebin Benjamin <sebinbenjamin@gmail.com>
2019-06-14 01:01:27 +0530

    Completed exercise 'object-defaults'

--- object-defaults/objectDefaults.js
+++ object-defaults/objectDefaults.js
@@ -1,6 +1,12 @@
 
-function objectDefaults(...args) {
-  return args;
+function objectDefaults(obj, defaultObj) {
+  const resultObject = { ...defaultObj };
+  for (const key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      resultObject[key] = obj[key];
+    }
+  }
+  return resultObject;
 }
 
 export {
