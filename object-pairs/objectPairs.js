17833e3c10e82e4a69c4573a7e49f2413c1bebd0
Kanav Arora <kanav_arora@statusbrew.com>
2019-06-14 01:05:32 +0530

    [WIP] Complete Object Pairs

--- object-pairs/objectPairs.js
+++ object-pairs/objectPairs.js
@@ -1,6 +1,9 @@
 
-function objectPairs(...args) {
-  return args;
+function objectPairs(object) {
+  return Object.keys(object).reduce((result = [], key) => {
+    result.push([key, object[key]]);
+    return result;
+  }, []);
 }
 
 export {
