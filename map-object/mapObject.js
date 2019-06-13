85fb1b427263252825be2a1625d3af54b4217057
Abdul Khuddus <khuddus1@gmail.com>
2019-06-14 00:20:06 +0530

    Complete mapObject

--- map-object/mapObject.js
+++ map-object/mapObject.js
@@ -1,6 +1,9 @@
 
-function mapObject(...args) {
-  return args;
+function mapObject(obj, fn) {
+  return Object.keys(obj).reduce((acc, key) => {
+    acc[key] = fn(obj[key]);
+    return acc;
+  }, {});
 }
 
 export {
