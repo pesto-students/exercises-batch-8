4ccc6ef4fbbb1e377486b18033788f5a02ee805a
sourabhmodi20051 <sourabhmodi20051@gmail.com>
2019-06-14 01:11:14 +0530

    Solved map object

--- map-object/mapObject.js
+++ map-object/mapObject.js
@@ -1,6 +1,10 @@
 
-function mapObject(...args) {
-  return args;
+function mapObject(inputObject, mappingFunction) {
+  const mappedObject = Object.assign(...Object.entries(inputObject)
+    .map(([k, v]) => ({
+      [k]: mappingFunction(v) // eslint-disable-line comma-dangle
+    })));
+  return mappedObject;
 }
 
 export {
