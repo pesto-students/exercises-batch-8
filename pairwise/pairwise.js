478204765ded1636489e345f5ef50a475b007594
saurabh shetty <saurabh.shetty100@gmail.com>
2019-06-13 08:16:16 +0530

    Solved pairwise

--- pairwise/pairwise.js
+++ pairwise/pairwise.js
@@ -1,6 +1,21 @@
 
-function pairwise(...args) {
-  return args;
+function pairwise(array, sum) {
+  const finalArray = [];
+  array.map((value1, i) => {
+    array.map((value2, j) => {
+      if (i !== j
+        && value1 + value2 === sum
+        && !finalArray.includes(i)
+        && !finalArray.includes(j)
+      ) {
+        finalArray.push(i);
+        finalArray.push(j);
+      }
+      return null;
+    });
+    return null;
+  });
+  return finalArray.reduce((a, b) => a + b, 0);
 }
 
 export {
