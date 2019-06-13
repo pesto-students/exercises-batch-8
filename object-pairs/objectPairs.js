e6444e9150891f917a4737ced30e0ca995f88d6a
Sebin Benjamin <sebinbenjamin@gmail.com>
2019-06-14 04:32:09 +0530

    completed 'object-pair' exercise

--- object-pairs/objectPairs.js
+++ object-pairs/objectPairs.js
@@ -1,6 +1,12 @@
 
-function objectPairs(...args) {
-  return args;
+function objectPairs(obj) {
+  const resultArray = [];
+  for (const key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      resultArray.push([key, obj[key]]);
+    }
+  }
+  return resultArray;
 }
 
 export {
