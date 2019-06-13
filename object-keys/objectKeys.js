f3597001fd78c6fceafcd69fa0f4b08d2816ca1c
kunnu01 <kunalbhatia2409@gmail.com>
2019-06-14 01:02:50 +0530

    Solved objectInvert and objectKeys

--- object-keys/objectKeys.js
+++ object-keys/objectKeys.js
@@ -1,6 +1,11 @@
 
-function objectKeys(...args) {
-  return args;
+function objectKeys(object) {
+  const keyArray = [];
+  // eslint-disable-next-line guard-for-in
+  for (const key in object) {
+    keyArray.push(key);
+  }
+  return keyArray;
 }
 
 export {
