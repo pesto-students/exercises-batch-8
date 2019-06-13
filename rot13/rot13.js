39bc5e46dd8027bb99626af5ff7256648a6c4d6b
Abhinav Sharma <abhi18av@outlook.com>
2019-06-14 00:08:32 +0530

    started working on rot13

--- rot13/rot13.js
+++ rot13/rot13.js
@@ -1,7 +1,18 @@
 
 function rot13(...args) {
-  return args;
+  var input     = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var output    = 'NOPQRSTUVWXYZABCDEFGHIJKLM';
+  var index     = x => input.indexOf(x);
+  var translate = x => index(x) > -1 ? output[index(x)] : x;
+  return str.split('').map(translate).join('');
 }
+
+
+//console.log(rot13("HELLO WORLD!"))
+
+
+
+
 
 export {
   rot13,
