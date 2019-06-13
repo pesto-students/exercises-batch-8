b8aefe6aa2cfd8bc8dcd8e4768c48250213f73d8
Kanav Arora <kanav_arora@statusbrew.com>
2019-06-13 17:43:31 +0530

    [WIP] Completed StramRoll

--- steamroll-array/steamrollArray.js
+++ steamroll-array/steamrollArray.js
@@ -1,6 +1,12 @@
+function flatten(arr) {
+  // eslint-disable-next-line arrow-body-style
+  return arr.reduce((flat, toFlatten) => {
+    return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
+  }, []);
+}
 
-function steamrollArray(...args) {
-  return args;
+function steamrollArray(nestedArray) {
+  return flatten(nestedArray);
 }
 
 export {
