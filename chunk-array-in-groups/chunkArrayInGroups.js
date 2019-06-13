0c062851d9bd039385c58b133de004cd8e602b01
prateekinstillion <Prateek Madaan>
2019-06-14 00:43:07 +0530

    Solved the array chunks problem

--- chunk-array-in-groups/chunkArrayInGroups.js
+++ chunk-array-in-groups/chunkArrayInGroups.js
@@ -1,8 +1,12 @@
 
-function chunkArrayInGroups(...args) {
-  return args;
+function chunkArrayInGroups(elements, groupSize) {
+  const groupArrays = [];
+  for (let i = 0; i < elements.length; i += groupSize) {
+    const chunk = elements.slice(i, i + groupSize);
+    groupArrays.push(chunk);
+  }
+  return groupArrays;
 }
-
 export {
   chunkArrayInGroups,
 };
