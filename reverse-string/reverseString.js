4dc523b4853467b84a95eeb5044a1c49572e3363
Abdul Khuddus <khuddus1@gmail.com>
2019-06-13 06:41:31 +0530

    Complete reverse string

--- reverse-string/reverseString.js
+++ reverse-string/reverseString.js
@@ -1,6 +1,7 @@
 
-function reverseString(...args) {
-  return args;
+function reverseString(str) {
+  const stringArray = str.split('');
+  return stringArray.reduce((acc, char, index, chars) => acc + chars[chars.length - 1 - index], '');
 }
 
 export {
