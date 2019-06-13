4cc592a02e01170ec64a47af672e951102501bc1
Kanav Arora <kanav_arora@statusbrew.com>
2019-06-13 18:36:17 +0530

    [WIP] Complete Report StringNumtimes

--- repeat-string-num-times/repeatStringNumTimes.js
+++ repeat-string-num-times/repeatStringNumTimes.js
@@ -1,6 +1,13 @@
 
-function repeatStringNumTimes(...args) {
-  return args;
+function repeatStringNumTimes(string, times) {
+  if (times < 0) {
+    return '';
+  }else if (times === 1) {
+    return string;
+  } else {
+    return string + repeatStringNumTimes(string, times - 1);
+  }
+
 }
 
 export {
