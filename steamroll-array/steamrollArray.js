8582b8c9ba19eb3767025ba88add4374c56d1ef0
Abdul Khuddus <khuddus1@gmail.com>
2019-06-13 08:04:46 +0530

    Complete steamrollArray and rotatedString

--- steamroll-array/steamrollArray.js
+++ steamroll-array/steamrollArray.js
@@ -1,6 +1,26 @@
 
-function steamrollArray(...args) {
-  return args;
+function steamrollArray(...arr) {
+  function flatten(complexArray) {
+    return complexArray.reduce((acc, element) => {
+      if (Array.isArray(element)) {
+        // eslint-disable-next-line no-param-reassign
+        acc = [...acc, ...flatten(element)];
+      } else {
+        acc.push(element);
+      }
+      return acc;
+    }, []);
+  }
+
+  return arr.reduce((acc, element) => {
+    if (Array.isArray(element)) {
+      // eslint-disable-next-line no-param-reassign
+      acc = [...acc, ...flatten(element)];
+    } else {
+      acc.push(element);
+    }
+    return acc;
+  }, []);
 }
 
 export {
