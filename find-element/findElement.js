d6b6190c20bd6db414e323226a9bebd925ebdfdc
Abhinav Sharma <abhi18av@outlook.com>
2019-06-14 00:50:58 +0530

    solved the find-element problem

--- find-element/findElement.js
+++ find-element/findElement.js
@@ -1,7 +1,11 @@
 
 function findElement(...args) {
-  return args;
+  const anArray = args[0];
+  const truthyFunction = args[1];
+  // Only return the first element of the filtered elements
+  return anArray.filter(truthyFunction)[0];
 }
+
 
 export {
   findElement,
