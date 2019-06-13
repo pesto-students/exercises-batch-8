b3c39ad54eeff452f3c8c2180d1da28f7b43a381
Pradyumna Newalkar <contactpradyumna1@gmail.com>
2019-06-13 18:46:08 +0000

    Added solution for find-element

--- find-element/findElement.js
+++ find-element/findElement.js
@@ -1,6 +1,12 @@
 
-function findElement(...args) {
-  return args;
+function findElement(arr, found) {
+  if (arr.length === 0) {
+    return undefined;
+  }
+  if (found(arr[0])) {
+    return arr[0];
+  }
+  return findElement(arr.slice(1), found);
 }
 
 export {
