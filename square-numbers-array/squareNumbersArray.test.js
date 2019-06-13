942470cfcd32cb2414a029d273b49e49f80d69fd
prateekinstillion <Prateek Madaan>
2019-06-14 04:56:39 +0530

    Solved the square numbers problem

--- square-numbers-array/squareNumbersArray.test.js
+++ square-numbers-array/squareNumbersArray.test.js
@@ -3,9 +3,10 @@ import { squareNumbersArray } from './squareNumbersArray';
 describe('squareNumbersArray', () => {
   test('should throw an error if array of numbers is not passed as argument', () => {
     const myArray = [1, 2, 'string', 4];
-    expect(() => squareNumbersArray(...myArray)).toThrow('My custom error');
+    expect(() => squareNumbersArray(...myArray)).toThrow('Array of numbers is required');
   });
   test('should return square array', () => {
-    
+    const myArray = [1, 2, 3];
+    expect(squareNumbersArray(...myArray)).toEqual([1, 4, 9]);
   });
 });
