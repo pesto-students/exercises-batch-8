2e91135bd6b29df03de75251864ff4d7d049a65e
Pradyumna Newalkar <contactpradyumna1@gmail.com>
2019-06-13 22:51:23 +0000

    Added memoize

--- memoize/memoize.js
+++ memoize/memoize.js
@@ -1,6 +1,13 @@
 
-function memoize(...args) {
-  return args;
+function memoize(func) {
+  const cache = {};
+  return (...args) => {
+    const key = JSON.stringify(args);
+    if (!Object.prototype.hasOwnProperty.call(cache, key)) {
+      cache[key] = func(...args);
+    }
+    return cache[key];
+  };
 }
 
 export {
