11d982de0947d8f2212698e69855d090ce11d9f2
Rakesh Tembhurne <tj2point0@gmail.com>
2019-06-13 18:52:44 +0530

    Complete is-palindrome

--- is-palindrome/isPalindrome.js
+++ is-palindrome/isPalindrome.js
@@ -1,7 +1,50 @@
+const regExPatternsToReplace = [
+  { regEx: /_/, replacer: '' },
+  { regEx: /([^a])\s/i, replacer: '$1' },
+  { regEx: /[,.\s-]/g, replacer: '' },
+];
+const regExReverseWordReplacers = [
+  { regEx: /\(/, replacer: ')' },
+  { regEx: /\)/, replacer: '(' },
+];
+const regExWordSeparator = /[,.\s]/g;
 
-function isPalindrome(...args) {
-  return args;
-}
+
+const hasWordSeparators = strToCheck => regExWordSeparator.test(strToCheck);
+
+const applyRegExPatterns = (dataToApplyOn, patternsToApply) => {
+  let appliedString = dataToApplyOn;
+
+  patternsToApply.map((pattern) => {
+    appliedString = appliedString.replace(pattern.regEx, pattern.replacer);
+    return true;
+  });
+
+  return appliedString;
+};
+
+const checkWordForPalindrome = (word) => {
+  const reversedWordStringArray = word.split('');
+  const reversedWordString = reversedWordStringArray.reverse();
+
+  let reversedWord = reversedWordString.join('');
+  reversedWord = applyRegExPatterns(reversedWord, regExReverseWordReplacers);
+
+  return reversedWord.toLowerCase() === word.toLowerCase();
+};
+
+const isPalindrome = (givenData) => {
+  let stringToTest = String(givenData);
+
+  stringToTest = applyRegExPatterns(stringToTest, regExPatternsToReplace);
+
+  if (hasWordSeparators(stringToTest)) {
+    stringToTest = stringToTest.toLowerCase();
+
+    return checkWordForPalindrome(stringToTest);
+  }
+  return checkWordForPalindrome(stringToTest);
+};
 
 export {
   isPalindrome,
