27cc707bd7c2c795e38e26488c5afb3ca8363781
saurabh shetty <saurabh.shetty100@gmail.com>
2019-06-13 09:09:18 +0530

    Solved rot13

--- rot13/rot13.js
+++ rot13/rot13.js
@@ -1,6 +1,23 @@
 
-function rot13(...args) {
-  return args;
+function rot13(string) {
+  const updatedWords = [];
+  let stringCopy = string;
+  const initialWords = string.replace(/[^a-zA-Z]/g, ' ').split(' ');
+  initialWords.map((word) => {
+    const updatedLetters = [];
+    word.split('').map((letter) => {
+      updatedLetters.push(String.fromCharCode(letter.charCodeAt(0) - 13 < 65
+        ? letter.charCodeAt(0) + 13 : letter.charCodeAt(0) - 13));
+      return null;
+    });
+    updatedWords.push(updatedLetters.join(''));
+    return null;
+  });
+  updatedWords.map((word, i) => {
+    stringCopy = stringCopy.replace(initialWords[i], word);
+    return null;
+  });
+  return stringCopy;
 }
 
 export {
