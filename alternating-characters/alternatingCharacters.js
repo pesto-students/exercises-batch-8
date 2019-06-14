06523b9788b541f23e485aa358939d3066a68ae7
sourabhmodi20051 <sourabhmodi20051@gmail.com>
2019-06-14 06:33:36 +0530

    Alternating Characters solved

--- alternating-characters/alternatingCharacters.js
+++ alternating-characters/alternatingCharacters.js
@@ -1,8 +1,15 @@
-
-function alternatingCharacters(...args) {
-  return args;
+function alternatingCharacters(strings) {
+  const replacementCounts = strings.map((string) => {
+    const alternatingCharactersString = string.split('').reduce((acc, character, index) => {
+      if (index === 0 || acc[acc.length - 1] !== character) {
+        return acc + character;
+      }
+      return acc;
+    }, '');
+    const replacementCount = string.length - alternatingCharactersString.length;
+    return replacementCount;
+  });
+  return replacementCounts;
 }
 
-export {
-  alternatingCharacters,
-};
+export { alternatingCharacters };
