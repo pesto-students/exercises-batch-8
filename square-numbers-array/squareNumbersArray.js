373ca00e2029a87c17c538e8aab257bd8c53f7f6
Sebin Benjamin <sebinbenjamin@gmail.com>
2019-06-14 19:19:26 +0530

    completed 'square-number' exercise

--- square-numbers-array/squareNumbersArray.js
+++ square-numbers-array/squareNumbersArray.js
@@ -1,6 +1,10 @@
-
-function squareNumbersArray(...args) {
-  return args;
+function squareNumbersArray(array) {
+  return array.map((element) => {
+    const numConvertedElement = Number(element);
+    const isElementNumber = !Number.isNaN(numConvertedElement);
+    if (isElementNumber) return numConvertedElement ** 2;
+    throw Error('Array contains a Non-number element');
+  });
 }
 
 export {
