260f6d1473d8986cb4bfdaeedf3f2a2263b97ec1
Radheshyam Etikala <radhey1851@gmail.com>
2019-06-15 00:50:05 +0530

    Completed lowercasekeys

--- json-parse/json-data.js
+++ json-parse/json-data.js
@@ -5,10 +5,12 @@ const clubMember = {
   memberStatus: 'Full',
 };
 
+clubMember.city = 'LA';
+
 const Sara = '{ "height":1.9, "age":36, "eyeColor":"brown"}';
 
 // use JSON.parse() to create an object 'objectSara':
-const objectSara = `${Sara} add code here`;
+const objectSara = JSON.parse(Sara);
 
 
 // var myArray = [];
@@ -31,7 +33,7 @@ const conference = {
 // You can access the individual properties of the object as if they were elements
 // in an associative array: `conference['startDay']`
 // Add a line of code to store the name of the conference's end day in variable endDayIs.
-const endDay = conference;
+const { endDay } = conference;
 
 
 const Mike = {};
