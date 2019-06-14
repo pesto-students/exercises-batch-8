d5b5e837dd3faba4d1149f073881e34fc6c0ee70
Abhinav Sharma <abhi18av@outlook.com>
2019-06-15 00:34:03 +0530

    solved the math-fns problem using method-1

--- math-fns/mathFns.js
+++ math-fns/mathFns.js
@@ -3,6 +3,29 @@ function mathFns(...args) {
   return args;
 }
 
+// Method-1 Wrapping the standard library
+function sqrt(...args) {
+  const aNumber = args[0];
+  return Math.sqrt(aNumber);
+}
+
+function power(...args) {
+  const aNumber = args[0];
+  const exponent = args[1];
+  return aNumber ** exponent;
+}
+
+
+function round(...args) {
+  const aNumber = args[0];
+  return Math.round(aNumber);
+}
+
+// TODO Method-2
+
 export {
   mathFns,
+  sqrt,
+  power,
+  round,
 };
