f89410e59eac5918356b537c9202af4404c77e3d
Kanav Arora <kanav_arora@statusbrew.com>
2019-06-14 16:36:47 +0530

    Solved Orbital Period

--- orbital-period/orbitalPeriod.js
+++ orbital-period/orbitalPeriod.js
@@ -1,6 +1,18 @@
+const calculatePeriod = (averageAltitude) => {
+  const GM = 398600.4418; // in km3s-2
+  const earthRadius = 6367.4447; // in km
+  const distanceFromCore = earthRadius + averageAltitude;
 
-function orbitalPeriod(...args) {
-  return args;
+  return (2 * Math.PI * (distanceFromCore ** 1.5)) / (GM ** 0.5);
+};
+
+function orbitalPeriod(spaceObjects) {
+  return spaceObjects.map((spaceObject) => {
+    return {
+      name: spaceObject.name,
+      orbitalPeriod: Math.round(calculatePeriod(spaceObject.avgAlt)),
+    };
+  });
 }
 
 export {
