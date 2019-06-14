3666ff42683c54e67fbacd7c58a87fcc7619c57f
Rakesh Tembhurne <tj2point0@gmail.com>
2019-06-15 00:58:55 +0530

    Complete maths-fns

--- math-fns/mathFns.js
+++ math-fns/mathFns.js
@@ -1,3 +1,8 @@
+const sqrt = number => Math.sqrt(number);
+
+const power = (number, exponent) => number ** exponent;
+
+const round = number => Math.round(number);
 
 function mathFns(...args) {
   return args;
@@ -5,4 +10,7 @@ function mathFns(...args) {
 
 export {
   mathFns,
+  sqrt,
+  power,
+  round,
 };
