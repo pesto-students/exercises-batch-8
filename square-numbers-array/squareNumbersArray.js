cdb0f1d5bc457fc0abbe198442f0f57beab624e2
Pradyumna Newalkar <contactpradyumna1@gmail.com>
2019-06-14 01:20:12 +0000

    Added square-numbers-array

--- square-numbers-array/squareNumbersArray.js
+++ square-numbers-array/squareNumbersArray.js
@@ -1,6 +1,11 @@
 
-function squareNumbersArray(...args) {
-  return args;
+function squareNumbersArray(arr) {
+  return arr.map((item) => {
+    if (typeof item === 'number') {
+      return item ** 2;
+    }
+    throw new Error(`Expected number, found ${typeof item}`);
+  });
 }
 
 export {
--- square-numbers-array/squareNumbersArray.test.js
+++ square-numbers-array/squareNumbersArray.test.js
@@ -3,9 +3,12 @@ import { squareNumbersArray } from './squareNumbersArray';
 describe('squareNumbersArray', () => {
   test('should throw an error if array of numbers is not passed as argument', () => {
     const myArray = [1, 2, 'string', 4];
-    expect(() => squareNumbersArray(...myArray)).toThrow('My custom error');
+    expect(() => {
+      squareNumbersArray(myArray);
+    }).toThrowError();
   });
   test('should return square array', () => {
-    
+    const arr = [1, 5, 6];
+    expect(squareNumbersArray(arr)).toStrictEqual([1, 25, 36]);
   });
 });
