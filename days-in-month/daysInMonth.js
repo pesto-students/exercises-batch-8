bcedb31fbd87f1dcb703a1c7990e1410b62bb1ce
Pradyumna Newalkar <contactpradyumna1@gmail.com>
2019-06-14 20:06:06 +0000

    Added daysInMonth

--- days-in-month/daysInMonth.js
+++ days-in-month/daysInMonth.js
@@ -1,6 +1,17 @@
+function isLeap(year) {
+  return (year % 100 !== 0 && year % 4 === 0)
+    || (year % 100 === 0 && year % 400 === 0);
+}
 
-function daysInMonth(...args) {
-  return args;
+function daysInMonth(year, month) {
+  if ((month <= 6 && month % 2 === 1)
+    || (month > 6 && month % 2 === 0)) {
+    return 31;
+  }
+  if (month === 2) {
+    return isLeap(year) ? 29 : 28;
+  }
+  return 30;
 }
 
 export {
