930f0db8ae4a83e5f1a6256cc074322f140b1d5d
Rakesh Tembhurne <tj2point0@gmail.com>
2019-06-15 00:50:31 +0530

    Complete throw-errors

--- throw-errors/throwErrors.js
+++ throw-errors/throwErrors.js
@@ -1,8 +1,22 @@
 
-function throwErrors(...args) {
-  return args;
+function throwErrors() {
+  return variableThatDoesNotExists;
 }
+
+function invokeErrors() {
+  let nameOfError = '';
+  try {
+    throwErrors();
+  } catch (e) {
+    nameOfError = e.name;
+    return e.name;
+  }
+  return nameOfError;
+}
+
+const errorName = invokeErrors();
 
 export {
   throwErrors,
+  errorName,
 };
