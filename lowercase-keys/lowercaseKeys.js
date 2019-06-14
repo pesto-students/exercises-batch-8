78579ce127af3718474b10f6801e8027f8856c83
Kanav Arora <kanav_arora@statusbrew.com>
2019-06-15 00:43:42 +0530

    Solve Lower Case keys

--- lowercase-keys/lowercaseKeys.js
+++ lowercase-keys/lowercaseKeys.js
@@ -1,6 +1,10 @@
 
-function lowercaseKeys(...args) {
-  return args;
+function lowercaseKeys(object) {
+  return Object.keys(object).reduce((result, key) => {
+    const lowercaseKey = key.toLowerCase();
+    result[lowercaseKey] = object[key];
+    return result;
+  }, {});
 }
 
 export {
