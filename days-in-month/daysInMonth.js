0f34534ba48591487a16c42f47b70c6318a38cfa
Radheshyam Etikala <radhey1851@gmail.com>
2019-06-15 00:30:17 +0530

    Completed daysInMonth

--- days-in-month/daysInMonth.js
+++ days-in-month/daysInMonth.js
@@ -1,6 +1,20 @@
+function isLeapYear(year) {
+  return ((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0);
+}
 
-function daysInMonth(...args) {
-  return args;
+function isEven(number) {
+  return number % 2 === 0;
+}
+
+function daysInMonth(year, month) {
+  if (month === 2) { // is February
+    return isLeapYear(year) ? 29 : 28;
+  }
+
+  if (isEven(month)) {
+    return 30;
+  }
+  return 31;
 }
 
 export {
