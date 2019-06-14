716f8f8f4e0e43432daea9af521d3e15e01e1703
sourabhmodi20051 <sourabhmodi20051@gmail.com>
2019-06-15 00:56:52 +0530

    Lowercase Keys solved

--- lowercase-keys/lowercaseKeys.js
+++ lowercase-keys/lowercaseKeys.js
@@ -1,6 +1,8 @@
 
-function lowercaseKeys(...args) {
-  return args;
+function lowercaseKeys(inputObject) {
+  const lowerCaseKeysObject = Object.keys(inputObject)
+    .reduce((acc, key) => ({ ...acc, [key.toLowerCase()]: inputObject[key] }), {});
+  return lowerCaseKeysObject;
 }
 
 export {
