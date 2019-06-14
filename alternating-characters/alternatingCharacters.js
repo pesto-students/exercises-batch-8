573023e773fbcf264bf4687f81d73ff92edbd9a5
kunnu01 <kunalbhatia2409@gmail.com>
2019-06-14 15:51:37 +0530

    Solved alternating-characters

--- alternating-characters/alternatingCharacters.js
+++ alternating-characters/alternatingCharacters.js
@@ -1,6 +1,19 @@
+/* eslint-disable array-callback-return */
 
-function alternatingCharacters(...args) {
-  return args;
+function getNumberOfDeletions(string) {
+  let timesToDelete = 0;
+  for (let i = 1; i < string.length; i += 1) {
+    if (string.charAt(i) === string.charAt(i - 1)) {
+      string.slice(i, i + 1);
+      timesToDelete += 1;
+    }
+  }
+  return timesToDelete;
+}
+
+function alternatingCharacters(stringArray) {
+  const numberOfDeletionsArray = stringArray.map(string => getNumberOfDeletions(string));
+  return numberOfDeletionsArray;
 }
 
 export {
