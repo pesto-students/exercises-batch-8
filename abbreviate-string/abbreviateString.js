e483230e326afc7c304c2ac9f2e10efaaffa2cb2
Radheshyam Etikala <radhey1851@gmail.com>
2019-06-15 00:11:27 +0530

    Completed abbreviateString

--- abbreviate-string/abbreviateString.js
+++ abbreviate-string/abbreviateString.js
@@ -1,6 +1,12 @@
 
-function abbreviateString(...args) {
-  return args;
+function abbreviateString(name) {
+  if (typeof name !== 'string') {
+    throw new Error('Invalid parameters.');
+  }
+
+  const names = name.split(' ');
+  const [firstName, lastName] = [names[0], names[names.length - 1]];
+  return `${firstName} ${lastName[0].toUpperCase()}.`;
 }
 
 export {
