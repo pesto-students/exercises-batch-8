09273eaa9e997510a82accad34e16c3bf5c15498
Abdul Khuddus <khuddus1@gmail.com>
2019-06-15 05:02:33 +0530

    Complete time was

--- time-was/timeWas.js
+++ time-was/timeWas.js
@@ -1,5 +1,5 @@
 
-function timeWas(givenTime) {
+function timeWas(givenTime, now) {
   const NOW = 1516816290;
   const SECONDS = 1000;
   const MINUTES = SECONDS * 60;
@@ -8,21 +8,30 @@ function timeWas(givenTime) {
   const WEEKS = DAYS * 7;
   const MONTHS = DAYS * 30;
   const YEARS = MONTHS * 12;
-  const passedMilliSeconds = NOW - givenTime;
-  if (Math.round(passedMilliSeconds / YEARS) > 0) {
-    return `${Math.round(passedMilliSeconds / YEARS)} years ago`;
-  } if (Math.round(passedMilliSeconds / MONTHS) > 0) {
-    return `${Math.round(passedMilliSeconds / MONTHS)} months ago`;
-  } if (Math.round(passedMilliSeconds / WEEKS) > 0) {
-    return `${Math.round(passedMilliSeconds / WEEKS)} weeks ago`;
-  } if (Math.round(passedMilliSeconds / DAYS) > 0) {
-    return `${Math.round(passedMilliSeconds / DAYS)} days ago`;
-  } if (Math.round(passedMilliSeconds / HOURS) > 0) {
-    return `${Math.round(passedMilliSeconds / HOURS)} hours ago`;
-  } if (Math.round(passedMilliSeconds / MINUTES) > 0) {
-    return `${Math.round(passedMilliSeconds / MINUTES)} minutes ago`;
-  } if (Math.round(passedMilliSeconds / SECONDS) > 0) {
-    return `${Math.round(passedMilliSeconds / SECONDS)} seconds ago`;
+  const passedMilliSeconds = Number.isInteger(now) ? (now - givenTime) : (NOW - givenTime);
+  if (Math.floor(passedMilliSeconds / YEARS) > 0) {
+    const years = Math.floor(passedMilliSeconds / YEARS);
+    return `${years} year${years > 1 ? 's' : ''} ago`;
+  } if (Math.floor(passedMilliSeconds / MONTHS) > 0) {
+    const months = Math.floor(passedMilliSeconds / MONTHS);
+    return `${months} month${months > 1 ? 's' : ''} ago`;
+  } if (Math.floor(passedMilliSeconds / WEEKS) > 0) {
+    const weeks = Math.floor(passedMilliSeconds / WEEKS);
+    return `${weeks} week${weeks > 1 ? 's' : ''} ago`;
+  } if (Math.floor(passedMilliSeconds / DAYS) > 0) {
+    const days = Math.floor(passedMilliSeconds / DAYS);
+    return `${days} day${days > 1 ? 's' : ''} ago`;
+  } if (Math.floor(passedMilliSeconds / HOURS) > 0) {
+    const hours = Math.floor(passedMilliSeconds / HOURS);
+    return `${hours} hour${hours > 1 ? 's' : ''} ago`;
+  } if (Math.floor(passedMilliSeconds / MINUTES) > 0) {
+    const minutes = Math.floor(passedMilliSeconds / MINUTES);
+    return `${minutes} minute${minutes > 1 ? 's' : ''} ago`;
+  } if (Math.floor(passedMilliSeconds / SECONDS) > 0) {
+    const seconds = Math.floor(passedMilliSeconds / SECONDS);
+    return `${seconds} second${seconds > 1 ? 's' : ''} ago`;
+  } if (passedMilliSeconds >= 0 && passedMilliSeconds < 5) {
+    return 'just now';
   }
   return '';
 }
