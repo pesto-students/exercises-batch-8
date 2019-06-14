919f32daf71996c62e89c9a231db067099cb8abd
kunnu01 <kunalbhatia2409@gmail.com>
2019-06-14 15:28:34 +0530

    Solved remaining subparts of accountDatasets

--- account-dataset/accountDataset.js
+++ account-dataset/accountDataset.js
@@ -11,7 +11,7 @@ function datasetWithRoundedDollar() {
     const { amount, ...rest } = account;
     return {
       amount,
-      rounded: Math.floor(amount),
+      rounded: Math.round(amount),
       ...rest,
     };
   });
@@ -22,21 +22,47 @@ function sumOfBankBalances() {
   return Math.round(sum * 100) / 100;
 }
 
-// function sumOfInterests() {
-//   const interestPercebtage = 18.9;
-//   const states = ['WI', 'IL', 'WY', 'OH', 'GA', 'DE'];
+function sumOfInterests() {
+  const interestPercentage = 18.9;
+  const states = ['WI', 'IL', 'WY', 'OH', 'GA', 'DE'];
 
-//   const accountsWithSelectedStates = bankBalances
-//     .filter(account => states.includes(account.state));
+  const accountsWithSelectedStates = bankBalances
+    .filter(account => states.includes(account.state));
 
-//   const totalInterest = accountsWithSelectedStates
-//     .reduce((total, { amount }) => {
+  const totalInterest = accountsWithSelectedStates
+    .reduce((total, { amount }) => {
+      const interestAmount = parseFloat((amount * interestPercentage) / 100);
+      const amountWithInterest = parseFloat(interestAmount + amount).toFixed(2);
 
-//     })
-// }
+      return total + parseFloat(amountWithInterest);
+    }, 0);
+  return parseFloat(totalInterest.toFixed(2));
+}
+
+function higherStateSums() {
+  const thresholdAmount = 1000000;
+  const states = {};
+  bankBalances.forEach((account) => {
+    const { state } = account;
+    const amount = parseFloat(account.amount);
+    if (!states[state]) {
+      states[state] = amount;
+    } else {
+      states[state] += amount;
+    }
+  });
+
+  const statesCrossingThresholdAmount = Object.values(states)
+    .filter(amount => amount > thresholdAmount);
+
+  return statesCrossingThresholdAmount
+    .reduce((sum, amount) => sum + amount, 0);
+}
 
 export {
   hundredThousandairs,
   datasetWithRoundedDollar,
   sumOfBankBalances,
+  sumOfInterests,
+  higherStateSums,
 };
