f5766c3622d135653c1ee7d5a5e23780ede60a75
Rakesh Tembhurne <tj2point0@gmail.com>
2019-06-15 01:41:59 +0530

    Complete abbreviate-string

--- abbreviate-string/abbreviateString.js
+++ abbreviate-string/abbreviateString.js
@@ -1,6 +1,38 @@
 
-function abbreviateString(...args) {
-  return args;
+function abbreviateString(stringToCheck) {
+  if (typeof stringToCheck === 'string') {
+    let nameToReturn = '';
+    const listOfWords = stringToCheck.split(' ');
+
+    if (listOfWords.length === 1) {
+      nameToReturn = listOfWords.slice(0, 1);
+    } else {
+      const firstWord = listOfWords.shift();
+      const lastWord = listOfWords.pop();
+
+      let lastWordShortHand = lastWord.charAt(0);
+      lastWordShortHand = lastWordShortHand.toUpperCase();
+      lastWordShortHand += '.';
+
+      nameToReturn = `${firstWord} ${lastWordShortHand}`;
+    }
+
+    return nameToReturn;
+  }
+
+  let errorMessage = "The input data should by of type 'string'.";
+
+  if (typeof stringToCheck === 'number') {
+    errorMessage = 'Input parameter of type number is not allowed.';
+  } else if (Array.isArray(stringToCheck)) {
+    errorMessage = 'Input parameter of type array is not allowed.';
+  } else if (stringToCheck === null) {
+    errorMessage = 'Input parameter of type null is not allowed.';
+  } else if (typeof stringToCheck === 'object') {
+    errorMessage = 'Input parameter of type object is not allowed.';
+  }
+
+  throw new Error(errorMessage);
 }
 
 export {
