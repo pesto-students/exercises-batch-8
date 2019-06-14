6ef11884da05b843ec528f145943a86a3adb11e3
Rakesh Tembhurne <tj2point0@gmail.com>
2019-06-15 00:20:24 +0530

    Unfinished sum-primes

--- sum-primes/sumPrimes.js
+++ sum-primes/sumPrimes.js
@@ -1,6 +1,20 @@
+const isPrime = (number) => {
+  for (let index = 2; index < number; index += 1) {
+    if (number % index === 0) {
+      return false;
+    }
+  }
+  return true;
+};
 
-function sumPrimes(...args) {
-  return args;
+function sumPrimes(number) {
+  const numberRange = [...Array(number).keys()];
+  numberRange.splice(0, 2);
+
+  const primesWithinRange = numberRange.filter(isPrime);
+  const sum = primesWithinRange.reduce((num, prevNum) => num + prevNum);
+
+  return sum;
 }
 
 export {
