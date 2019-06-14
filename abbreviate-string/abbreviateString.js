db80aa13b810e63bb472ddd2848a64a0d0dda77f
Pradyumna Newalkar <contactpradyumna1@gmail.com>
2019-06-14 20:25:38 +0000

    Added abbreviateString

--- abbreviate-string/abbreviateString.js
+++ abbreviate-string/abbreviateString.js
@@ -1,6 +1,12 @@
 
-function abbreviateString(...args) {
-  return args;
+function abbreviateString(str) {
+  if (typeof str !== 'string') {
+    throw TypeError;
+  }
+  const mainTokens = str
+    .split(' ')
+    .filter((token, index, arr) => index === 0 || index === arr.length - 1);
+  return `${mainTokens[0]} ${mainTokens[1].toUpperCase()[0]}.`;
 }
 
 export {
