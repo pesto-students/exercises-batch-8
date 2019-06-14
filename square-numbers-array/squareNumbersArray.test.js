94670b9b48360037e3835f6508fb436fc1ad6c74
sourabhmodi20051 <sourabhmodi20051@gmail.com>
2019-06-14 10:59:51 +0530

    Solved squared numbers problem

--- square-numbers-array/squareNumbersArray.test.js
+++ square-numbers-array/squareNumbersArray.test.js
@@ -3,9 +3,10 @@ import { squareNumbersArray } from './squareNumbersArray';
 describe('squareNumbersArray', () => {
   test('should throw an error if array of numbers is not passed as argument', () => {
     const myArray = [1, 2, 'string', 4];
-    expect(() => squareNumbersArray(...myArray)).toThrow('My custom error');
+    expect(() => squareNumbersArray(...myArray)).toThrow('List should contain numbers only');
   });
   test('should return square array', () => {
-    
+    const myArray = [1, 2, 4];
+    expect(squareNumbersArray(...myArray)).toMatchObject([1, 4, 16]);
   });
 });
