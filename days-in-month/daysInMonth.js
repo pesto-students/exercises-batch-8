d930a512014b353a126d812410c0c5a442ea6d35
Sebin Benjamin <sebinbenjamin@gmail.com>
2019-06-15 01:40:53 +0530

    completed exercise 'days-in-month'

--- days-in-month/daysInMonth.js
+++ days-in-month/daysInMonth.js
@@ -1,6 +1,7 @@
 
-function daysInMonth(...args) {
-  return args;
+function daysInMonth(year, month) {
+  const monthYearRepresentation = new Date(year, month, 0);
+  return monthYearRepresentation.getDate();
 }
 
 export {
