1715ce3786fd6e8c92bf24b0c1a0c4f32a2195d3
Abhinav Sharma <abhi18av@outlook.com>
2019-06-14 19:34:21 +0530

    solved curry problem

--- curry/curry.js
+++ curry/curry.js
@@ -1,11 +1,15 @@
 
-function curry(...args) {
-    return args => {
-        return  args[0];
-    };
+function curry(func) {
+  return function curried (...args1) {
+    if (args1.length >= func.length) {
+      return func.apply(this, args1);
+    } else {
+      return function (...args2) {
+        return curried.apply(this, args1.concat(args2));
+      };
+    }
+  };
 }
-
-
 
 export {
   curry,
