ef91aeadda8b8af7c9c498ea9c721d7eb45b42fd
Abdul Khuddus <khuddus1@gmail.com>
2019-06-15 01:50:27 +0530

    Complete parse MS

--- parse-ms/parseMs.js
+++ parse-ms/parseMs.js
@@ -1,6 +1,23 @@
 
-function parseMs(...args) {
-  return args;
+function parseMs(ms) {
+  if (ms > 0) {
+    const milliseconds = ms;
+    return {
+      days: Math.floor(milliseconds / (24 * 60 * 60 * 1000)),
+      hours: Math.floor(milliseconds / (60 * 60 * 1000)) % 24,
+      minutes: Math.floor(milliseconds / (60 * 1000)) % 60,
+      seconds: Math.floor(milliseconds / 1000) % 60,
+      milliseconds: milliseconds % 1000,
+    };
+  }
+  const milliseconds = 0 - ms;
+  return {
+    days: -Math.floor(milliseconds / (24 * 60 * 60 * 1000)),
+    hours: -(Math.floor(milliseconds / (60 * 60 * 1000)) % 24),
+    minutes: -(Math.floor(milliseconds / (60 * 1000)) % 60),
+    seconds: -(Math.floor(milliseconds / 1000) % 60),
+    milliseconds: -(milliseconds % 1000),
+  };
 }
 
 export {
