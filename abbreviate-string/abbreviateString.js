6ae2772b185728319b0b9a013072421bc063519e
Sebin Benjamin <sebinbenjamin@gmail.com>
2019-06-15 00:27:14 +0530

    completed exercise 'abbreviate-string'

--- abbreviate-string/abbreviateString.js
+++ abbreviate-string/abbreviateString.js
@@ -1,6 +1,14 @@
+const abbreviateWord = word => `${word[0].toUpperCase()}.`;
 
-function abbreviateString(...args) {
-  return args;
+function abbreviateString(string) {
+  if (typeof string === 'string' || string instanceof String) {
+    const wordInString = string.split(' ');
+    const firstWord = wordInString[0];
+    const lastWord = wordInString[wordInString.length - 1];
+    const abbreviatedLasrWord = abbreviateWord(lastWord);
+    return `${firstWord} ${abbreviatedLasrWord}`;
+  }
+  throw Error('Invalid parameters !');
 }
 
 export {
