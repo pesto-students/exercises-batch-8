ed7b1959f6450f7c8791b8910b9f01e5f70d8c3e
prateekinstillion <Prateek Madaan>
2019-06-15 00:39:39 +0530

    Solved the loweracse keys problem

--- days-in-month/daysInMonth.js
+++ days-in-month/daysInMonth.js
@@ -1,23 +1,6 @@
 
-const YEAR = {
-  JAN: 31,
-  FEB: 28,
-  MAR: 31,
-  APR: 30,
-  MAY: 31,
-  JUNE: 30,
-  JULY: 31,
-  AUG: 31,
-  SEP: 30,
-  OCT: 31,
-  NOV: 30,
-  DEC: 31,
-};
-
-function daysInMonth(year, month) {
-    const isLeapYear = year % 4 === 0;
-    const isFebruary = month => month === YEAR.FEB;
-    return isLeapYear && isFebruary ? 29 : normalYear[month];
+function daysInMonth(...args) {
+  return args;
 }
 
 export {
--- lowercase-keys/lowercaseKeys.js
+++ lowercase-keys/lowercaseKeys.js
@@ -1,6 +1,8 @@
 
-function lowercaseKeys(...args) {
-  return args;
+function lowercaseKeys(object) {
+  return Object.fromEntries(
+    Object.entries(object).map(([key, value]) => [key.toLowerCase(), value]),
+  );
 }
 
 export {
