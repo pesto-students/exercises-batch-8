9558344dd9b14e415bd19d35d6e80ad23f703b78
Kanav Arora <kanav_arora@statusbrew.com>
2019-06-15 00:05:09 +0530

    Completed Abbreviate String

--- abbreviate-string/abbreviateString.js
+++ abbreviate-string/abbreviateString.js
@@ -1,6 +1,15 @@
+const firstWord = string => string.slice(0, string.indexOf(' '));
 
-function abbreviateString(...args) {
-  return args;
+const lastWord = string => string.slice(string.lastIndexOf(' '), (string.lastIndexOf(' ') + 2));
+
+const lastWordInitial = string => lastWord(string).toUpperCase();
+
+function abbreviateString(string) {
+  if (typeof string === 'string') {
+    return firstWord(string) + lastWordInitial(string) + '.';
+  } else {
+    throw new Error('The function only supports Strings')
+  }
 }
 
 export {
