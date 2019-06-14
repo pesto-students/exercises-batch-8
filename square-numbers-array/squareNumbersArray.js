d361c378323855e98e511d09a905a5860b6f1434
saurabh shetty <saurabh.shetty100@gmail.com>
2019-06-14 20:33:17 +0530

    Memoize

--- square-numbers-array/squareNumbersArray.js
+++ square-numbers-array/squareNumbersArray.js
@@ -1,7 +1,7 @@
 
 function squareNumbersArray(...array) {
   const squareArray = array.map((value) => {
-    if (!Number(value)) {
+    if (Number.isNaN(value ** 2)) {
       throw Error('My custom error');
     }
     return value ** 2;
