6d2e1d0abbe07a1c791eaec026880cae6eeda5bf
Kanav Arora <kanav_arora@statusbrew.com>
2019-06-15 00:48:13 +0530

    Solved MathFns

--- math-fns/mathFns.js
+++ math-fns/mathFns.js
@@ -3,6 +3,21 @@ function mathFns(...args) {
   return args;
 }
 
+function sqrt(value) {
+  return Math.sqrt(value);
+} 
+
+function power(value, pow) {
+  return Math.pow(value, pow);
+} 
+
+function round(value) {
+  return Math.round(value);
+} 
+
 export {
   mathFns,
+  sqrt,
+  power,
+  round,
 };
