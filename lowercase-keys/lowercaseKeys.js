138b01112d292766fc585359d12e9cf63f0325d8
Sebin Benjamin <sebinbenjamin@gmail.com>
2019-06-15 02:00:01 +0530

    completed exercise 'lowercase-keys'

--- lowercase-keys/lowercaseKeys.js
+++ lowercase-keys/lowercaseKeys.js
@@ -1,6 +1,11 @@
 
-function lowercaseKeys(...args) {
-  return args;
+function lowercaseKeys(obj) {
+  const lowerCaseKeyedObject = {};
+  Object.entries(obj).forEach(([key, value]) => {
+    const lowerCaseKey = key.toString().toLowerCase();
+    lowerCaseKeyedObject[lowerCaseKey] = value;
+  });
+  return lowerCaseKeyedObject;
 }
 
 export {
