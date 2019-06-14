4784823c935bcedc7e0c9795ace7203a0fdbc4bb
sourabhmodi20051 <sourabhmodi20051@gmail.com>
2019-06-15 00:43:40 +0530

    Solved days in month

--- days-in-month/daysInMonth.js
+++ days-in-month/daysInMonth.js
@@ -1,6 +1,36 @@
+const daysInMonthLookup = {
+  1: 31,
+  3: 31,
+  4: 30,
+  5: 31,
+  6: 30,
+  7: 31,
+  8: 31,
+  9: 30,
+  10: 31,
+  11: 30,
+  12: 31,
+};
 
-function daysInMonth(...args) {
-  return args;
+function isLeapYear(year) {
+  if (year % 100 === 0) {
+    return year % 400 === 0;
+  }
+  return year % 4 === 0;
+}
+
+function isFeb(month) {
+  return month === 2;
+}
+
+function daysInMonth(year, month) {
+  if (isFeb(month)) {
+    if (isLeapYear(year)) {
+      return 29;
+    }
+    return 28;
+  }
+  return daysInMonthLookup[month];
 }
 
 export {
