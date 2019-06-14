fc864958fbc53360472e60a597e9f7a518622e17
Abdul Khuddus <khuddus1@gmail.com>
2019-06-15 00:47:57 +0530

    Complete lowercase keys

--- round-to/roundTo.js
+++ round-to/roundTo.js
@@ -1,6 +1,19 @@
-
-function roundTo(...args) {
-  return args;
+function roundTo(num, precision) {
+  this.up = (number, precisionDigits) => {
+    const multiplier = 10 ** precisionDigits;
+    const multipliedNumber = number * multiplier;
+    const isNumberUp = multipliedNumber > Math.floor(multipliedNumber);
+    return isNumberUp ? (multipliedNumber + 1) / multiplier : multipliedNumber / multiplier;
+  };
+  this.down = (number, precisionDigits) => {
+    const multiplier = 10 ** precisionDigits;
+    const multipliedNumber = number * multiplier;
+    return multipliedNumber / multiplier;
+  };
+  const multiplier = 10 ** precision;
+  const multipliedNumber = num * multiplier;
+  const isNumberUp = multipliedNumber > Math.floor(multipliedNumber);
+  return isNumberUp ? (multipliedNumber + 1) / multiplier : multipliedNumber / multiplier;
 }
 
 export {
