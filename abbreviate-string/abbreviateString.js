7dd7963ab899cf33ab5014033aa7c81571262152
saurabh shetty <saurabh.shetty100@gmail.com>
2019-06-15 00:01:10 +0530

    SOlved AbbreviateString

--- abbreviate-string/abbreviateString.js
+++ abbreviate-string/abbreviateString.js
@@ -1,6 +1,7 @@
 
-function abbreviateString(...args) {
-  return args;
+function abbreviateString(string) {
+  const splittedString = string.split(' ');
+  return `${splittedString[0]} ${splittedString[splittedString.length - 1][0].toUpperCase()}.`;
 }
 
 export {
