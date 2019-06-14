4d3ab0778e8bbd4f3303b3e0339ac202bc659d2f
Abhinav Sharma <abhi18av@outlook.com>
2019-06-15 00:16:56 +0530

    solved the throw-error problem

--- throw-errors/throwErrors.js
+++ throw-errors/throwErrors.js
@@ -1,8 +1,14 @@
 
-function throwErrors(...args) {
-  return args;
+function errorName(...args) {
+try {
+undefinedName;
+} catch (e) {
+if (e instanceof ReferenceError) {
+    return e.name;
+    } 
+  } 
 }
 
 export {
-  throwErrors,
+  throwError,
 };
