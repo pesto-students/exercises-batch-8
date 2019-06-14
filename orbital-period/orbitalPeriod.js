c3be1ed4f81318f99be3221dced416f0b37613e3
kunnu01 <kunalbhatia2409@gmail.com>
2019-06-14 17:15:18 +0530

    Solved orbital-period

--- orbital-period/orbitalPeriod.js
+++ orbital-period/orbitalPeriod.js
@@ -1,6 +1,21 @@
+const GM = 398600.4418;
+const earthRadius = 6367.4447;
 
-function orbitalPeriod(...args) {
-  return args;
+function getOrbitalPeriod(orbitSize) {
+  return Math.round(
+    2 * Math.PI * Math.sqrt((orbitSize ** 3) / GM),
+  );
+}
+
+function orbitalPeriod(satellitesDetails) {
+  return satellitesDetails.map((satellite) => {
+    const sizeOfOrbit = satellite.avgAlt + earthRadius;
+
+    return {
+      name: satellite.name,
+      orbitalPeriod: getOrbitalPeriod(sizeOfOrbit),
+    };
+  });
 }
 
 export {
