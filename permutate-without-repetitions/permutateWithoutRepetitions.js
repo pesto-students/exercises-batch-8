7ae387eee7bc2735b8d04b02ce49a55fe33e84d8
aayushtiwari <arsenicaayush@gmail.com>
2019-06-16 05:24:43 +0530

    completed

--- permutate-without-repetitions/permutateWithoutRepetitions.js
+++ permutate-without-repetitions/permutateWithoutRepetitions.js
@@ -1,8 +1,17 @@
-
-function permutateWithoutRepetitions(...args) {
-  return args;
+function permutateWithoutRepetitions([...args]) {
+  return args.reduce(permute, []);
 }
 
-export {
-  permutateWithoutRepetitions,
-};
+function permute(res, item, key, arr) {
+  return res.concat(
+    (arr.length > 1
+      && arr
+        .slice(0, key)
+        .concat(arr.slice(key + 1))
+        .reduce(permute, [])
+        .map(perm => [item].concat(perm)))
+      || item,
+  );
+}
+
+export { permutateWithoutRepetitions };
