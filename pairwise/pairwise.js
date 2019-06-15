f5d176dc96df8e0eeee61fd0cdbef3c2f35f8225
sourabhmodi20051 <sourabhmodi20051@gmail.com>
2019-06-16 02:57:45 +0530

    Solved Pairwise

--- pairwise/pairwise.js
+++ pairwise/pairwise.js
@@ -1,21 +1,37 @@
 function pairwise(array, pairSum) {
-  const indexLookupMap = array.reduce((acc, element, index) => {
-    return { ...acc, [element]: index };
-  }, {})
-
-  const pairIndexSum = array.reduce((acc, element, index) => {
-    const pairedItem = pairSum - element
-    if (indexLookupMap[pairedItem] != undefined && indexLookupMap[pairedItem] != index) {
-      return acc + index + indexLookupMap[pairedItem]
-    } else {
-      return acc
+  const indexLookup = array.reduce(
+    (acc, item, index) => ({
+      ...acc,
+      [item]: acc[item] === undefined ? [index] : [...acc[item], index],
+    }),
+    {},
+  );
+  function recursivePairwise(sum, index) {
+    if (array.length === 0) {
+      return sum;
     }
-  }, 0);
-
-  // Every pair is counted twice
-  return pairIndexSum / 2;
+    const element = array[0];
+    const pair = pairSum - element;
+    if (
+      indexLookup[element] !== undefined
+      && indexLookup[element].length > 0
+      && indexLookup[pair] !== undefined
+      && indexLookup[pair].length > 0
+    ) {
+      const pairIndex = indexLookup[pair][0];
+      if (index !== pairIndex) {
+        array.shift();
+        indexLookup[pair].shift();
+        indexLookup[element].shift();
+        return recursivePairwise(sum + index + pairIndex, index + 1);
+      }
+      indexLookup[pair].shift();
+      return recursivePairwise(sum, index);
+    }
+    array.shift();
+    return recursivePairwise(sum, index + 1);
+  }
+  return recursivePairwise(0, 0);
 }
 
-export {
-  pairwise,
-};
+export { pairwise };
