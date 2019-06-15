ca5bb12e14f415af8cb2f4d5a16efde8e8b5aedb
saurabh shetty <saurabh.shetty100@gmail.com>
2019-06-15 09:37:24 +0530

    solved updateInventory

--- update-inventory/updateInventory.js
+++ update-inventory/updateInventory.js
@@ -1,6 +1,26 @@
+const convertArrayToObject = (array) => {
+  const object = {};
+  array.map((item) => {
+    // eslint-disable-next-line prefer-destructuring
+    object[item[1]] = item[0];
+    return null;
+  });
+  return object;
+};
+const convertObjectToArray = object => Object.keys(object).map(key => [object[key], key]);
 
-function updateInventory(...args) {
-  return args;
+function updateInventory(initialInventory, newItems) {
+  const inventoryObject = convertArrayToObject(initialInventory);
+  const newItemsObject = convertArrayToObject(newItems);
+
+  Object.keys(newItemsObject).map((key) => {
+    if (!inventoryObject[key]) {
+      inventoryObject[key] = 0;
+    }
+    inventoryObject[key] += newItemsObject[key];
+    return null;
+  });
+  return convertObjectToArray(inventoryObject).sort((a, b) => a[1] > b[1]);
 }
 
 export {
