ed2de2835464cec2234b94b8e2ea77e55c8a2dae
Pradyumna Newalkar <contactpradyumna1@gmail.com>
2019-06-15 21:11:13 +0000

    Added solution for sum-fibs

--- sum-fibs/sumFibs.js
+++ sum-fibs/sumFibs.js
@@ -1,6 +1,21 @@
+function isPerfectSquare(num) {
+  return Math.floor(Math.sqrt(num)) ** 2 === num;
+}
 
-function sumFibs(...args) {
-  return args;
+function isFibonacci(num) {
+  return isPerfectSquare(5 * num * num + 4)
+    || isPerfectSquare(5 * num * num - 4);
+}
+
+function range(n) {
+  return [...Array(n).keys()].map(x => x + 1);
+}
+
+function sumFibs(num) {
+  return 1 + range(num).reduce(
+    (sum, number) => (number % 2 === 1 && isFibonacci(number) ? sum + number : sum),
+    0,
+  );
 }
 
 export {
