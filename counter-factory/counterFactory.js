7069cbb53d7eab0ee69fa83be4ceb404334736ba
Pradyumna Newalkar <contactpradyumna1@gmail.com>
2019-06-15 19:38:22 +0000

    Added solution for counter-factory

--- counter-factory/counterFactory.js
+++ counter-factory/counterFactory.js
@@ -1,6 +1,19 @@
 
-function counterFactory(...args) {
-  return args;
+function counterFactory() {
+  function Counter() {
+    this.count = 0;
+  }
+
+  Counter.prototype.increment = function () {
+    this.count += 1;
+    return this.count;
+  };
+
+  Counter.prototype.decrement = function () {
+    this.count -= 1;
+    return this.count;
+  };
+  return new Counter();
 }
 
 export {
