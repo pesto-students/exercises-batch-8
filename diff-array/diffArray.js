87c7cee0dc795c8a339864018efd89cc7baf8391
Radheshyam Etikala <radhey1851@gmail.com>
2019-06-16 03:00:23 +0530

    Completed diffArr

--- diff-array/diffArray.js
+++ diff-array/diffArray.js
@@ -1,6 +1,8 @@
 
-function diffArray(...args) {
-  return args;
+function diffArray(arrA, arrB) {
+  const uniqueInA = arrA.filter(el => !arrB.includes(el));
+  const uniqueInB = arrB.filter(el => !arrA.includes(el));
+  return uniqueInA.concat(uniqueInB);
 }
 
 export {
