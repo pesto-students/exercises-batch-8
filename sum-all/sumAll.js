aee5deab14735e289f4fa9cf86131832e43400b8
Abhinav Sharma <abhi18av@outlook.com>
2019-06-16 02:36:32 +0530

    completed sum-all problem

--- sum-all/sumAll.js
+++ sum-all/sumAll.js
@@ -1,6 +1,18 @@
 
 function sumAll(...args) {
-  return args;
+  const num1 = args[0][0];
+  const num2 = args[0][1];
+  let sum = 0;
+  if (num2 >= num1) {
+    for (let i = num1; i <= num2; i++) {
+      sum += i;
+    }
+  } else {
+    for (let i = num1; i >= num2; i--) {
+      sum += i;
+    }
+  }
+  return sum;
 }
 
 export {
