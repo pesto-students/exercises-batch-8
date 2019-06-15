0eed8d676d611b675dd58814f93650a15e194915
Rakesh Tembhurne <tj2point0@gmail.com>
2019-06-16 03:16:22 +0530

    Unfinished sum-of-perfect-squares

--- sum-of-perfect-squares/sumOfPerfectSquares.js
+++ sum-of-perfect-squares/sumOfPerfectSquares.js
@@ -1,6 +1,25 @@
+const isSquare = n => n > 0 && Math.sqrt(n) % 1 === 0;
+const range = (min, max) => [...Array(max).keys()].map(num => num + min);
+const getPerfectSquares = size => range(1, size).filter(num => isSquare(num));
 
-function sumOfPerfectSquares(...args) {
-  return args;
+function sumOfPerfectSquares(size) {
+  const perfectSquares = getPerfectSquares(size);
+  const summables = [];
+
+  let sum = size;
+  perfectSquares.reverse().forEach((square) => {
+    if (sum > square) {
+      sum -= square;
+      if (sum >= 0) {
+        summables.push(square);
+      }
+      if (sum === 1) {
+        summables.push(1);
+      }
+    }
+  });
+
+  return summables.length;
 }
 
 export {
