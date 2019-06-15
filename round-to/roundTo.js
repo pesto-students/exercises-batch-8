799f4e5eb56be0e31ff0cab99880ba6965a12870
sourabhmodi20051 <sourabhmodi20051@gmail.com>
2019-06-15 20:21:21 +0530

    Solved Round To

--- round-to/roundTo.js
+++ round-to/roundTo.js
@@ -1,17 +1,22 @@
-function roundTo(num, precision) {
-  function up(number, upprecision) {
-    const rounder = 10 ** upprecision;
+const roundFunctions = {
+  up: (number, precision) => {
+    const rounder = 10 ** precision;
     return Math.ceil(number * rounder) / rounder;
-  }
-  function down(number, downprecision) {
-    const rounder = 10 ** downprecision;
+  },
+  down: (number, precision) => {
+    const rounder = 10 ** precision;
     return Math.floor(number * rounder) / rounder;
-  }
+  },
+};
+
+function roundTo(num, precision) {
   if ((num * (10 ** (precision + 1))) % 10 > 4.5) {
-    return up(num, precision);
+    return roundFunctions.up(num, precision);
   }
-  return down(num, precision);
+  return roundFunctions.down(num, precision);
 }
+
+Object.setPrototypeOf(roundTo, roundFunctions);
 
 export {
   roundTo,
