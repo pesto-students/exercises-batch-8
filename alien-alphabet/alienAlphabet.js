d9c53531e7fe1cfbd722f3c830d093830ecdfcf8
sourabhmodi20051 <sourabhmodi20051@gmail.com>
2019-06-15 19:43:31 +0530

    Solved Alien Alphabet

--- alien-alphabet/alienAlphabet.js
+++ alien-alphabet/alienAlphabet.js
@@ -1,6 +1,20 @@
 
-function alienAlphabet(...args) {
-  return args;
+function alienAlphabet(words) {
+  const characterLookup = {};
+  const characterArrays = words.map(word => word.split(''));
+  const alienAlphabetArray = characterArrays[0]
+    .reduce((accumulatedAlphabet, _, index) => {
+      const charactersAtIndex = characterArrays.map(array => array[index]);
+      const newAlienCharacters = charactersAtIndex.reduce((acc, currentItem) => {
+        if (currentItem !== undefined && characterLookup[currentItem] === undefined) {
+          characterLookup[currentItem] = true;
+          return [...acc, currentItem];
+        }
+        return acc;
+      });
+      return [...accumulatedAlphabet, ...newAlienCharacters];
+    }, []);
+  return alienAlphabetArray;
 }
 
 export {
