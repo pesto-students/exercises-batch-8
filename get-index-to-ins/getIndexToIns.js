b6082362a1a209e3d984f35d4063e411b5ce192e
Radheshyam Etikala <radhey1851@gmail.com>
2019-06-16 03:43:55 +0530

    completed isPalin

--- get-index-to-ins/getIndexToIns.js
+++ get-index-to-ins/getIndexToIns.js
@@ -1,6 +1,12 @@
 
-function getIndexToIns(...args) {
-  return args;
+function getIndexToIns(arr, num) {
+  arr.sort((a, b) => a - b);
+  for (const el of arr) {
+    if (num <= el) {
+      return arr.indexOf(el);
+    }
+  }
+  return arr.length; // insert at end
 }
 
 export {
