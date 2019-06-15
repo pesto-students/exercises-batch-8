c8474606ee996e4e28755ae49ee546483c1b22ed
saurabh shetty <saurabh.shetty100@gmail.com>
2019-06-15 09:54:23 +0530

    solved chuckArray

--- chunk-array-in-groups/chunkArrayInGroups.js
+++ chunk-array-in-groups/chunkArrayInGroups.js
@@ -1,6 +1,14 @@
 
-function chunkArrayInGroups(...args) {
-  return args;
+function chunkArrayInGroups(array, nestedArraySize) {
+  let lb = 0;
+  let ub = nestedArraySize;
+  const resultArray = [];
+  while (lb < array.length) {
+    resultArray.push(array.slice(lb, ub));
+    lb = ub;
+    ub += nestedArraySize;
+  }
+  return resultArray;
 }
 
 export {
