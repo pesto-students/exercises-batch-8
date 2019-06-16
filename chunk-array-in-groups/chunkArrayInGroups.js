e4576f1e9583769ff5bafb439dedb41ecd8c2e69
Pradyumna Newalkar <contactpradyumna1@gmail.com>
2019-06-16 09:54:33 +0000

    Added solution for chunk-array-in-groups

--- chunk-array-in-groups/chunkArrayInGroups.js
+++ chunk-array-in-groups/chunkArrayInGroups.js
@@ -1,6 +1,12 @@
 
-function chunkArrayInGroups(...args) {
-  return args;
+function chunkArrayInGroups(arr, groupSize) {
+  if (arr.length === 0) {
+    return [[]];
+  }
+  if (arr.length <= groupSize) {
+    return [arr];
+  }
+  return [arr.slice(0, groupSize), ...chunkArrayInGroups(arr.slice(groupSize), groupSize)];
 }
 
 export {
