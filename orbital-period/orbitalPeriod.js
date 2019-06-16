fe5e30f9f517659cbc475d1346ab1df37e782de9
Pradyumna Newalkar <contactpradyumna1@gmail.com>
2019-06-16 10:07:20 +0000

    Added solution for orbital-period

--- orbital-period/orbitalPeriod.js
+++ orbital-period/orbitalPeriod.js
@@ -1,6 +1,14 @@
 
-function orbitalPeriod(...args) {
-  return args;
+const GM = 398600.4418; // in km3s-2
+const earthRadius = 6367.4447; // in km
+
+function orbitalPeriod(satellites) {
+  return satellites.map(satellite => ({
+    name: satellite.name,
+    orbitalPeriod: Math.round(
+      2 * Math.PI * Math.sqrt(((satellite.avgAlt + earthRadius) ** 3) / GM),
+    ),
+  }));
 }
 
 export {
