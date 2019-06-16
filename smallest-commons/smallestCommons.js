8ae45451b0cad7814f09dac9df96991574250fe4
Kanav Arora <kanav_arora@statusbrew.com>
2019-06-16 19:31:07 +0530

    solved smalles Common

--- smallest-commons/smallestCommons.js
+++ smallest-commons/smallestCommons.js
@@ -1,6 +1,24 @@
+// Using Euclidean algorithm 
+function greatestCommonDivisor(num1, num2) {
+  if (num2 === 0) {
+    return num1;
+  }
+  return greatestCommonDivisor(num2, num1 % num2);
+}
 
-function smallestCommons(...args) {
-  return args;
+function leastCommonMultiple(num1, num2) {
+  return num1 * num2 / greatestCommonDivisor(num1, num2);
+}
+function smallestCommons(limits) {
+  const [lowerlimit, upperLimit] = limits.sort((a, b) => a - b);
+  if (lowerlimit === upperLimit) {
+    return lowerlimit;
+  }
+  let multiple = lowerlimit;
+  for (let i = lowerlimit; i <= upperLimit; i += 1) {
+    multiple = leastCommonMultiple(multiple, i);
+  }
+  return multiple;
 }
 
 export {
