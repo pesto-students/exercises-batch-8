790637a2dd1b4ce769cbe36ef9d074e4762e9ce2
Pradyumna Newalkar <contactpradyumna1@gmail.com>
2019-06-16 01:41:42 +0000

    Added solution for rotated-string

--- rotated-string/rotatedString.js
+++ rotated-string/rotatedString.js
@@ -1,6 +1,9 @@
 
-function rotatedString(...args) {
-  return args;
+function rotatedString(string1, string2) {
+  if (string1 === string2) {
+    return false;
+  }
+  return (string2 + string2).includes(string1);
 }
 
 export {
