70e6f4e53ebe703f4cb6b668c66f941f48d2639b
Radheshyam Etikala <radhey1851@gmail.com>
2019-06-16 07:13:24 +0530

    Completed rotateStr

--- rotated-string/rotatedString.js
+++ rotated-string/rotatedString.js
@@ -1,6 +1,28 @@
 
-function rotatedString(...args) {
-  return args;
+const circularIncrementerOfCircleLen = length => index => (index + 1) % length;
+
+function rotatedString(strA, strB) {
+  if (strA === strB || strA.length !== strB.length) {
+    return false;
+  }
+  const strLen = strA.length;
+
+  for (let start = 0; start < strLen - 1; start += 1) {
+    const builderArr = [];
+    const circularIncrementer = circularIncrementerOfCircleLen(strLen);
+    let rotatingIdx = start + 1;
+
+    while (rotatingIdx !== start) {
+      builderArr.push(strA[rotatingIdx]);
+      rotatingIdx = circularIncrementer(rotatingIdx);
+    }
+
+    builderArr.push(strA[rotatingIdx]);
+    if (builderArr.join('') === strB) {
+      return true;
+    }
+  }
+  return false;
 }
 
 export {
