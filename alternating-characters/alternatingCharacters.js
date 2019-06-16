e5f10189be7a3572064951d8d7ce8a0d970390a2
Kanav Arora <kanav_arora@statusbrew.com>
2019-06-17 03:09:08 +0530

    Solved Alternating Character

--- alternating-characters/alternatingCharacters.js
+++ alternating-characters/alternatingCharacters.js
@@ -1,6 +1,16 @@
 
-function alternatingCharacters(...args) {
-  return args;
+const alternatingCharacterCount = charArray => charArray.reduce((result, char, index, arr) => {
+  if (char === arr[index + 1]) {
+    return result + 1;
+  }
+  return result;
+}, 0);
+
+function alternatingCharacters(stringArray) {
+  return stringArray.map((string) => {
+    const charArray = string.split('');
+    return alternatingCharacterCount(charArray);
+  });
 }
 
 export {
