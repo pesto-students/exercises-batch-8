133596314fdb7101f02d1b3375e450ee1315fd27
Pradyumna Newalkar <contactpradyumna1@gmail.com>
2019-06-16 11:41:04 +0000

    Added solution for update-inventory

--- update-inventory/updateInventory.js
+++ update-inventory/updateInventory.js
@@ -1,6 +1,15 @@
-
-function updateInventory(...args) {
-  return args;
+function updateInventory(oldInv, newInv) {
+  const inventoryMap = {};
+  return [...oldInv, ...newInv].reduce((updatedInventory, record) => {
+    const [count, itemName] = record;
+    if (itemName in inventoryMap) {
+      inventoryMap[itemName][0] += count;
+    } else {
+      inventoryMap[itemName] = record;
+      updatedInventory.push(record);
+    }
+    return updatedInventory;
+  }, []).sort((record1, record2) => record1[1].localeCompare(record2[1]));
 }
 
 export {
