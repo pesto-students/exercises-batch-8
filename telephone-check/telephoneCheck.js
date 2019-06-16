fe5cd941aba4caff679580b991adb2c24f41e130
Radheshyam Etikala <radhey1851@gmail.com>
2019-06-16 11:07:08 +0530

    Completed Regex

--- telephone-check/telephoneCheck.js
+++ telephone-check/telephoneCheck.js
@@ -1,6 +1,6 @@
 
 function telephoneCheck(phoneNumber) {
-  const phoneNumRegex = /^[1]{0,1}[ ]*[(]{0,1}[0-9]{3}[)]{0,1}[ ]*[(|-| ]{0,1}[0-9]{3}[ |(|)|-]{0,1}[0-9]{4}$/;
+  const phoneNumRegex = /^(1\s?)?((\([0-9]{3}\))|[0-9]{3})[\s|(|-]?[0-9]{3}[ |(|)|-]{0,1}[0-9]{4}$/;
   return Boolean(phoneNumRegex.exec(phoneNumber));
 }
 
