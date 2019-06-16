27a3321290441b7115fbb5600f79d47bcb468223
saurabh shetty <saurabh.shetty100@gmail.com>
2019-06-17 03:52:20 +0530

    solved parseMs

--- parse-ms/parseMs.js
+++ parse-ms/parseMs.js
@@ -1,6 +1,29 @@
+const msToUnits = {
+  milliseconds: 1,
+  seconds: 1000,
+  minutes: 1000 * 60,
+  hours: 1000 * 60 * 60,
+  days: 1000 * 60 * 60 * 24,
+};
 
-function parseMs(...args) {
-  return args;
+function parseMs(ms) {
+  let copyMs = Math.abs(ms);
+  let negativeFlag = 1;
+  if (ms < 0) {
+    negativeFlag = -1;
+  }
+  const parsedMs = {};
+  Object.keys(msToUnits).reverse().map((unit) => {
+    if (copyMs > msToUnits[unit]) {
+      const amountOfUnits = Math.floor(copyMs / msToUnits[unit]);
+      parsedMs[unit] = negativeFlag * amountOfUnits;
+      copyMs -= amountOfUnits * msToUnits[unit];
+    } else {
+      parsedMs[unit] = negativeFlag * 0;
+    }
+    return unit;
+  });
+  return parsedMs;
 }
 
 export {
--- round-to/roundTo.js
+++ round-to/roundTo.js
@@ -1,8 +1,17 @@
 
-function roundTo(...args) {
-  return args;
+const roundPrototype = {
+  up: (number, decimals) => Math.ceil(number * (10 ** decimals)) / (10 ** decimals),
+  down: (number, decimals) => Math.floor(number * (10 ** decimals)) / (10 ** decimals),
+};
+
+
+function roundTo(number, decimals) {
+  return Math.round(number * (10 ** decimals)) / (10 ** decimals);
 }
 
+
+Object.setPrototypeOf(roundTo, roundPrototype);
+// roundTo.prototype = roundPrototype;
 export {
   roundTo,
 };
