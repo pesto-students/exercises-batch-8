3367a214656a9ef97c77bf61f954fcca7c8800f5
Pradyumna Newalkar <contactpradyumna1@gmail.com>
2019-06-16 12:20:40 +0000

    Added solution for request-validator

--- request-validator/requestValidator.js
+++ request-validator/requestValidator.js
@@ -1,6 +1,33 @@
+function validateHTTPMethod(method) {
+  return ['GET', 'POST', 'DELETE'].includes(method);
+}
 
-function requestValidator(...args) {
-  return args;
+function validateHTTPURI(uri) {
+  if (uri === '*') {
+    return true;
+  }
+  const uriRegex = /^([a-z]+:\/\/)?([a-z.]+[^ ]+[a-z.]+)+$/ig;
+  return uriRegex.test(uri);
+}
+
+function validateHTTPVersion(version) {
+  return ['HTTP/0.9', 'HTTP/1.0', 'HTTP/1.1', 'HTTP/2.0'].includes(version);
+}
+
+function requestValidator(request) {
+  if (!validateHTTPMethod(request.method)) {
+    throw new Error('Invalid request header: Invalid Method');
+  }
+  if (!validateHTTPURI(request.uri)) {
+    throw new Error('Invalid request header: Invalid URI');
+  }
+  if (!validateHTTPVersion(request.version)) {
+    throw new Error('Invalid request header: Invalid Version');
+  }
+  if (request.message == null) {
+    throw new Error('Invalid request header: Invalid Message');
+  }
+  return request;
 }
 
 export {
