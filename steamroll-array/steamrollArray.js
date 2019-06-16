1afb4ab74882beeb4a72405d3c3f52b44ae849b9
Radheshyam Etikala <radhey1851@gmail.com>
2019-06-16 06:39:15 +0530

    Completed SteamrollArr

--- steamroll-array/steamrollArray.js
+++ steamroll-array/steamrollArray.js
@@ -1,6 +1,21 @@
+function flatteningFunction(arg, ...rest) {
+  // if arg still has to be flattened
+  if (Array.isArray(arg)) {
+    if (rest.length > 0) {
+      return [...flatteningFunction(...arg), ...flatteningFunction(...rest)];
+    }
+    return flatteningFunction(...arg);
+  }
+  // if arg is no longer an array
+  return rest.length > 0 ? [arg, ...flatteningFunction(...rest)] : [arg];
+}
 
-function steamrollArray(...args) {
-  return args;
+function steamrollArray(arr) {
+  const flattenedArr = flatteningFunction(arr.filter(el => (
+    // is A non empty arr or non-array element
+    (Array.isArray(el) && el.length > 0) || !Array.isArray(el)
+  )));
+  return flattenedArr;
 }
 
 export {
