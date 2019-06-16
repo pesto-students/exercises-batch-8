088c5389789a76622d61e4b0e1e0bd87228841c2
Radheshyam Etikala <radhey1851@gmail.com>
2019-06-16 08:48:27 +0530

    Completed rot13

--- rot13/rot13.js
+++ rot13/rot13.js
@@ -1,6 +1,26 @@
+function isALetter(char) {
+  const capLetterRegex = /[A-Z]/;
+  return char.match(capLetterRegex);
+}
 
-function rot13(...args) {
-  return args;
+function decipher(char) {
+  const rot13CharCode = char.charCodeAt();
+  const charCode = rot13CharCode + 13;
+  const overFlowOver90 = charCode - 91; // 90 is char Code of Z
+  const charCodeOfA = 65;
+  const decipheredCharCode = overFlowOver90 >= 0 ? charCodeOfA + overFlowOver90 : charCode;
+  return String.fromCharCode(decipheredCharCode);
+}
+
+function rot13(encryptedStr) {
+  const strArr = encryptedStr.split('');
+  const decryptedArr = strArr.map((char) => {
+    if (isALetter(char)) {
+      return decipher(char);
+    }
+    return char;
+  });
+  return decryptedArr.join('');
 }
 
 export {
