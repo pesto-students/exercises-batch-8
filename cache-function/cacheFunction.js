b17d818efe3e650ee7c99343137cb06c5ac25a11
Kanav Arora <kanav_arora@statusbrew.com>
2019-06-16 05:33:34 +0530

    Solved CacheFunction

--- cache-function/cacheFunction.js
+++ cache-function/cacheFunction.js
@@ -1,6 +1,17 @@
 
-function cacheFunction(...args) {
-  return args;
+function cacheFunction(fn) {
+  // eslint-disable-next-line prefer-const
+  let cache = {};
+  return (args) => {
+    let result;
+    if (args in cache) {
+      result = cache[args];
+    } else {
+      result = fn(args);
+      cache[args] = result;
+    }
+    return result;
+  };
 }
 
 export {
