28b06989584ad45c7a92257797c74da7cbc81dfa
Pradyumna Newalkar <contactpradyumna1@gmail.com>
2019-06-16 10:28:32 +0000

    Added solution for smallest-commons

--- smallest-commons/smallestCommons.js
+++ smallest-commons/smallestCommons.js
@@ -1,6 +1,25 @@
-
-function smallestCommons(...args) {
-  return args;
+function greatestCommmonDivisor(num1, num2) {
+  if (num1 > num2) {
+    return greatestCommmonDivisor(num2, num1);
+  }
+  if (num2 % num1 === 0) {
+    return num1;
+  }
+  return greatestCommmonDivisor(num2 % num1, num1);
+}
+function leastCommonMultiple(num1, num2) {
+  return (num1 * num2) / greatestCommmonDivisor(num1, num2);
+}
+function smallestCommons(limits) {
+  const [lowerInd, upperInd] = limits[0] > limits[1] ? [1, 0] : [0, 1];
+  if (limits[lowerInd] === limits[upperInd]) {
+    return limits[lowerInd];
+  }
+  const middle = Math.floor((limits[lowerInd] + limits[upperInd]) / 2);
+  return leastCommonMultiple(
+    smallestCommons([limits[lowerInd], middle]),
+    smallestCommons([middle + 1, limits[upperInd]]),
+  );
 }
 
 export {
