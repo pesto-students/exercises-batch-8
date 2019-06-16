5654a29cb4516b4bffda9edb4cdc1dad14eb2ee7
Kanav Arora <kanav_arora@statusbrew.com>
2019-06-16 17:56:27 +0530

    solved rot13

--- rot13/rot13.js
+++ rot13/rot13.js
@@ -1,6 +1,19 @@
 
-function rot13(...args) {
-  return args;
+const charCodes = stringArray => stringArray.map(char => char.charCodeAt());
+
+const CHAR_CODE_A = 'A'.charCodeAt();
+const CHAR_CODE_Z = 'Z'.charCodeAt();
+const isAlphabet = charCode => charCode >= CHAR_CODE_A && charCode <= CHAR_CODE_Z;
+const decipherCodesArray = stringArray => charCodes(stringArray)
+  // eslint-disable-next-line no-confusing-arrow
+  .map(charCode => isAlphabet(charCode) ? CHAR_CODE_A + ((charCode - CHAR_CODE_A + 13) % 26) : charCode);
+
+const charArray = codesArray => codesArray.map(code => String.fromCharCode(code));
+
+function rot13(string) {
+  const decipherCodes = decipherCodesArray(string.split(''));
+
+  return charArray(decipherCodes).join('');
 }
 
 export {
