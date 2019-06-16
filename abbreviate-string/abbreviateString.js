88dbeb5176249e732db2bb461e9cf4cec50d94d3
Pradyumna Newalkar <contactpradyumna1@gmail.com>
2019-06-16 15:37:31 +0000

    optimized solution for abbreviateString

--- abbreviate-string/abbreviateString.js
+++ abbreviate-string/abbreviateString.js
@@ -3,10 +3,8 @@ function abbreviateString(str) {
   if (typeof str !== 'string') {
     throw TypeError;
   }
-  const mainTokens = str
-    .split(' ')
-    .filter((token, index, arr) => index === 0 || index === arr.length - 1);
-  return `${mainTokens[0]} ${mainTokens[1].toUpperCase()[0]}.`;
+  const mainTokens = str.split(' ');
+  return `${mainTokens[0]} ${mainTokens[mainTokens.length - 1].toUpperCase()[0]}.`;
 }
 
 export {
