93fb2043b52e31a4a24a2aeb7649c633691cf70f
Radheshyam Etikala <radhey1851@gmail.com>
2019-06-16 12:14:01 +0530

    Completed Accounts Partially

--- account-dataset/accountDataset.js
+++ account-dataset/accountDataset.js
@@ -1,8 +1,77 @@
+import dataset from './dataset.json';
 
 function accountDataset(...args) {
   return args;
 }
 
+function hasMoreThanHundredThousandUSD(account) {
+  const HUNDRED_THOUSAND = 100000;
+  return Number(account.amount) > HUNDRED_THOUSAND;
+}
+
+function hundredThousandairs() {
+  return dataset.bankBalances.filter(hasMoreThanHundredThousandUSD);
+}
+
+function datasetWithRoundedDollar() {
+  const roundedAmountsDataset = dataset.bankBalances
+    .map(account => (
+      {
+        ...account,
+        rounded: Math.round(account.amount),
+      }
+    ));
+  return roundedAmountsDataset;
+}
+
+function sumOfBankBalances() {
+  const cumulativeAmountObj = dataset.bankBalances.reduce((acc, curr) => ({
+    amount: Number(acc.amount) + Number(curr.amount),
+  }));
+  return Number(cumulativeAmountObj.amount.toFixed(2));
+}
+
+
+function sumOfInterests() {
+  const { bankBalances } = dataset;
+  const states = ['WI', 'IL', 'WY', 'OH', 'GA', 'DE'];
+  const accountsOfStates = bankBalances.filter(account => states.includes(account.state));
+  const totalInterest = accountsOfStates
+    .reduce((acc, curr) => {
+      const interestAmount = parseFloat((curr.amount * 18.9) / 100);
+      const amountWithInterest = parseFloat(interestAmount + curr.amount).toFixed(2);
+
+      return acc + parseFloat(amountWithInterest);
+    }, 0);
+  return parseFloat(totalInterest.toFixed(2));
+}
+
+function higherStateSums() {
+  const { bankBalances } = dataset;
+  const minium = 1000000;
+  const amountOfStates = {};
+  bankBalances.forEach((account) => {
+    if (amountOfStates[account.state] === undefined) {
+      amountOfStates[account.state] = account.amount;
+    } else {
+      amountOfStates[account.state] += account.amount;
+    }
+  });
+  const amountOfStatesArray = Object.values(amountOfStates);
+  console.log(amountOfStatesArray);
+  return amountOfStatesArray.reduce((acc, curr) => {
+    if (curr > minium) {
+      return acc + curr;
+    }
+    return acc;
+  }, 0);
+}
+
 export {
   accountDataset,
+  hundredThousandairs,
+  datasetWithRoundedDollar,
+  sumOfBankBalances,
+  sumOfInterests,
+  higherStateSums,
 };
