6217aa3d2f94dbc5f7ec63a053d523c0f7142ff8
Pradyumna Newalkar <contactpradyumna1@gmail.com>
2019-06-16 19:29:41 +0000

    added solution for permutation-with-repetitiion

--- permutate-with-repetitions/permutateWithRepetitions.js
+++ permutate-with-repetitions/permutateWithRepetitions.js
@@ -1,6 +1,19 @@
+function permutate(chars, slots) {
+  if (slots === 1) {
+    return chars.map(char => [char]);
+  }
+  const subPermutations = permutate(chars, slots - 1);
+  return chars.reduce(
+    (permutations, char) => [
+      ...permutations,
+      ...subPermutations.map(subPermutation => [char, ...subPermutation]),
+    ],
+    [],
+  );
+}
 
-function permutateWithRepetitions(...args) {
-  return args;
+function permutateWithRepetitions(chars) {
+  return permutate(chars, chars.length);
 }
 
 export {
