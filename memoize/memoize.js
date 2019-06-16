ccf523937008d6d8b09892a99b31ca2f3947a708
Kanav Arora <kanav_arora@statusbrew.com>
2019-06-16 21:59:52 +0530

    Solved Memoize

--- memoize/memoize.js
+++ memoize/memoize.js
@@ -1,6 +1,17 @@
 
-function memoize(...args) {
-  return args;
+function memoize(fn) {
+  const cache = {};
+  let result;
+  return (...args) => {
+    const key = args.toString();
+    if (key in cache) {
+      result = cache[key];
+    } else {
+      cache[key] = fn(...args);
+      result = cache[key];
+    }
+    return result;
+  };
 }
 
 export {
