8e0c9d13fd632677894107aa9ce8a85b5703d075
Pradyumna Newalkar <contactpradyumna1@gmail.com>
2019-06-16 01:57:02 +0000

    Added solution for rot13

--- rot13/rot13.js
+++ rot13/rot13.js
@@ -1,6 +1,18 @@
-
-function rot13(...args) {
-  return args;
+function range(n) {
+  return [...Array(n).keys()];
+}
+function isAlphabet(char) {
+  return /[a-z]/i.test(char);
+}
+const CHAR_CODE_A = 'A'.charCodeAt(0);
+function rot13(str) {
+  return range(str.length).map((ind) => {
+    if (isAlphabet(str[ind])) {
+      const charCode = str.charCodeAt(ind);
+      return String.fromCharCode(CHAR_CODE_A + ((charCode - CHAR_CODE_A + 13) % 26));
+    }
+    return str[ind];
+  }).join('');
 }
 
 export {
