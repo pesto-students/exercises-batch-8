697d4e80f791d6d6efaae8eb18886b78e5949978
Pradyumna Newalkar <contactpradyumna1@gmail.com>
2019-06-16 18:34:50 +0000

    Added solution for is-palindrome

--- is-palindrome/isPalindrome.js
+++ is-palindrome/isPalindrome.js
@@ -1,6 +1,10 @@
 
-function isPalindrome(...args) {
-  return args;
+function isPalindrome(arg) {
+  if (typeof arg === 'number') {
+    return isPalindrome(String(arg));
+  }
+  const clean = arg.replace(/[^a-z0-9]/gi, '').toLowerCase();
+  return clean === clean.split('').reverse().join('');
 }
 
 export {
