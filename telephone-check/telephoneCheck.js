98a2ce7867e04598b8690cb494a73b8f6f4ccae8
Radheshyam Etikala <radhey1851@gmail.com>
2019-06-16 10:46:35 +0530

    Completing tele

--- telephone-check/telephoneCheck.js
+++ telephone-check/telephoneCheck.js
@@ -1,6 +1,7 @@
 
-function telephoneCheck(...args) {
-  return args;
+function telephoneCheck(phoneNumber) {
+  const phoneNumRegex = /^[1]{0,1}[ ]*[(]{0,1}[0-9]{3}[)]{0,1}[ ]*[(|-| ]{0,1}[0-9]{3}[ |(|)|-]{0,1}[0-9]{4}$/;
+  return Boolean(phoneNumRegex.exec(phoneNumber));
 }
 
 export {
