9ec38664cdfd58306abfd108e99d6a84b3ef5f40
Jaspreet Singh <erjaspreet57@gmail.com>
2019-06-18 01:51:58 +0530

    Solve map-filter-and-reduce

--- map-filter-and-reduce/mapFilterAndReduce.js
+++ map-filter-and-reduce/mapFilterAndReduce.js
@@ -1,6 +1,12 @@
 
-function mapFilterAndReduce(...args) {
-  return args;
+function mapFilterAndReduce(people) {
+  return people
+    .map(person => person.firstName)
+    .filter(name => name.length < 5)
+    .reduce((acc, name) => {
+      acc[name] = name.length;
+      return acc;
+    }, {});
 }
 
 export {
