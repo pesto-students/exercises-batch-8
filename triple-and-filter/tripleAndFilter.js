59c5d03798a64a702575a1a13540431eeb11d1a0
Sebin Benjamin <sebinbenjamin@gmail.com>
2019-06-18 00:25:44 +0530

    completed exercise 'triple-and-filter'

--- triple-and-filter/tripleAndFilter.js
+++ triple-and-filter/tripleAndFilter.js
@@ -1,6 +1,8 @@
 
-function tripleAndFilter(...args) {
-  return args;
+function tripleAndFilter(numbers) {
+  const multiplesOf5 = numbers.filter(num => num % 5 === 0);
+  const tripledNumbers = multiplesOf5.map(num => num * 3);
+  return tripledNumbers;
 }
 
 export {
