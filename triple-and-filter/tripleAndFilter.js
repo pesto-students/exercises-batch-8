7e6cb5d4cdb66c54351c52bb7e41670b82819397
Pradyumna Newalkar <contactpradyumna1@gmail.com>
2019-06-17 20:26:23 +0000

    Added solution for triple-and-filter

--- triple-and-filter/tripleAndFilter.js
+++ triple-and-filter/tripleAndFilter.js
@@ -1,6 +1,6 @@
 
-function tripleAndFilter(...args) {
-  return args;
+function tripleAndFilter(arr) {
+  return [arr.filter(val => val % 5 === 0).reduce((sum, val) => sum + (val * 3), 0)];
 }
 
 export {
