c7504ba401914236fd5cc5532760e0d77dc4c04f
Abdul Khuddus <khuddus1@gmail.com>
2019-06-18 00:26:30 +0530

    Complete triple and filter

--- triple-and-filter/tripleAndFilter.js
+++ triple-and-filter/tripleAndFilter.js
@@ -1,6 +1,7 @@
 
-function tripleAndFilter(...args) {
-  return args;
+function tripleAndFilter(numbers) {
+  const tripledNumbers = numbers.map(number => number * 3);
+  return tripledNumbers.filter(number => number % 5 === 0);
 }
 
 export {
