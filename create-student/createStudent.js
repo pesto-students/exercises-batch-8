bc41fb2d72806ff98ce6594efdee69645a54455e
Abdul Khuddus <khuddus1@gmail.com>
2019-06-18 03:56:03 +0530

    Partially complete create student

--- create-student/createStudent.js
+++ create-student/createStudent.js
@@ -1,6 +1,18 @@
 
-function createStudent(...args) {
-  return args;
+function createStudent(studentLikes) {
+  const generalLikes = {
+    likesJavaScript: false,
+    likesES2015: false,
+  };
+  const finalLikes = Object.assign(studentLikes, generalLikes);
+  if (finalLikes.likesES2015 && finalLikes.likesJavaScript) {
+    return 'The student likes JavaScript and ES2015';
+  } if (finalLikes.likesES2015 && !finalLikes.likesJavaScript) {
+    return 'The student likes ES2015!';
+  } if (!finalLikes.likesES2015 && finalLikes.likesJavaScript) {
+    return 'The student likes JavaScript!';
+  }
+  return 'The student does not like much...';
 }
 
 export {
