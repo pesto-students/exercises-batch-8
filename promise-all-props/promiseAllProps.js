46c09c91761c66e2bd4e69bc9a17c1d898988adc
Sebin Benjamin <sebinbenjamin@gmail.com>
2019-06-18 04:57:59 +0530

    Completed exercise 'promise-all-props'

--- promise-all-props/promiseAllProps.js
+++ promise-all-props/promiseAllProps.js
@@ -1,6 +1,21 @@
 
-function promiseAllProps(...args) {
-  return args;
+function promiseAllProps(object) {
+  return new Promise((resolve, reject) => {
+    Promise.all(object.foo, object.bar)
+      .then((result) => {
+        resolve({
+          foo: result[0],
+          bar: result[1],
+        });
+      })
+      .error((error) => {
+        // eslint-disable-next-line prefer-promise-reject-errors
+        reject({
+          foo: error[0],
+          bar: error[1],
+        });
+      });
+  });
 }
 
 export {
