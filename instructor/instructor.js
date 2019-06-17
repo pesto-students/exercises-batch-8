966d78e161af94b1205c3562f42c8268c3028a35
saurabh shetty <saurabh.shetty100@gmail.com>
2019-06-18 00:52:09 +0530

    reverted instructor

--- instructor/instructor.js
+++ instructor/instructor.js
@@ -2,17 +2,7 @@
 function instructor(...args) {
   return args;
 }
-const instructorPrototype = {
-  fName: 'Rama',
-  sayHi: () => {
-    this.fName = 'Ram';
-    return 'Name Updated';
-  },
-  get firstName() {
-    return this.fName;
-  },
-};
-Object.setPrototypeOf(instructor, instructorPrototype);
+
 export {
   instructor,
 };
