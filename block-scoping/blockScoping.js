3cf1754cc700f73860ee5350efe5ebae43b9155a
kunnu01 <kunalbhatia2409@gmail.com>
2019-06-18 00:30:47 +0530

    Solved blockScoping

--- block-scoping/blockScoping.js
+++ block-scoping/blockScoping.js
@@ -1,6 +1,13 @@
 
-function blockScoping(...args) {
-  return args;
+function blockScoping(number) {
+  if (typeof number !== 'number') {
+    throw new Error(`Expected a number, got ${number}`);
+  }
+  const callbacks = [];
+  for (let i = 0; i <= 10; i += 1) {
+    callbacks.push(() => i);
+  }
+  return callbacks[number]();
 }
 
 export {
