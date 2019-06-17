cbc00c015f436e6151e6a55fcf9df8e5a0962811
Abdul Khuddus <khuddus1@gmail.com>
2019-06-18 00:21:35 +0530

    Complete template literals

--- template-literals/templateLiterals.js
+++ template-literals/templateLiterals.js
@@ -1,6 +1,12 @@
 
-function templateLiterals(...args) {
-  return args;
+function templateLiterals() {
+  const names = [
+    'Rooney',
+    'Ronaldo',
+    'Messi',
+    'Pogba',
+  ];
+  return `There are 4 people on the football team. Their names are ${names[0]}, ${names[1]}, ${names[2]}, ${names[3]}.`;
 }
 
 export {
