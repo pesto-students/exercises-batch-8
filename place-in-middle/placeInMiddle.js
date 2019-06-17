b155ce467812be49ab24ba0a88d743ede0e57635
Sebin Benjamin <sebinbenjamin@gmail.com>
2019-06-18 00:51:16 +0530

    completed exercise 'place-in-middle'

--- place-in-middle/placeInMiddle.js
+++ place-in-middle/placeInMiddle.js
@@ -1,6 +1,10 @@
 
-function placeInMiddle(...args) {
-  return args;
+function placeInMiddle(array, valuesToInsert) {
+  const midPoint = array.length / 2;
+  const firstPart = array.slice(0, midPoint);
+  const lastPart = array.slice(midPoint);
+  const newArray = [...firstPart, ...valuesToInsert, ...lastPart];
+  return newArray;
 }
 
 export {
