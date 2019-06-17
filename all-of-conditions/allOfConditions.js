515b6e8e40c97fa1dfff359c38b4faa3fc206de6
kunnu01 <kunalbhatia2409@gmail.com>
2019-06-18 00:24:49 +0530

    solved allOfConditions

--- all-of-conditions/allOfConditions.js
+++ all-of-conditions/allOfConditions.js
@@ -1,6 +1,11 @@
 
-function allOfConditions(...args) {
-  return args;
+function allOfConditions(...functions) {
+  return args => functions.reduce((acc, func) => {
+    if (acc) {
+      return func(args);
+    }
+    return null;
+  }, true);
 }
 
 export {
