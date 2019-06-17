ffef53ce1bbf20839a1f1e37151d4732f4c157cc
kunnu01 <kunalbhatia2409@gmail.com>
2019-06-18 04:20:49 +0530

    Solved alphabetic-shift, greet, isPromise, person

--- alphabetic-shift/alphabeticShift.js
+++ alphabetic-shift/alphabeticShift.js
@@ -1,6 +1,12 @@
 
-function alphabeticShift(...args) {
-  return args;
+function alphabeticShift(string) {
+  const chars = string.split('');
+  const shiftedChars = chars.map((char) => {
+    const shiftedASCIIValue = char.charCodeAt(0) + 1;
+    return String.fromCharCode(shiftedASCIIValue);
+  });
+  const shiftedString = shiftedChars.join('');
+  return shiftedString;
 }
 
 export {
