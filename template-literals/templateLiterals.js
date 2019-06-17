0e046626c1870bf00f88aec4f708b3fdd7853a3e
Abhinav Sharma <abhi18av@outlook.com>
2019-06-18 00:39:03 +0530

    solved the template-literals problem

--- template-literals/templateLiterals.js
+++ template-literals/templateLiterals.js
@@ -1,6 +1,7 @@
 
-function templateLiterals(...args) {
-  return args;
+function templateLiterals() {
+  const messi = 'Messi';
+  return `There are 4 people on the football team. Their names are Rooney, Ronaldo, ${messi}, Pogba.`;
 }
 
 export {
