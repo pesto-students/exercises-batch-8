08acc88cf6dfdf369f22c9c5ec67d39a009e0cb1
Pradyumna Newalkar <contactpradyumna1@gmail.com>
2019-06-17 19:19:49 +0000

    Added solution for const-immutable

--- const-immutable/constImmutable.js
+++ const-immutable/constImmutable.js
@@ -1,6 +1,19 @@
-
-function constImmutable(...args) {
-  return args;
+function constImmutable() {
+  const account = {
+    username: 'pesto',
+    password: 'initialPassword',
+  };
+  const { password } = account;
+  Object.defineProperty(account, 'password', {
+    get() {
+      return password;
+    },
+    set() {
+      return password;
+    },
+  });
+  account.password = 's3cret';
+  return account.password;
 }
 
 export {
