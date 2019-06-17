9071c6fac12f7a54539e8737f93dd0dafa95c102
prateekinstillion <Prateek Madaan>
2019-06-18 04:40:55 +0530

    Solved the person probelm

--- person/Person.js
+++ person/Person.js
@@ -1,8 +1,18 @@
+const isDigit = char => /^\d$/.test(char);
+class Person {
+  constructor(firstName, lastName, dateOfBirth) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dateOfBirth = dateOfBirth;
+  }
 
-function person(...args) {
-  return args;
+  addDobDigits() {
+    const digitsOfDOB = this.dateOfBirth.split('').filter(isDigit);
+    const digitSum = digitsOfDOB.reduce((acc, digit) => acc + parseInt(digit, 10), 0);
+    return digitSum;
+  }
 }
 
 export {
-  person,
+  Person,
 };
