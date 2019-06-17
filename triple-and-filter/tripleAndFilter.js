3bcbf7b2ce32e56377dc9af0ff9cf72e0f994473
saurabh shetty <saurabh.shetty100@gmail.com>
2019-06-18 01:03:28 +0530

    solved triplefilter

--- triple-and-filter/tripleAndFilter.js
+++ triple-and-filter/tripleAndFilter.js
@@ -1,6 +1,6 @@
 
-function tripleAndFilter(...args) {
-  return args;
+function tripleAndFilter(numbers) {
+  return numbers.map(number => number * 3).filter(number => number % 5 === 0);
 }
 
 export {
