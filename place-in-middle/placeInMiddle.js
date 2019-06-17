6093e00eb7a9133879e83627307d3c2440ba43f6
aayushtiwari <arsenicaayush@gmail.com>
2019-06-18 01:51:56 +0530

    updated

--- place-in-middle/placeInMiddle.js
+++ place-in-middle/placeInMiddle.js
@@ -1,8 +1,7 @@
-
-function placeInMiddle(...args) {
-  return args;
+function placeInMiddle([...array1], [...array2]) {
+  const { length } = array1;
+  const arrstart = array1.splice(0, length / 2);
+  return [...arrstart, ...array2, ...array1];
 }
 
-export {
-  placeInMiddle,
-};
+export { placeInMiddle };
