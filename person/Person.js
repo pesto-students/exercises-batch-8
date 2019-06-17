df99dfa4ee36f3d85055b6d39fd9a974493b1ded
Radheshyam Etikala <radhey1851@gmail.com>
2019-06-18 04:34:31 +0530

    completed Person

--- person/Person.js
+++ person/Person.js
@@ -1,8 +1,29 @@
 
-function person(...args) {
-  return args;
+
+class Person {
+  constructor(firstName, lastName, dateOfBirth) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dateOfBirth = dateOfBirth;
+  }
+
+  static sum(a, b) {
+    return a + b;
+  }
+
+  static isDigit(char) {
+    const digitRegex = /^[0-9]$/;
+    return digitRegex.exec(char);
+  }
+
+  addDobDigits() {
+    const { sum, isDigit } = Person;
+    const dobElementsArray = this.dateOfBirth.split('');
+    const digits = dobElementsArray.filter(isDigit).map(Number);
+    return digits.reduce(sum);
+  }
 }
 
 export {
-  person,
+  Person,
 };
