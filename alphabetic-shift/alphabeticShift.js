60ec3ff661a38f1316549cb0f9eb0b25cf56dc88
Abdul Khuddus <khuddus1@gmail.com>
2019-06-18 05:00:15 +0530

    Complete alphabetic shift

--- alphabetic-shift/alphabeticShift.js
+++ alphabetic-shift/alphabeticShift.js
@@ -1,6 +1,10 @@
 
-function alphabeticShift(...args) {
-  return args;
+function alphabeticShift(string) {
+  const shiftedString = string.split('').map((char) => {
+    const charCode = char.charCodeAt(0);
+    return String.fromCharCode(charCode + 1);
+  });
+  return shiftedString.join('');
 }
 
 export {
