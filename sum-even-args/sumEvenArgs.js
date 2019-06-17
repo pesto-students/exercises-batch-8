262cbcb4c3c2d573e98828a28fd4c0cc91b4617f
Sebin Benjamin <sebinbenjamin@gmail.com>
2019-06-18 00:42:02 +0530

    completed exercise 'sum-even-args'

--- sum-even-args/sumEvenArgs.js
+++ sum-even-args/sumEvenArgs.js
@@ -1,6 +1,8 @@
 
-function sumEvenArgs(...args) {
-  return args;
+function sumEvenArgs(...numbers) {
+  const evenNumbers = numbers.filter(num => num % 2 === 0);
+  const sumOfEvenNumbers = evenNumbers.reduce((acc, num) => acc + num, 0);
+  return sumOfEvenNumbers;
 }
 
 export {
