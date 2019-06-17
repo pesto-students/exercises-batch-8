9f7213594a88d100481479518ff5bb0ef137cf56
prateekinstillion <Prateek Madaan>
2019-06-18 04:52:41 +0530

    Solved the alphabetic shift problem

--- alphabetic-shift/alphabeticShift.js
+++ alphabetic-shift/alphabeticShift.js
@@ -1,6 +1,12 @@
 
-function alphabeticShift(...args) {
-  return args;
+const getCharacterCode = char => char.charCodeAt(0);
+const shiftCharacterCodeByOne = charCode => charCode + 1;
+
+function alphabeticShift(string) {
+  const characterCodes = Array.from(string).map(getCharacterCode);
+  const shiftedCharacterCodes = characterCodes.map(shiftCharacterCodeByOne);
+  const shiftedString = String.fromCharCode(...shiftedCharacterCodes);
+  return shiftedString;
 }
 
 export {
--- queue/Queue.js
+++ queue/Queue.js
@@ -1,8 +1,25 @@
+// import { classBody } from "@babel/types";
 
-function queue(...args) {
-  return args;
+// function queue(...args) {
+//   return args;
+// }
+class Queue {
+  constructor() {
+    this.items = [];
+  }
+
+  enqueue(item) {
+    this.items.unshift(item);
+  }
+
+  dequeue() {
+    this.items.shift();
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
 }
-
 export {
-  queue,
+  Queue,
 };
