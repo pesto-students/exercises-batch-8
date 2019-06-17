647e4bc239ec085ce9fb013f944e5c39089c311f
prateekinstillion <Prateek Madaan>
2019-06-18 01:47:48 +0530

    Solved the double odd numbers problem

--- double-odd-numbers/doubleOddNumbers.js
+++ double-odd-numbers/doubleOddNumbers.js
@@ -1,6 +1,9 @@
 
-function doubleOddNumbers(...args) {
-  return args;
+const checkOdd = number => number % 2 === 1;
+const performDouble = number => number * 2;
+
+function doubleOddNumbers(numbers) {
+  return numbers.filter(checkOdd).map(performDouble);
 }
 
 export {
