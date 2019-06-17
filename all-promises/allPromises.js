b05f8910a7be2e9cfb899dce3e68c369bd52d800
sourabhmodi20051 <sourabhmodi20051@gmail.com>
2019-06-18 04:44:35 +0530

    Solved All Promises

--- all-promises/allPromises.js
+++ all-promises/allPromises.js
@@ -1,8 +1,29 @@
-
-function allPromises(...args) {
-  return args;
+function resolvePromiseIfAllResolved(resolvedCount, promises, promiseMap, allPromiseResolve) {
+  if (resolvedCount === promises.length) {
+    allPromiseResolve(Object.values(promiseMap));
+  }
 }
 
-export {
-  allPromises,
-};
+function allPromises(promises) {
+  const allPromise = new Promise((resolve) => {
+    let promiseMap = {};
+    let resolvedCount = 0;
+    for (const [index, promise] of promises.entries()) {
+      if (promise instanceof Promise) {
+        // eslint-disable-next-line
+        promise.then(res => {
+          promiseMap = { ...promiseMap, [index]: res };
+          resolvedCount += 1;
+          resolvePromiseIfAllResolved(resolvedCount, promises, promiseMap, resolve);
+        });
+      } else {
+        promiseMap = { ...promiseMap, [index]: promise };
+        resolvedCount += 1;
+        resolvePromiseIfAllResolved(resolvedCount, promises, promiseMap, resolve);
+      }
+    }
+  });
+  return allPromise;
+}
+
+export { allPromises };
