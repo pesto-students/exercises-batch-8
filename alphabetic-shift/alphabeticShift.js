b25b41db937af5a847012ee62faa44d1c2668239
Sebin Benjamin <sebinbenjamin@gmail.com>
2019-06-18 04:36:43 +0530

    completed exercise 'alphabetic-shift'

--- alphabetic-shift/alphabeticShift.js
+++ alphabetic-shift/alphabeticShift.js
@@ -1,6 +1,9 @@
 
-function alphabeticShift(...args) {
-  return args;
+function alphabeticShift([...charArray]) {
+  const charCodeArray = charArray.map(char => char.charCodeAt(0));
+  const shiftedCharCodeArray = charCodeArray.map(charCode => charCode + 1);
+  const shiftedString = String.fromCharCode(...shiftedCharCodeArray);
+  return shiftedString;
 }
 
 export {
