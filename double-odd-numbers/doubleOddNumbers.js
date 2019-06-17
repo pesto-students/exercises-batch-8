04d389e179f5e58ecd13e26bd32de207a1ae9d36
kunnu01 <kunalbhatia2409@gmail.com>
2019-06-18 01:41:51 +0530

    Solved doubleOddNumbers, instructor, joinArrays

--- double-odd-numbers/doubleOddNumbers.js
+++ double-odd-numbers/doubleOddNumbers.js
@@ -1,6 +1,12 @@
 
-function doubleOddNumbers(...args) {
-  return args;
+function doubleOddNumbers(array) {
+  const doubledArray = array.map((ele) => {
+    if (ele % 2 !== 0) {
+      return ele * 2;
+    }
+    return null;
+  });
+  return doubledArray.filter(ele => ele);
 }
 
 export {
