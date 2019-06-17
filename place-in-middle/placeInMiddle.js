15263151da3adb515839a4b65c3f844d69e1cb88
Pradyumna Newalkar <contactpradyumna1@gmail.com>
2019-06-17 20:21:55 +0000

    Added solution for place-in-middle

--- place-in-middle/placeInMiddle.js
+++ place-in-middle/placeInMiddle.js
@@ -1,6 +1,7 @@
 
-function placeInMiddle(...args) {
-  return args;
+function placeInMiddle(arr1, arr2) {
+  const mid = Math.floor(arr1.length / 2);
+  return [...arr1.slice(0, mid), ...arr2, ...arr1.slice(mid)];
 }
 
 export {
