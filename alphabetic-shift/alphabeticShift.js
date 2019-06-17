e087695db9ea03afb0ca7993e1e838a71367c2fb
Rakesh Tembhurne <tj2point0@gmail.com>
2019-06-18 04:23:58 +0530

    Complete alphabetic-shift

--- alphabetic-shift/alphabeticShift.js
+++ alphabetic-shift/alphabeticShift.js
@@ -1,6 +1,11 @@
+const nextChar = char => String.fromCharCode(char.charCodeAt(0) + 1);
 
-function alphabeticShift(...args) {
-  return args;
+function alphabeticShift(text) {
+  const characters = text.split('');
+  const replacedCharacters = characters.map(char => nextChar(char));
+  const replacedText = replacedCharacters.join('');
+
+  return replacedText;
 }
 
 export {
