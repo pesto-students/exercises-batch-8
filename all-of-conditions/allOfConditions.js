0e14888d3bf3fc19c5eaab2fcfd766b9872a2843
sourabhmodi20051 <sourabhmodi20051@gmail.com>
2019-06-18 00:56:49 +0530

    Solved All of Conditions

--- all-of-conditions/allOfConditions.js
+++ all-of-conditions/allOfConditions.js
@@ -1,8 +1,17 @@
-
 function allOfConditions(...args) {
-  return args;
+  let functions = [];
+  if (typeof args[0] === 'function') {
+    functions = args;
+    return function (arg) {
+      for (const fn in functions) {
+        if (functions[fn](arg) === false) {
+          return false;
+        }
+      }
+      return true;
+    };
+  }
+  throw new Error('Unexpected arguments to the function');
 }
 
-export {
-  allOfConditions,
-};
+export { allOfConditions };
