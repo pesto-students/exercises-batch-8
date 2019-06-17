c20add4ec6318b03740e47719b94a3871e52f3f7
Pradyumna Newalkar <contactpradyumna1@gmail.com>
2019-06-17 19:30:23 +0000

    Added solution for double-odd-numbers

--- double-odd-numbers/doubleOddNumbers.js
+++ double-odd-numbers/doubleOddNumbers.js
@@ -1,6 +1,11 @@
 
-function doubleOddNumbers(...args) {
-  return args;
+function doubleOddNumbers(arr) {
+  return arr.reduce((result, num) => {
+    if (num % 2 === 1) {
+      return [...result, num * 2];
+    }
+    return result;
+  }, []);
 }
 
 export {
