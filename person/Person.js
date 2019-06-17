56b4a79c77e0cd4f9d980baf7851e091c9b34302
Jaspreet Singh <erjaspreet57@gmail.com>
2019-06-18 04:43:42 +0530

    Solving person

--- person/Person.js
+++ person/Person.js
@@ -1,8 +1,22 @@
 
-function person(...args) {
-  return args;
+class Person {
+  constructor(firstName, lastName, dateOfBirth) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dateOfBirth = dateOfBirth;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  addDobDigits() {
+    // eslint-disable-next-line no-restricted-globals
+    const isNumber = character => !isNaN(character);
+    const sum = (acc, digit) => acc + Number(digit);
+
+    const digits = String(this.dateOfBirth).split('').filter(isNumber);
+    return digits.reduce(sum, 0);
+  }
 }
 
 export {
-  person,
+  Person,
 };
