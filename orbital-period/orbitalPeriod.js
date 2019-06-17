f3f80676daaae64db1033a1bd2f5731363f52192
Radheshyam Etikala <radhey1851@gmail.com>
2019-06-17 10:11:35 +0530

    Completed OrbitalPeriod

--- orbital-period/orbitalPeriod.js
+++ orbital-period/orbitalPeriod.js
@@ -1,6 +1,21 @@
+const orbitalPeriodEquationFromEarth = (altitudeInKms) => {
+  const GM = 398600.4418; // in km3s-2
+  const { PI } = Math;
+  const earthRadius = 6367.4447; // in km
+  const effectiveRadius = altitudeInKms + earthRadius; // in km
+  const orbitalPeriodInSecs = Math.sqrt((4 * (PI ** 2) * (effectiveRadius ** 3)) / GM);
+  return Math.round(orbitalPeriodInSecs);
+};
 
-function orbitalPeriod(...args) {
-  return args;
+function orbitalPeriod(satellites) {
+  const orbitalPeriodsOfSatellites = satellites.map((satellite) => {
+    const { name, avgAlt } = satellite;
+    return {
+      name,
+      orbitalPeriod: orbitalPeriodEquationFromEarth(avgAlt),
+    };
+  });
+  return orbitalPeriodsOfSatellites;
 }
 
 export {
