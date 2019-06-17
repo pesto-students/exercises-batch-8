5646842eb28000efac25d5e63a40f28b12b2a25f
aayushtiwari <arsenicaayush@gmail.com>
2019-06-18 03:58:25 +0530

    updaated

--- place-in-middle/placeInMiddle.js
+++ place-in-middle/placeInMiddle.js
@@ -1,7 +1,7 @@
 function placeInMiddle([...array1], [...array2]) {
   const { length } = array1;
-  const arrstart = array1.splice(0, length / 2);
-  return [...arrstart, ...array2, ...array1];
+  const arraystart = array1.splice(0, length / 2);
+  return [...arraystart, ...array2, ...array1];
 }
 
 export { placeInMiddle };
--- triple-and-filter/tripleAndFilter.js
+++ triple-and-filter/tripleAndFilter.js
@@ -1,8 +1,7 @@
-import { arrayExpression } from '@babel/types';
 
 function tripleAndFilter([...args]) {
-  const newarr = args.map(element => element * 3);
-  return newarr.filter(element => element % 5 === 0);
+  const newArray = args.map(element => element * 3);
+  return newArray.filter(element => element % 5 === 0);
 }
 
 export { tripleAndFilter };
