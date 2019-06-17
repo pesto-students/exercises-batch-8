deb29db165c0adc11a1a19f8b0a3e9e01b1dc816
Sebin Benjamin <sebinbenjamin@gmail.com>
2019-06-18 00:35:18 +0530

    completed exercise 'template-literals'

--- template-literals/templateLiterals.js
+++ template-literals/templateLiterals.js
@@ -1,6 +1,11 @@
 
-function templateLiterals(...args) {
-  return args;
+function templateLiterals() {
+  const name1 = 'Rooney';
+  const name2 = 'Ronaldo';
+  const name3 = 'Messi';
+  const name4 = 'Pogba';
+  const message = `There are 4 people on the football team. Their names are ${name1}, ${name2}, ${name3}, ${name4}.`;
+  return message;
 }
 
 export {
