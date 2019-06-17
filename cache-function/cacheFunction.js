5917243bc7e4525b419e07edfdb51058205d75ea
sourabhmodi20051 <sourabhmodi20051@gmail.com>
2019-06-17 20:25:30 +0530

    Solved Cache function

--- cache-function/cacheFunction.js
+++ cache-function/cacheFunction.js
@@ -1,8 +1,15 @@
-
-function cacheFunction(...args) {
-  return args;
+function cacheFunction(fn) {
+  const cache = {};
+  // eslint-disable-next-line func-names
+  return function () {
+    const key = JSON.stringify(...arguments); // eslint-disable-line
+    if (Object.prototype.hasOwnProperty.call(cache, key)) {
+      return cache[key];
+    }
+    const value = fn.apply(null, arguments); // eslint-disable-line
+    cache[key] = value;
+    return value;
+  };
 }
 
-export {
-  cacheFunction,
-};
+export { cacheFunction };
