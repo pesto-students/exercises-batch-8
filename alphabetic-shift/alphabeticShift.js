374d93f732282753fb786c66ad83ab200aa8d497
Pradyumna Newalkar <contactpradyumna1@gmail.com>
2019-06-17 22:57:55 +0000

    Added solution for alphabheticShift

--- alphabetic-shift/alphabeticShift.js
+++ alphabetic-shift/alphabeticShift.js
@@ -1,6 +1,17 @@
-
-function alphabeticShift(...args) {
-  return args;
+function shiftChar(char) {
+  const charCode = char.charCodeAt(0);
+  const SMALLA = 'a'.charCodeAt(0);
+  const CAPITALA = 'A'.charCodeAt(0);
+  if (charCode >= SMALLA) {
+    return String.fromCharCode(SMALLA + ((charCode - SMALLA + 1) % 26));
+  }
+  return String.fromCharCode(CAPITALA + ((charCode - CAPITALA + 1) % 26));
+}
+function alphabeticShift(str) {
+  if (str.length === 0) {
+    return '';
+  }
+  return shiftChar(str[0]) + alphabeticShift(str.substr(1));
 }
 
 export {
