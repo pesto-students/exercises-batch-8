db76fe9f61ad34d0aa9d5f6e6825dad2c2e6abc4
saurabh shetty <saurabh.shetty100@gmail.com>
2019-06-18 02:00:33 +0530

    solved placeInMiddle

--- place-in-middle/placeInMiddle.js
+++ place-in-middle/placeInMiddle.js
@@ -1,6 +1,8 @@
 
-function placeInMiddle(...args) {
-  return args;
+function placeInMiddle(initialArray, arrayToInsert) {
+  const resultArray = initialArray;
+  resultArray.splice(Math.floor(resultArray.length / 2), 0, ...arrayToInsert);
+  return resultArray;
 }
 
 export {
