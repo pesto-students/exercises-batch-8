671cd4366703245d087010c4e7318a7cd55dcf15
Rakesh Tembhurne <tj2point0@gmail.com>
2019-06-18 00:25:22 +0530

    Complete const-immutable

--- const-immutable/constImmutable.js
+++ const-immutable/constImmutable.js
@@ -1,6 +1,16 @@
 
-function constImmutable(...args) {
-  return args;
+function constImmutable() {
+  const account = {
+    username: 'pesto',
+  };
+  Object.defineProperty(account, 'password', {
+    value: 'initialPassword',
+    writable: false,
+    ennumerable: false,
+    configurable: false,
+  });
+
+  return account.password;
 }
 
 export {
