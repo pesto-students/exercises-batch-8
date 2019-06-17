f0d29d8511ff07ab376461b3c327fbbb3c883c61
Abdul Khuddus <khuddus1@gmail.com>
2019-06-18 00:31:28 +0530

    Complete sum of even args

--- sum-even-args/sumEvenArgs.js
+++ sum-even-args/sumEvenArgs.js
@@ -1,6 +1,11 @@
 
-function sumEvenArgs(...args) {
-  return args;
+function sumEvenArgs(...numbers) {
+  return numbers.reduce((acc, number) => {
+    if (number % 2 === 0) {
+      return acc + number;
+    }
+    return acc;
+  }, 0);
 }
 
 export {
