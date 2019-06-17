10d9c99a361a26da9e8cf796c1b67ee0561a220e
Abhinav Sharma <abhi18av@outlook.com>
2019-06-18 00:31:25 +0530

    solved the sum-even-args problem

--- sum-even-args/sumEvenArgs.js
+++ sum-even-args/sumEvenArgs.js
@@ -1,6 +1,11 @@
 
 function sumEvenArgs(...args) {
-  return args;
+  const anArray = args;
+  const evenNumbers = anArray.filter(x => x % 2 === 0);
+  if (evenNumbers.length >= 1) {
+    return evenNumbers.reduce((acc, sum) => acc + sum);
+  }
+  return 0;
 }
 
 export {
