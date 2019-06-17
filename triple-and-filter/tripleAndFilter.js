0a16626ad831108b09b7a769a4abf11a8000b950
Sebin Benjamin <sebinbenjamin@gmail.com>
2019-06-18 00:27:26 +0530

    Added comments explaining logic

--- triple-and-filter/tripleAndFilter.js
+++ triple-and-filter/tripleAndFilter.js
@@ -1,5 +1,7 @@
 
 function tripleAndFilter(numbers) {
+  // Since 3 and 5 have no common factors, we can skip tripling all numbers
+  // We only need to triple multiples of 5.
   const multiplesOf5 = numbers.filter(num => num % 5 === 0);
   const tripledNumbers = multiplesOf5.map(num => num * 3);
   return tripledNumbers;
