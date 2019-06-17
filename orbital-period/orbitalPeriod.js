951d4617f3403b1a49f3954143a48b506d50e373
saurabh shetty <saurabh.shetty100@gmail.com>
2019-06-17 07:24:01 +0530

    solved orbital

--- orbital-period/orbitalPeriod.js
+++ orbital-period/orbitalPeriod.js
@@ -1,6 +1,13 @@
+const GM = 398600.4418; // in km3s-2
+const earthRadius = 6367.4447; // in km
 
-function orbitalPeriod(...args) {
-  return args;
+function orbitalPeriod(satallites) {
+  const resultSatallites = satallites.map(satallite => ({
+    name: satallite.name,
+    orbitalPeriod:
+      Math.round(2 * Math.PI * Math.sqrt(((satallite.avgAlt + earthRadius) ** 3) / GM)),
+  }));
+  return resultSatallites;
 }
 
 export {
