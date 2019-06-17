253e8c7b601952aae233fc582f0d37689bfd4046
Abdul Khuddus <khuddus1@gmail.com>
2019-06-18 00:37:20 +0530

    Complete place in middle

--- place-in-middle/placeInMiddle.js
+++ place-in-middle/placeInMiddle.js
@@ -1,6 +1,8 @@
 
-function placeInMiddle(...args) {
-  return args;
+function placeInMiddle(array1, array2) {
+  const array1part1 = array1.slice(0, array1.length / 2);
+  const array1part2 = array1.slice(array1.length / 2, array1.length);
+  return [...array1part1, ...array2, ...array1part2];
 }
 
 export {
