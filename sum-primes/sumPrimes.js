18e65aeb215d14f5234f38df0b38ff2d105a1f15
saurabh shetty <saurabh.shetty100@gmail.com>
2019-06-17 06:10:47 +0530

    solved sum-primes partially

--- sum-primes/sumPrimes.js
+++ sum-primes/sumPrimes.js
@@ -1,6 +1,17 @@
 
-function sumPrimes(...args) {
-  return args;
+function sumPrimes(number) {
+  let allNumbers = [];
+  let index = 2;
+  while (index < number) {
+    allNumbers.push(index);
+    index += 1;
+  }
+  const allNumbersCopy = [...allNumbers];
+  allNumbersCopy.map((divisor) => {
+    allNumbers = [...allNumbers.filter(value => (value % divisor !== 0 || divisor === value))];
+    return null;
+  });
+  return allNumbers.reduce((a, b) => a + b);
 }
 
 export {
