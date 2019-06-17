5d4e35c14d3a9d6e01165ed328aea9d031f3600b
Radheshyam Etikala <radhey1851@gmail.com>
2019-06-18 00:55:02 +0530

    completed blockScoping

--- block-scoping/blockScoping.js
+++ block-scoping/blockScoping.js
@@ -1,6 +1,15 @@
+/* eslint-disable no-var */
 
-function blockScoping(...args) {
-  return args;
+
+function blockScoping(n) {
+  var callbacks = [];
+  var i;
+  for (i = 0; i <= 10; i += 1) {
+    callbacks.push((function (j) {
+      return () => j;
+    })(i));
+  }
+  return callbacks[n]();
 }
 
 export {
