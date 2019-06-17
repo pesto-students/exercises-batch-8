f5c6eed7dbd395c7dfb554eb50519a7a79a96e5e
Abhinav Sharma <abhi18av@outlook.com>
2019-06-18 04:15:28 +0530

    solved is-promise problem

--- is-promise/isPromise.js
+++ is-promise/isPromise.js
@@ -1,15 +1,12 @@
 
 // WIP
 function isPromise(...args) {
-let prom = new Promise(() => {});
-if(args[0] instanceof Promise) {
-  return true
-  } else {
-    return (Promise.resolve(args[0]) === prom); 
+  const prom = new Promise(() => {});
+  if (args[0] instanceof Promise) {
+    return true;
   }
+  return (Promise.resolve(args[0]) === prom);
 }
-
-
 
 export {
   isPromise,
