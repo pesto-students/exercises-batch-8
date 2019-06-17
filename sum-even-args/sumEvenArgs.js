8f77e0bc3ab581f55f3b84c14c03a42503064099
saurabh shetty <saurabh.shetty100@gmail.com>
2019-06-18 00:58:34 +0530

    solbeved sumEventArgs

--- sum-even-args/sumEvenArgs.js
+++ sum-even-args/sumEvenArgs.js
@@ -1,6 +1,7 @@
-
-function sumEvenArgs(...args) {
-  return args;
+const isEven = number => number % 2 === 0;
+const sum = (a, b) => a + b;
+function sumEvenArgs(...numbers) {
+  return numbers.filter(isEven).reduce(sum, 0);
 }
 
 export {
