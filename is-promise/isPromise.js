cd3e5c65e97747f912f43cc6b09639a37ac915fb
Jaspreet Singh <erjaspreet57@gmail.com>
2019-06-18 04:32:41 +0530

    Solving is-promise

--- is-promise/isPromise.js
+++ is-promise/isPromise.js
@@ -1,6 +1,7 @@
 
-function isPromise(...args) {
-  return args;
+function isPromise(value) {
+  // value is promise only if it is object with then function. i.e thennable object
+  return Boolean(value && typeof value.then === 'function');
 }
 
 export {
