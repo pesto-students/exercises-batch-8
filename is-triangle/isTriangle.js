1eea24a6c2fac5cf43df9443dff8e94e611e9059
Rakesh Tembhurne <tj2point0@gmail.com>
2019-06-17 19:23:28 +0530

    Complete is-triangle

--- is-triangle/isTriangle.js
+++ is-triangle/isTriangle.js
@@ -1,6 +1,15 @@
 
-function isTriangle(...args) {
-  return args;
+function isTriangle(...sides) {
+  const triangleConditions = sides.map((side) => {
+    const otherSides = sides.filter(thisSide => thisSide !== side);
+    const otherSideSum = otherSides.reduce((sidesTotal, otherSide) => sidesTotal + otherSide);
+
+    return otherSideSum > side;
+  });
+
+  const itIsTriangle = triangleConditions.every(cond => cond === true);
+
+  return itIsTriangle;
 }
 
 export {
