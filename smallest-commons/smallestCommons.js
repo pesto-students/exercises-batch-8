199622cefc151546a9ea03d04fe080f6c7a88b30
Radheshyam Etikala <radhey1851@gmail.com>
2019-06-17 09:46:58 +0530

    Completed Smallest Commons

--- smallest-commons/smallestCommons.js
+++ smallest-commons/smallestCommons.js
@@ -1,6 +1,29 @@
 
-function smallestCommons(...args) {
-  return args;
+// Euclids method
+function gcd(a, b) {
+  if (a === 0) {
+    return b;
+  }
+  return gcd(b % a, a);
+}
+
+function lcm(numA, numB) {
+  return (numA * numB) / gcd(numA, numB);
+}
+
+function range(min, max) {
+  return [...Array(max + 1).keys()].slice(min);
+}
+
+function smallestCommons(numsRange) {
+  numsRange.sort((a, b) => a - b);
+  const [minNum, maxNum] = numsRange;
+  const numbers = range(minNum, maxNum);
+  let multiple = minNum;
+  numbers.forEach((num) => {
+    multiple = lcm(multiple, num);
+  });
+  return multiple;
 }
 
 export {
