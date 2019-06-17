6e565670599b14da70475028fdf8eb74e18de7b3
Jaspreet Singh <erjaspreet57@gmail.com>
2019-06-18 01:45:41 +0530

    Solve template-literals

--- template-literals/templateLiterals.js
+++ template-literals/templateLiterals.js
@@ -1,6 +1,23 @@
 
-function templateLiterals(...args) {
-  return args;
+function templateLiterals() {
+  const teamName = 'football';
+  const people = [{
+    name: 'Rooney',
+    role: 'CF',
+  },
+  {
+    name: 'Ronaldo',
+    role: 'LW',
+  },
+  {
+    name: 'Messi',
+    role: 'RW',
+  },
+  {
+    name: 'Pogba',
+    role: 'CM',
+  }];
+  return `There are ${people.length} people on the ${teamName} team. Their names are ${people.map(player => player.name).join(', ')}.`;
 }
 
 export {
