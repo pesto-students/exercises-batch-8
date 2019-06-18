16f068faa8ddae34993c0e0d7e6e2624e5993843
saurabh shetty <saurabh.shetty100@gmail.com>
2019-06-18 07:24:14 +0530

    solved promisAll

--- promise-all-props/promiseAllProps.js
+++ promise-all-props/promiseAllProps.js
@@ -1,6 +1,12 @@
 
-function promiseAllProps(...args) {
-  return args;
+function promiseAllProps(promisesObject) {
+  const resultObject = {};
+  const promisesInArray = Object.keys(promisesObject).map(key => promisesObject[key]);
+  return Promise.all(promisesInArray)
+    .then(results => results.forEach((result, i) => {
+      resultObject[Object.keys(promisesObject)[i]] = result;
+    }))
+    .then(() => resultObject);
 }
 
 export {
