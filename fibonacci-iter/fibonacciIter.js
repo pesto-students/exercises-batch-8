cf754da4007d95f1ba85b885a6c38407546d071a
Abdul Khuddus <khuddus1@gmail.com>
2019-06-19 01:16:45 +0530

    Complete fibanocciiter

--- fibonacci-iter/fibonacciIter.js
+++ fibonacci-iter/fibonacciIter.js
@@ -1,7 +1,21 @@
 
-function fibonacciIter(...args) {
-  return args;
-}
+const fibonacciIter = {
+  [Symbol.iterator]() {
+    this.first = 0;
+    this.second = 1;
+    return {
+      next: () => {
+        const temp = this.second;
+        this.second = this.second + this.first;
+        this.first = temp;
+        return {
+          value: this.second,
+          done: false,
+        };
+      },
+    };
+  },
+};
 
 export {
   fibonacciIter,
