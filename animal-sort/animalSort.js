9ec3398a8729270d44bc238c10e65a0bb32b9f3c
Sebin Benjamin <sebinbenjamin@gmail.com>
2019-06-19 05:06:57 +0530

    Completed exercise 'animal-sort'

--- animal-sort/animalSort.js
+++ animal-sort/animalSort.js
@@ -1,6 +1,18 @@
+const animalNameComparator = (animalA, animalB) => {
+  if (animalA.name > animalB.name) return 1;
+  return -1;
+};
 
-function animalSort(...args) {
-  return args;
+const animalLegComparator = ((animalA, animalB) => {
+  const legDiff = animalA.numberOfLegs - animalB.numberOfLegs;
+  const hasSameNoOfLegs = legDiff === 0;
+  if (hasSameNoOfLegs) return animalNameComparator(animalA, animalB);
+  return animalA.numberOfLegs - animalB.numberOfLegs;
+});
+
+function animalSort(animalObjects) {
+  if (animalObjects.length === 0) return animalObjects;
+  return animalObjects.sort(animalLegComparator);
 }
 
 export {
