addc74699e059e20082f1b6756632267cc405029
prateekinstillion <Prateek Madaan>
2019-06-18 10:36:14 +0530

    Solved the reject pronlem

--- reject-promise/rejectPromise.js
+++ reject-promise/rejectPromise.js
@@ -1,6 +1,9 @@
 
-function rejectPromise(...args) {
-  return args;
+function rejectPromise() {
+  const promise = new Promise((resolve) => {
+    resolve('REJECTED!');
+  });
+  return promise;
 }
 
 export {
