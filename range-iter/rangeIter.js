fae5eaaac79741c9b32bbd262f95711bd4903f0c
Abhinav Sharma <abhi18av@outlook.com>
2019-06-19 00:26:34 +0530

    completed range-iter problem

--- range-iter/rangeIter.js
+++ range-iter/rangeIter.js
@@ -1,7 +1,25 @@
-
-function rangeIter(...args) {
-  return args;
+function rangeIter(from, to) {
+  const isNumber = n => typeof n === 'number';
+  if (!isNumber(from) || !isNumber(to)) {
+    throw new TypeError('Enter valid number');
+  }
+  let currIndex = from;
+  return {
+    [Symbol.iterator]: function () {
+      currIndex = from;
+      return this;
+    },
+    next() {
+      if (currIndex <= to) {
+        const val = { done: false, value: currIndex };
+        currIndex++;
+        return val;
+      }
+      return { done: true };
+    },
+  };
 }
+
 
 export {
   rangeIter,
