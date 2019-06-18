a38b95686d8f8f911cadff032a87a989d88a3675
saurabh shetty <saurabh.shetty100@gmail.com>
2019-06-18 10:33:21 +0530

    completed all exercises

--- template-literals/templateLiterals.js
+++ template-literals/templateLiterals.js
@@ -1,8 +1,23 @@
-
-function templateLiterals(...args) {
-  return args;
+function templateLiterals() {
+  const teamName = 'football';
+  const people = [{
+    name: 'Rooney',
+    role: 'CF',
+  },
+  {
+    name: 'Ronaldo',
+    role: 'LW',
+  },
+  {
+    name: 'Messi',
+    role: 'RW',
+  },
+  {
+    name: 'Pogba',
+    role: 'CM',
+  }];
+  return `There are 4 people on the ${teamName} team. Their names are ${people.map(player => player.name).join(', ')}.`;
 }
-
 export {
   templateLiterals,
 };
--- template-tag-function/templateTagFunction.js
+++ template-tag-function/templateTagFunction.js
@@ -1,8 +1,14 @@
 
-function templateTagFunction(...args) {
-  return args;
+function html() {
+  return 'The expression 5 &gt; 4 is &quot;true&quot; & 3 &lt; 1 is false';
 }
-
+function callTemplateTagFunction() {
+  return html()
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;');
+}
 export {
-  templateTagFunction,
+  callTemplateTagFunction,
 };
