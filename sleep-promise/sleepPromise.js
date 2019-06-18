3cf009c5bf5ab8dfcf53bddabc3cad082f4551be
Jaspreet Singh <erjaspreet57@gmail.com>
2019-06-18 23:50:15 +0530

    Solving sleep-promise

--- sleep-promise/sleepPromise.js
+++ sleep-promise/sleepPromise.js
@@ -1,8 +1,20 @@
+function sleep(timeout) {
+  const sleepPromise = new Promise((res) => {
+    setTimeout(() => {
+      res();
+    }, timeout);
+  });
 
-function sleepPromise(...args) {
-  return args;
+  // Act as function, and pass value through when used in promise chain
+  const promiseFunction = value => sleepPromise.then(() => value);
+
+  // Normal promise
+  promiseFunction.then = (...args) => sleepPromise.then(...args);
+  promiseFunction.catch = Promise.resolve().catch;
+
+  return promiseFunction;
 }
 
 export {
-  sleepPromise,
+  sleep,
 };
