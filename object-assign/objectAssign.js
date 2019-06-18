02f1e660e8fae72170b0106ee5d79c2e6100a091
Pradyumna Newalkar <contactpradyumna1@gmail.com>
2019-06-18 23:31:59 +0000

    Added solution for object-assign

--- object-assign/objectAssign.js
+++ object-assign/objectAssign.js
@@ -1,6 +1,17 @@
+function objectAssign(dest, ...sources) {
+  if (sources.length === 0) {
+    return dest;
+  }
 
-function objectAssign(...args) {
-  return args;
+  for (const source of sources) {
+    if (typeof source === 'object' && source !== null) {
+      for (const key of Object.keys(source)) {
+        // eslint-disable-next-line no-param-reassign
+        dest[key] = source[key];
+      }
+    }
+  }
+  return dest;
 }
 
 export {
