9738010a28ec5c70c1eb99c45526d403f0804f1d
kunnu01 <kunalbhatia2409@gmail.com>
2019-06-19 01:48:28 +0530

    Solved rangeIter

--- range-iter/rangeIter.js
+++ range-iter/rangeIter.js
@@ -1,6 +1,32 @@
 
-function rangeIter(...args) {
-  return args;
+function rangeIter(start, end) {
+  const isNumber = num => typeof num === 'number';
+
+  if (!end) {
+    throw new TypeError(`${end} is not a number`);
+  }
+
+  if (!isNumber(end)) {
+    throw new TypeError(
+      `${end} is not a number`,
+    );
+  }
+
+  let currentIndex = start;
+  return {
+    [Symbol.iterator]() {
+      currentIndex = start;
+      return this;
+    },
+    next() {
+      if (currentIndex <= end) {
+        const value = { done: false, value: currentIndex };
+        currentIndex += 1;
+        return value;
+      }
+      return { done: true };
+    },
+  };
 }
 
 export {
