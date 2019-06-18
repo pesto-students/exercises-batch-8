9f26cbbaca21f38b66346fa1de58cfc69ec71dc3
Radheshyam Etikala <radhey1851@gmail.com>
2019-06-19 02:02:59 +0530

    completed fibonacciIter

--- fibonacci-iter/fibonacciIter.js
+++ fibonacci-iter/fibonacciIter.js
@@ -1,7 +1,21 @@
 
-function fibonacciIter(...args) {
-  return args;
-}
+const fibonacciIter = {
+  [Symbol.iterator]() {
+    let firstNumber = 0;
+    let currentNumber = 1;
+    let nextNumber;
+    const iterator = {
+      next() {
+        nextNumber = firstNumber + currentNumber;
+        const returnObj = { value: nextNumber, done: false };
+        firstNumber = currentNumber;
+        currentNumber = nextNumber;
+        return returnObj;
+      },
+    };
+    return iterator;
+  },
+};
 
 export {
   fibonacciIter,
