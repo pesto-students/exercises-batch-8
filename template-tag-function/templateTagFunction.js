5804731bf3ee50e2c421c7295eba38521889526b
sourabhmodi20051 <sourabhmodi20051@gmail.com>
2019-06-18 12:48:01 +0530

    Solved Template Tag Function

--- template-tag-function/templateTagFunction.js
+++ template-tag-function/templateTagFunction.js
@@ -1,8 +1,18 @@
-
-function templateTagFunction(...args) {
-  return args;
+function escape(string) {
+  return string
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;');
 }
 
-export {
-  templateTagFunction,
-};
+function html(args) {
+  return escape(args[0]);
+}
+
+function callTemplateTagFunction() {
+  const escapeOnce = html`The expression 5 > 4 is "true" & 3 < 1 is false`;
+  return escape(escapeOnce);
+}
+
+export { callTemplateTagFunction };
