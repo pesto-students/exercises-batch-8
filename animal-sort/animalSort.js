d9d619229d623525051ebdb0588daf6ece64525c
Abdul Khuddus <khuddus1@gmail.com>
2019-06-19 03:49:45 +0530

    Complete animal sort

--- animal-sort/animalSort.js
+++ animal-sort/animalSort.js
@@ -1,6 +1,18 @@
 
-function animalSort(...args) {
-  return args;
+function animalSort(animals) {
+  const alphabeticallySortedAnimals = animals.sort((animal1, animal2) => {
+    const nameA = animal1.name.toUpperCase();
+    const nameB = animal2.name.toUpperCase();
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+    return 0;
+  });
+  // eslint-disable-next-line max-len
+  return alphabeticallySortedAnimals.sort((animal1, animal2) => animal1.numberOfLegs - animal2.numberOfLegs);
 }
 
 export {
