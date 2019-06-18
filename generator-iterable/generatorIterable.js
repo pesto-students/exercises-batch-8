d5d2d368f69fb07e77718d48fcc24fa6ef50db26
kunnu01 <kunalbhatia2409@gmail.com>
2019-06-19 00:11:27 +0530

    Solved generatorIterable

--- generator-iterable/generatorIterable.js
+++ generator-iterable/generatorIterable.js
@@ -1,6 +1,15 @@
 
-function generatorIterable(...args) {
-  return args;
+function* generatorIterable() {
+  // if (value < 5) {
+  //   yield value;
+  //   return generatorIterable(value + 1);
+  // }
+  // return value;
+  let i = 1;
+  while (i <= 5) {
+    yield i;
+    i += 1;
+  }
 }
 
 export {
