8834064af98812a5e66dbdacfdd9d329786b89e2
sourabhmodi20051 <sourabhmodi20051@gmail.com>
2019-06-18 23:53:08 +0530

    Solved Alphabetic Shift

--- alphabetic-shift/alphabeticShift.js
+++ alphabetic-shift/alphabeticShift.js
@@ -1,6 +1,39 @@
+function isUpperCaseAlphabet(charCode) {
+  if (charCode <= 90 && charCode >= 65) {
+    return true;
+  }
+  return false;
+}
 
-function alphabeticShift(...args) {
-  return args;
+function isLowerCaseAlphabet(charCode) {
+  if (charCode <= 122 && charCode >= 97) {
+    return true;
+  }
+  return false;
+}
+
+function isAlphabet(charCode) {
+  if (isLowerCaseAlphabet(charCode) || isUpperCaseAlphabet(charCode)) {
+    return true;
+  }
+  return false;
+}
+
+function shift(character, shiftCount) {
+  const charCode = character.charCodeAt(0);
+  if (!isAlphabet(charCode)) {
+    throw new Error(`Invalid character ${character} in input string`);
+  }
+  if (isUpperCaseAlphabet(charCode)) {
+    const resultantCharCode = ((charCode + shiftCount) % 65) + 65;
+    return String.fromCharCode(resultantCharCode);
+  }
+  const resultantCharCode = ((charCode + shiftCount) % 97) + 97;
+  return String.fromCharCode(resultantCharCode);
+}
+
+function alphabeticShift(string) {
+  return string.split('').map(character => shift(character, 1)).join('');
 }
 
 export {
