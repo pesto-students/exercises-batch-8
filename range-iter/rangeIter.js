94c4e44a1545f5ff144a611ba93cea001aa48d46
Jaspreet Singh <erjaspreet57@gmail.com>
2019-06-19 00:29:47 +0530

    Solving range-iter

--- range-iter/rangeIter.js
+++ range-iter/rangeIter.js
@@ -1,6 +1,17 @@
-
-function rangeIter(...args) {
-  return args;
+function rangeIter(fromNum, toNum) {
+  // eslint-disable-next-line no-restricted-globals
+  if (isNaN(fromNum)) {
+    throw new TypeError(`${typeof fromNum} is not a number`);
+  }
+  // eslint-disable-next-line no-restricted-globals
+  if (isNaN(toNum)) {
+    throw new TypeError(`${typeof fromNum} is not a number`);
+  }
+  const range = [];
+  for (let i = fromNum; i <= toNum; i += 1) {
+    range.push(i);
+  }
+  return range;
 }
 
 export {
