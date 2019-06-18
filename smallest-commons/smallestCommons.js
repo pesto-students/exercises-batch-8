c6e7a889847fdc179ab532355f01f99aa21c7328
kunnu01 <kunalbhatia2409@gmail.com>
2019-06-18 13:25:08 +0530

    Solved smallestCommon and userCheckPassword

--- smallest-commons/smallestCommons.js
+++ smallest-commons/smallestCommons.js
@@ -1,6 +1,21 @@
+const greatestCommonDivisor = (num1, num2) => (
+  num2 === 0 ? num1 : greatestCommonDivisor(num2, num1 % num2)
+);
 
-function smallestCommons(...args) {
-  return args;
+const leastCommonMultiple = (num1, num2) => (
+  (num1 * num2) / greatestCommonDivisor(num1, num2)
+);
+
+function smallestCommons(range) {
+  let [lowerBound, upperBound] = range;
+  if (lowerBound > upperBound) {
+    [lowerBound, upperBound] = [upperBound, lowerBound];
+  }
+  let smallestCommon = lowerBound;
+  for (let i = upperBound; i >= lowerBound; i -= 1) {
+    smallestCommon = leastCommonMultiple(smallestCommon, i);
+  }
+  return smallestCommon;
 }
 
 export {
