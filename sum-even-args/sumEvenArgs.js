bca7e1bb6e64e9b8edaf03f65783a36b99896f31
kunnu01 <kunalbhatia2409@gmail.com>
2019-06-18 12:43:24 +0530

    solved sumEvenArgs and templateLiterals

--- sum-even-args/sumEvenArgs.js
+++ sum-even-args/sumEvenArgs.js
@@ -1,6 +1,9 @@
 
-function sumEvenArgs(...args) {
-  return args;
+function sumEvenArgs(...numbers) {
+  const isEven = num => num % 2 === 0;
+  const evenNumbers = numbers.filter(isEven);
+  const sumOfEvenNumbers = evenNumbers.reduce((sum, num) => sum + num, 0);
+  return sumOfEvenNumbers;
 }
 
 export {
