de2b162b662479908e36cfdecb958ab6adbf1ff0
Abhinav Sharma <abhi18av@outlook.com>
2019-06-19 00:48:46 +0530

    completed cache-function problem

--- cache-function/cacheFunction.js
+++ cache-function/cacheFunction.js
@@ -1,7 +1,17 @@
 
-function cacheFunction(...args) {
-  return args;
-}
+ const cacheFunction = (fn) => {
+  const cache = {};
+  return (...args) => {
+    const n = args[0];
+    if (n in cache) {
+      return cache[n];
+    } else {
+      const result = fn(n);
+      cache[n] = result;
+      return result;
+    }
+  };
+};
 
 export {
   cacheFunction,
