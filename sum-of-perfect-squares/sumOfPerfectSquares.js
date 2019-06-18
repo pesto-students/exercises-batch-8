d9694ffa3f2ae3fcadc0a4bcb3ec9876970846d4
Abhinav Sharma <abhi18av@outlook.com>
2019-06-19 01:33:10 +0530

    completed sum-of-perfect-squares problem

--- sum-of-perfect-squares/sumOfPerfectSquares.js
+++ sum-of-perfect-squares/sumOfPerfectSquares.js
@@ -1,7 +1,23 @@
 
-function sumOfPerfectSquares(...args) {
-  return args;
+function sumOfPerfectSquares(number) {
+  if (number === 0) {
+    return 0;
+  }
+  if (number === 1) {
+    return 1;
+  }
+  let perfectSquareCount = 0;
+  const lastPerfectSquareRoot = parseInt(Math.sqrt(number));
+  let minimumPerfectSquareCount = Number.MAX_VALUE;
+  for (let i = lastPerfectSquareRoot; i >= 1; i--) {
+    perfectSquareCount = 1 + sumOfPerfectSquares(number - (i ** 2));
+    if (perfectSquareCount < minimumPerfectSquareCount) {
+      minimumPerfectSquareCount = perfectSquareCount;
+    }
+  }
+  return minimumPerfectSquareCount;
 }
+
 
 export {
   sumOfPerfectSquares,
