7b64b67940a808c80c6fc6dc98b81bee4f40f5eb
Abdul Khuddus <khuddus1@gmail.com>
2019-06-18 15:57:32 +0530

    Complete round to

--- round-to/roundTo.js
+++ round-to/roundTo.js
@@ -1,20 +1,30 @@
 function roundTo(num, precision) {
-  this.up = (number, precisionDigits) => {
-    const multiplier = 10 ** precisionDigits;
-    const multipliedNumber = number * multiplier;
-    const isNumberUp = multipliedNumber > Math.floor(multipliedNumber);
-    return isNumberUp ? (multipliedNumber + 1) / multiplier : multipliedNumber / multiplier;
-  };
-  this.down = (number, precisionDigits) => {
-    const multiplier = 10 ** precisionDigits;
-    const multipliedNumber = number * multiplier;
-    return multipliedNumber / multiplier;
-  };
+  const number = num < 0 ? -num : num;
   const multiplier = 10 ** precision;
-  const multipliedNumber = num * multiplier;
-  const isNumberUp = multipliedNumber > Math.floor(multipliedNumber);
-  return isNumberUp ? (multipliedNumber + 1) / multiplier : multipliedNumber / multiplier;
+  const multipliedNumber = (number * multiplier).toFixed(2);
+  const upperBoundary = Math.ceil(multipliedNumber);
+  const lowerBoundary = Math.floor(multipliedNumber);
+  const isNumberUp = (upperBoundary - multipliedNumber) <= (multipliedNumber - lowerBoundary);
+  const roundedNum = isNumberUp ? upperBoundary / multiplier : lowerBoundary / multiplier;
+  return num < 0 ? -roundedNum : roundedNum;
 }
+roundTo.up = (num, precisionDigits) => {
+  const number = num;
+  const multiplier = 10 ** precisionDigits;
+  const multipliedNumber = (number * multiplier).toFixed(2);
+  const upperBoundary = Math.ceil(multipliedNumber);
+  const lowerBoundary = Math.floor(multipliedNumber);
+  const isNumberUp = lowerBoundary !== upperBoundary;
+  const roundedNum = isNumberUp ? upperBoundary / multiplier : lowerBoundary / multiplier;
+  return roundedNum;
+};
+roundTo.down = (num, precisionDigits) => {
+  const number = num;
+  const multiplier = 10 ** precisionDigits;
+  const multipliedNumber = Math.floor(number * multiplier);
+  const roundedNum = multipliedNumber / multiplier;
+  return roundedNum;
+};
 
 export {
   roundTo,
