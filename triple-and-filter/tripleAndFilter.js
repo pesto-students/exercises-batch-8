55292255de71c4ff1e3536b491877ad8984c605b
Radheshyam Etikala <radhey1851@gmail.com>
2019-06-18 12:16:48 +0530

    completed tripleAndFilter

--- triple-and-filter/tripleAndFilter.js
+++ triple-and-filter/tripleAndFilter.js
@@ -1,6 +1,8 @@
 
-function tripleAndFilter(...args) {
-  return args;
+function tripleAndFilter(numbers) {
+  const isDivisibleByFive = num => num % 5 === 0;
+  const filteredNumbers = numbers.filter(isDivisibleByFive);
+  return filteredNumbers.map(num => num * 3);
 }
 
 export {
