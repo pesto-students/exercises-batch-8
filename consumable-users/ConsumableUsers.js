f6bbe975f000e94bfbda9acc4e1987e4c057a636
sourabhmodi20051 <sourabhmodi20051@gmail.com>
2019-06-19 00:51:25 +0530

    Refactor consumable users

--- consumable-users/ConsumableUsers.js
+++ consumable-users/ConsumableUsers.js
@@ -1,13 +1,10 @@
 
 function ConsumableUsers() {
-  this.users = ['Alice', 'Bob'];
+  this.users = ['user: Alice', 'user: Bob'];
   this.size = 2;
   this.nextUser = function () {
     this.size -= 1;
-    if (this.users.length === 0) {
-      return undefined;
-    }
-    return `user: ${this.users.shift()}`;
+    return this.users.shift();
   };
   this.done = function () {
     return this.size === -1;
