4dd1fd5972be33e31dea117ae8d620cdf5b7d92a
kunnu01 <kunalbhatia2409@gmail.com>
2019-06-18 20:59:59 +0530

    Solved templateTagFunction

--- template-tag-function/templateTagFunction.js
+++ template-tag-function/templateTagFunction.js
@@ -1,8 +1,13 @@
 
-function templateTagFunction(...args) {
-  return args;
+function callTemplateTagFunction() {
+  const html = 'The expression 5 > 4 is "true" & 3 < 1 is false';
+  return html
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/&/g, '&amp;');
 }
 
 export {
-  templateTagFunction,
+  callTemplateTagFunction,
 };
