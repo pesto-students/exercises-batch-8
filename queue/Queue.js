421434fdba2a764b2b1346588482d15b02abe837
Radheshyam Etikala <radhey1851@gmail.com>
2019-06-18 08:27:44 +0530

    Completed Queue

--- queue/Queue.js
+++ queue/Queue.js
@@ -1,8 +1,46 @@
 
-function queue(...args) {
-  return args;
+class Queue {
+  constructor() {
+    this.list = [];
+  }
+
+  dequeue() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    return this.list.shift();
+  }
+
+  enqueue(num) {
+    this.list.push(num);
+  }
+
+  isEmpty() {
+    return this.list.length === 0;
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    return this.list[0];
+  }
+
+  toString(stringifier) {
+    if (stringifier) {
+      const stringifiedList = this.list
+        .map((item) => {
+          if (item instanceof Object) {
+            return stringifier(item);
+          }
+          return item.toString();
+        });
+      return stringifiedList.join(',');
+    }
+    return this.list.toString();
+  }
 }
 
 export {
-  queue,
+  Queue,
 };
