ec1d67b72b620bd1d2e5a42a5ecf38ce8eef3803
Sebin Benjamin <sebinbenjamin@gmail.com>
2019-06-19 00:05:46 +0530

    Completed exercise 'person'

--- person/Person.js
+++ person/Person.js
@@ -1,8 +1,19 @@
+/* eslint-disable class-methods-use-this */
+class Person {
+  constructor(firstName, lastName, dateOfBirth) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dateOfBirth = dateOfBirth;
+  }
 
-function person(...args) {
-  return args;
+  addDobDigits() {
+    const dateWithoutSeparator = [...this.dateOfBirth].filter(char => char !== '/');
+    const dateNumberArray = dateWithoutSeparator.map(Number);
+    const sumOfDateDigits = dateNumberArray.reduce((sum, digit) => sum + digit, 0);
+    return sumOfDateDigits;
+  }
 }
 
 export {
-  person,
+  Person,
 };
