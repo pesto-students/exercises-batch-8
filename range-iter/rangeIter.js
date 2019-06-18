dea450857c92ff60cbb79a55331d459b95e5fd3b
saurabh shetty <saurabh.shetty100@gmail.com>
2019-06-19 01:19:28 +0530

    solved rangeIter

--- range-iter/rangeIter.js
+++ range-iter/rangeIter.js
@@ -1,6 +1,13 @@
 
-function rangeIter(...args) {
-  return args;
+function rangeIter(lb, ub) {
+  if (ub < lb) {
+    return [];
+  }
+  if (!lb || !ub || !Number(ub) || !Number(lb)) {
+    throw new TypeError('Missing or invalid values');
+  }
+  const range = [...Array(Math.abs(ub - lb + 1)).keys()].map(x => x + lb);
+  return range;
 }
 
 export {
