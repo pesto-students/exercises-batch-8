b9f7ea588b0e53340aa7eaaa537d8282f6dbd178
Abhinav Sharma <abhi18av@outlook.com>
2019-06-19 01:58:43 +0530

    completed diff-array problem

--- diff-array/diffArray.js
+++ diff-array/diffArray.js
@@ -1,7 +1,10 @@
 
-function diffArray(...args) {
-  return args;
+function diffArray(array1, array2) {
+  const onlyInArray1 = array1.filter(element => !array2.includes(element));
+  const onlyInArray2 = array2.filter(element => !array1.includes(element));
+  return [...onlyInArray1, ...onlyInArray2];
 }
+
 
 export {
   diffArray,
