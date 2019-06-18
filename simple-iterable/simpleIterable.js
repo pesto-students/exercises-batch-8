613fa5f2fcaa275b188c77c618b09e1c0900b730
Pradyumna Newalkar <contactpradyumna1@gmail.com>
2019-06-18 19:28:57 +0000

    Added solution for simple-iterable

--- simple-iterable/simpleIterable.js
+++ simple-iterable/simpleIterable.js
@@ -1,6 +1,21 @@
 
-function simpleIterable(...args) {
-  return args;
+function simpleIterable() {
+  return {
+    values: [1, 2, 3, 4, 5],
+    current: 0,
+    [Symbol.iterator]() {
+      return this;
+    },
+    next() {
+      const value = this.values[this.current];
+      const done = value === undefined;
+      this.current += 1;
+      return {
+        value,
+        done,
+      };
+    },
+  };
 }
 
 export {
