676a05f2dbc46c865aee76df61050f07ad0065dc
Radheshyam Etikala <radhey1851@gmail.com>
2019-06-18 12:13:27 +0530

    Completed sumEvenArgs

--- sum-even-args/sumEvenArgs.js
+++ sum-even-args/sumEvenArgs.js
@@ -1,6 +1,9 @@
 
-function sumEvenArgs(...args) {
-  return args;
+function sumEvenArgs(...numbers) {
+  const sum = (a, b) => a + b;
+  const isEven = num => num % 2 === 0;
+  const evenNumbers = numbers.filter(isEven);
+  return evenNumbers.reduce(sum, 0);
 }
 
 export {
