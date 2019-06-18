020dd967ef0fb6cf8bf4b6a04a03baae72788f98
Abdul Khuddus <khuddus1@gmail.com>
2019-06-18 07:51:07 +0530

    Complete promise all props

--- promise-all-props/promiseAllProps.js
+++ promise-all-props/promiseAllProps.js
@@ -1,6 +1,10 @@
 
-function promiseAllProps(...args) {
-  return args;
+function promiseAllProps(promisesObject) {
+  const promises = Object.keys(promisesObject).reduce((acc, key) => {
+    acc[key] = promisesObject[key];
+    return acc;
+  }, []);
+  return Promise.all(promises);
 }
 
 export {
