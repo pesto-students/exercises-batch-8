67cdbfcc46086f7bc70249117c9f42693fd32c47
sourabhmodi20051 <sourabhmodi20051@gmail.com>
2019-06-18 11:40:21 +0530

    Solved - Const Immutable, Double Odd Numbers and Create Student

--- double-odd-numbers/doubleOddNumbers.js
+++ double-odd-numbers/doubleOddNumbers.js
@@ -1,8 +1,9 @@
-
-function doubleOddNumbers(...args) {
-  return args;
+function doubleOddNumbers(numbers) {
+  const isOdd = number => number % 2 === 1;
+  const double = number => number * 2;
+  const oddNumbers = numbers.filter(isOdd);
+  const doubledOddNumbers = oddNumbers.map(double);
+  return doubledOddNumbers;
 }
 
-export {
-  doubleOddNumbers,
-};
+export { doubleOddNumbers };
