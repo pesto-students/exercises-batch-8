bdebf5612ae62320d6fa7050fa7fda2a993a79b6
sourabhmodi20051 <sourabhmodi20051@gmail.com>
2019-06-18 12:02:40 +0530

    Solved Map Filter Reduce

--- map-filter-and-reduce/mapFilterAndReduce.js
+++ map-filter-and-reduce/mapFilterAndReduce.js
@@ -1,8 +1,11 @@
-
-function mapFilterAndReduce(...args) {
-  return args;
+function mapFilterAndReduce(array) {
+  const lengthFilterPredicate = string => string.split('').length < 5;
+  const mappedArray = array.map(obj => obj.firstName);
+  const lengthFilteredArray = mappedArray.filter(lengthFilterPredicate);
+  return lengthFilteredArray.reduce(
+    (object, currentItem) => ({ ...object, [currentItem]: currentItem.length }),
+    {},
+  );
 }
 
-export {
-  mapFilterAndReduce,
-};
+export { mapFilterAndReduce };
