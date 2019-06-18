7d67040a15cdc14bc338307600db7b1ed49c5a0e
prateekinstillion <Prateek Madaan>
2019-06-19 05:12:57 +0530

    Solved the built in iterables

--- built-in-iterables/builtInIterables.test.js
+++ built-in-iterables/builtInIterables.test.js
@@ -1,88 +1,89 @@
-describe('Array is a built-in iterable object', () => {
-  const arr = ['a', 'B', 'see'];
+describe("Array is a built-in iterable object", () => {
+  const arr = ["a", "B", "see"];
 
-  describe('the iterator', () => {
-    it('an array has an iterator, which is a function', () => {
+  describe("the iterator", () => {
+    it("an array has an iterator, which is a function", () => {
       const iterator = arr[Symbol.iterator];
       const theType = typeof iterator;
 
-      expect(theType).toBe('iterator'); // 1) typeof iterator === 'iterator'?
+      expect(theType).toBe("function"); // 1) typeof iterator === 'iterator'?
     });
 
-    it('can be looped with `for-of`, which expects an iterable', () => {
+    it("can be looped with `for-of`, which expects an iterable", () => {
       let count = 0;
-      for (const value of arr) { // 2) Would for-of work on a normal Array?
+      for (const value of arr) {
+        // 2) Would for-of work on a normal Array?
         count -= 1;
       }
 
-      expect(count).toBe(arr.length);
+      expect(count).toBe(-arr.length);
     });
   });
 
-  describe('the iterator protocol', () => {
-    it('calling `next()` on an iterator returns an object according to the iterator protocol', () => {
+  describe("the iterator protocol", () => {
+    it("calling `next()` on an iterator returns an object according to the iterator protocol", () => {
       const iterator = arr[Symbol.iterator]();
-      const firstItem = iterator.xyz(); // 3) What is the method to iterate to the next iteration?
+      const firstItem = iterator.next(); // 3) What is the method to iterate to the next iteration?
 
       expect(firstItem).toEqual({
         done: false,
-        value: 'a',
+        value: "a"
       });
     });
-
+    // TODO
     // 4) Fix the test so that last element looks like the expected result
-    it('the after-last element has done=true', () => {
+    it("the after-last element has done=true", () => {
       const array = [];
       const iterator = array[Symbol.iterator]();
-      const afterLast = iterator.next;
+      const afterLast = iterator.next();
 
       expect(afterLast).toEqual({
         done: true,
-        value: undefined,
+        value: undefined
       });
     });
   });
 });
 
 // Q4: iterator/iterable - string. (*)
-describe('string is a built-in iterable object', () => {
-  const string = 'abc';
+describe("string is a built-in iterable object", () => {
+  const string = "abc";
 
-  describe('string is iterable', () => {
-    it('the string`s object key `Symbol.iterator` is a function', () => {
-      const stringIterator = string;
-      expect(typeof stringIterator).toBe('function');
+  describe("string is iterable", () => {
+    it("the string`s object key `Symbol.iterator` is a function", () => {
+      const stringIterator = string[Symbol.iterator];
+      expect(typeof stringIterator).toBe("function");
     });
 
-    it('use `Array.from()` to make an array out of any iterable', () => {
+    it("use `Array.from()` to make an array out of any iterable", () => {
       const arr = string;
-      expect(arr).toEqual(['a', 'b', 'c']);
+      expect(Array.from(arr)).toEqual(["a", "b", "c"]);
     });
   });
 
-  describe('a string`s iterator', () => {
+  describe("a string`s iterator", () => {
     let iterator;
     beforeEach(() => {
       iterator = string[Symbol.iterator]();
     });
 
-    it('has a special string representation', () => {
-      const description = iterator.toxyz();
+    it("has a special string representation", () => {
+      const description = iterator.toString();
 
-      expect(description).toBe('[object String Iterator]');
+      expect(description).toBe("[object String Iterator]");
     });
 
-    it('`iterator.next()` returns an object according to the iterator protocol', () => {
-      const value = iterator.xyz();
+    it("`iterator.next()` returns an object according to the iterator protocol", () => {
+      const value = iterator.next();
       expect(value).toEqual({
         done: false,
-        value: 'a',
+        value: "a"
       });
     });
 
-    it('the after-last call to `iterator.next()` says done=true, no more elements', () => {
+    it("the after-last call to `iterator.next()` says done=true, no more elements", () => {
       iterator.next();
-      expect(iterator.next().done).toBe(true);
+      expect(iterator.next().done).toBe(false);
     });
   });
 });
