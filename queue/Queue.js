5d3cc48699091a163d71eb375191e4036a56b0dc
prateekinstillion <Prateek Madaan>
2019-06-18 09:42:28 +0530

    Solved the queue problem

--- queue/Queue.js
+++ queue/Queue.js
@@ -1,25 +1,40 @@
-// import { classBody } from "@babel/types";
 
-// function queue(...args) {
-//   return args;
-// }
 class Queue {
   constructor() {
     this.items = [];
   }
 
   enqueue(item) {
-    this.items.unshift(item);
+    this.items.push(item);
   }
 
   dequeue() {
-    this.items.shift();
+    if (this.isEmpty()) {
+      return null;
+    }
+    const removedItem = this.items.shift();
+    return removedItem;
   }
 
   isEmpty() {
     return this.items.length === 0;
   }
+
+  peek() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    return this.items[0];
+  }
+
+  toString(stringifier) {
+    if (stringifier) {
+      return this.items.map(stringifier).join(',');
+    }
+    return this.items.toString();
+  }
 }
+
 export {
   Queue,
 };
