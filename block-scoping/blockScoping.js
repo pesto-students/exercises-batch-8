9f5d9d6110e44058e33f4138ae3c2b9656223ea2
prateekinstillion <Prateek Madaan>
2019-06-18 11:33:59 +0530

    Solved the block scoping problem

--- block-scoping/blockScoping.js
+++ block-scoping/blockScoping.js
@@ -1,8 +1,11 @@
 
-function blockScoping(...args) {
-  return args;
+function blockScoping(n) {
+  const callbacks = [];
+  for (let i = 0; i <= 10; i += 1) {
+    callbacks.push(() => i);
+  }
+  return callbacks[n]();
 }
-
 export {
   blockScoping,
 };
