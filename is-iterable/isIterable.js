a4037ccf096ea5d17efab2900c33ea3f78350854
Abdul Khuddus <khuddus1@gmail.com>
2019-06-19 01:32:16 +0530

    Complete is iterable

--- is-iterable/isIterable.js
+++ is-iterable/isIterable.js
@@ -1,6 +1,21 @@
 
-function isIterable(...args) {
-  return args;
+function isIterable(arg) {
+  if (typeof arg[Symbol.iterator] !== 'function') {
+    return false;
+  }
+  if (typeof arg[Symbol.iterator]().next !== 'function') {
+    return false;
+  }
+  if (arg[Symbol.iterator]().next().done === undefined) {
+    return false;
+  }
+  if (arg[Symbol.iterator]().next().done === true) {
+    return true;
+  }
+  if (arg[Symbol.iterator]().next().value === undefined) {
+    return false;
+  }
+  return true;
 }
 
 export {
