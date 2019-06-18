1400eb7ecbdfae36df71593bd73cef289356819e
Abhinav Sharma <abhi18av@outlook.com>
2019-06-19 01:49:41 +0530

    completed alphabetic-shift problem

--- alphabetic-shift/alphabeticShift.js
+++ alphabetic-shift/alphabeticShift.js
@@ -1,28 +1,21 @@
-// WIP
-function isAlphabet(c){
-	if (c.match(/[a-z]/i)) {
-        return true;
-    }
-    return false; 
-}
-
 function shiftCharByAmount (str, amount) {
 	// Get its code
 	var charCode = str.charCodeAt(0);
-
 	// Uppercase letters
-	if ((charCode >= 65) && (charCode <= 90))
+	if ((charCode >= 65) && (charCode <= 90)) {
 	return String.fromCharCode(((charCode - 65 + amount) % 26) + 65);
-
-	// Lowercase letters
-	else if ((charCode >= 97) && (charCode <= 122))
-    return String.fromCharCode(((charCode - 97 + amount) % 26) + 97);
+  } 
+  // Lowercase letters
+  return String.fromCharCode(((charCode - 97 + amount) % 26) + 97);
 }
 
-function caesarShift (str) {
-const shiftVal = -1
-const output = "abcDef".split('').reduce((acc, aChar) => shiftCharByAmount(aChar, shiftVal) + acc);
-	return output;
+function alphabeticShift (aString) {
+  const shiftVal = 1;
+  let outputString = '';
+  for (const aChar of aString) {
+    outputString += shiftCharByAmount(aChar, shiftVal);
+  }
+  return outputString;
 }
 
 export {
