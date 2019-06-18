a3d80d258389ff11d654cb5db72e8f29eed8b12e
Rakesh Tembhurne <tj2point0@gmail.com>
2019-06-19 00:39:49 +0530

    Complete simple-iterable

--- simple-iterable/simpleIterable.js
+++ simple-iterable/simpleIterable.js
@@ -1,6 +1,27 @@
+function simpleIterable() {
+  const numbers = [1, 2, 3, 4, 5];
 
-function simpleIterable(...args) {
-  return args;
+  return {
+    [Symbol.iterator]() {
+      let index = 0;
+      return ({
+        next() {
+          if (index < numbers.length) {
+            const nextValue = {
+              value: numbers[index],
+              done: false,
+            };
+            index += 1;
+            return nextValue;
+          }
+          return ({
+            value: undefined,
+            done: true,
+          });
+        },
+      });
+    },
+  };
 }
 
 export {
