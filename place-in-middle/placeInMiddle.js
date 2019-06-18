40a1b39505523996745fb23dcef6df8166b1d735
sourabhmodi20051 <sourabhmodi20051@gmail.com>
2019-06-18 12:10:20 +0530

    Solved Place in Arrayu

--- place-in-middle/placeInMiddle.js
+++ place-in-middle/placeInMiddle.js
@@ -1,8 +1,14 @@
-
-function placeInMiddle(...args) {
-  return args;
+function placeInMiddle(firstArray, secondArray) {
+  if (firstArray.length === 0) {
+    return secondArray;
+  }
+  const placeLocation = Math.floor(firstArray.length / 2);
+  return firstArray.reduce((acc, currentItem, index) => {
+    if (index === placeLocation) {
+      return [...acc, ...secondArray, currentItem];
+    }
+    return [...acc, currentItem];
+  }, []);
 }
 
-export {
-  placeInMiddle,
-};
+export { placeInMiddle };
