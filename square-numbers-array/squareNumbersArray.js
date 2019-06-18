a21155dc233cba6e175f9464b87d6199ecd2783b
Abdul Khuddus <khuddus1@gmail.com>
2019-06-18 16:02:23 +0530

    Complete square numbers array

--- square-numbers-array/squareNumbersArray.js
+++ square-numbers-array/squareNumbersArray.js
@@ -1,6 +1,9 @@
 
-function squareNumbersArray(...args) {
-  return args;
+function squareNumbersArray(...numbers) {
+  if (numbers.filter(number => typeof number !== 'number').length > 0) {
+    throw Error('My custom error');
+  }
+  return numbers.map(number => number ** 2);
 }
 
 export {
