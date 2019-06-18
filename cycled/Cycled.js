c4868db01c5765325b9b906f3c2baf0076279ad3
Radheshyam Etikala <radhey1851@gmail.com>
2019-06-19 01:06:28 +0530

    InProgress

--- cycled/Cycled.js
+++ cycled/Cycled.js
@@ -1,8 +1,19 @@
 
-function cycled(...args) {
-  return args;
+class Cycled {
+  constructor(array = []) {
+    this.array = array;
+    this.length = array.length;
+    this.index = 0;
+  }
+
+  * next() {
+    while (this.array) {
+      yield this.array[this.index];
+      this.index += 1;
+    }
+  }
 }
 
 export {
-  cycled,
+  Cycled,
 };
