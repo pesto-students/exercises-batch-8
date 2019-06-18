924bc4151ca383f9e945b561999be9073e1dc5d3
sourabhmodi20051 <sourabhmodi20051@gmail.com>
2019-06-19 00:44:25 +0530

    Solved Consumable Users

--- consumable-users/ConsumableUsers.js
+++ consumable-users/ConsumableUsers.js
@@ -1,8 +1,19 @@
 
-function consumableUsers(...args) {
-  return args;
+function ConsumableUsers() {
+  this.users = ['Alice', 'Bob'];
+  this.size = 2;
+  this.nextUser = function () {
+    this.size -= 1;
+    if (this.users.length === 0) {
+      return undefined;
+    }
+    return `user: ${this.users.shift()}`;
+  };
+  this.done = function () {
+    return this.size === -1;
+  };
 }
 
 export {
-  consumableUsers,
+  ConsumableUsers,
 };
