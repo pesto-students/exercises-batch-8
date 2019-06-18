63b582b55a874a5504c3166fcb085db6bb892d5d
prateekinstillion <Prateek Madaan>
2019-06-18 23:56:35 +0530

    Solved the fibonacci iterator problem

--- fibonacci-iter/fibonacciIter.js
+++ fibonacci-iter/fibonacciIter.js
@@ -1,8 +1,21 @@
-
-function fibonacciIter(...args) {
-  return args;
-}
-
+const fibonacciIter = {
+  [Symbol.iterator]() {
+    let curr = 1;
+    let prev = 0;
+    let temp;
+    const iterator = {
+      next() {
+        while (true) {
+          temp = prev + curr;
+          prev = curr;
+          curr = temp;
+          return { value: temp, done: false };
+        }
+      },
+    };
+    return iterator;
+  },
+};
 export {
   fibonacciIter,
 };
