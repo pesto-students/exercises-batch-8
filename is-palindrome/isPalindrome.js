89ef30bc7af82d8580f0a52855261b3f97f1f91e
Abhinav Sharma <abhi18av@outlook.com>
2019-06-19 02:01:48 +0530

    completed is-palindrome problem

--- is-palindrome/isPalindrome.js
+++ is-palindrome/isPalindrome.js
@@ -1,7 +1,16 @@
 
-function isPalindrome(...args) {
-  return args;
+function isPalindrome(input) {
+  const inputString = input.toString();
+  // Removes unnecessary charecters from inputString
+  const filteredInputString = inputString.toLowerCase().replace(/[\W_]/g, '');
+
+  const reversedInputString = filteredInputString
+                              .split('')
+                              .reverse()
+                              .join('');
+  return filteredInputString === reversedInputString;
 }
+
 
 export {
   isPalindrome,
