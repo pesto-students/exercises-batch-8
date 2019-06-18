d6e188fd620780f4088d2fa15b1691c3ef55d428
Pradyumna Newalkar <contactpradyumna1@gmail.com>
2019-06-18 19:13:25 +0000

    Added solution for fibonacci-iter

--- fibonacci-iter/fibonacciIter.js
+++ fibonacci-iter/fibonacciIter.js
@@ -1,7 +1,20 @@
 
-function fibonacciIter(...args) {
-  return args;
+function* fibonacciGenerator() {
+  let first = 1;
+  let second = 2;
+  yield first;
+  yield second;
+  while (true) {
+    const next = first + second;
+    first = second;
+    second = next;
+    yield next;
+  }
 }
+
+const fibonacciIter = {
+  [Symbol.iterator]: fibonacciGenerator,
+};
 
 export {
   fibonacciIter,
