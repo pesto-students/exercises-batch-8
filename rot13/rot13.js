51b437426ca21eaf9bd2e8ea71ecc806d34a356f
Abhinav Sharma <abhi18av@outlook.com>
2019-06-19 01:54:54 +0530

    simplified the code for rot13 problem

--- rot13/rot13.js
+++ rot13/rot13.js
@@ -1,17 +1,34 @@
 
-function rot13(...args) {
-  var input     = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  var output    = 'NOPQRSTUVWXYZABCDEFGHIJKLM';
-  var index     = x => input.indexOf(x);
-  var translate = x => index(x) > -1 ? output[index(x)] : x;
-  return str.split('').map(translate).join('');
+function isAlphabet(c){
+	if (c.match(/[a-z]/i)) {
+        return true;
+    }
+    return false; 
 }
 
 
-//console.log(rot13("HELLO WORLD!"))
+function shiftCharByAmount (str, amount) {
+  if (!isAlphabet(str)){
+    return str;
+  }
+	// Get its code
+  var charCode = str.charCodeAt(0);
+	// Uppercase letters
+	if ((charCode >= 65) && (charCode <= 90)) {
+	return String.fromCharCode(((charCode - 65 + amount) % 26) + 65);
+  } 
+  // Lowercase letters
+  return String.fromCharCode(((charCode - 97 + amount) % 26) + 97);
+}
 
-
-
+function rot13 (aString) {
+  const shiftVal = 13;
+  let outputString = '';
+  for (const aChar of aString) {
+    outputString += shiftCharByAmount(aChar, shiftVal);
+  }
+  return outputString;
+}
 
 
 export {
