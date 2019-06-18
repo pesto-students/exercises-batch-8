509b890863e1eec7683be00a7fefab71979eaca6
Radheshyam Etikala <radhey1851@gmail.com>
2019-06-18 09:39:26 +0530

    Completed SequentialPromise

--- sequential-promise/sequentialPromise.js
+++ sequential-promise/sequentialPromise.js
@@ -1,6 +1,8 @@
 
-function sequentialPromise(...args) {
-  return args;
+function sequentialPromise(promises) {
+  return promises.reduce((prevPromise, currPromise) => (
+    prevPromise.then((result => currPromise(result)))
+  ), Promise.resolve());
 }
 
 export {
