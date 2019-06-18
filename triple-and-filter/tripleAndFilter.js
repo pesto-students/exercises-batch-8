5cd87063db2782dc8d455340762b96fcf18f35fd
kunnu01 <kunalbhatia2409@gmail.com>
2019-06-18 12:58:34 +0530

    Solved createStudent and tripleAndFilter

--- triple-and-filter/tripleAndFilter.js
+++ triple-and-filter/tripleAndFilter.js
@@ -1,6 +1,9 @@
 
-function tripleAndFilter(...args) {
-  return args;
+function tripleAndFilter(numbers) {
+  const trippledNumbers = numbers.map(number => number * 3);
+  const isMultipleOfFive = num => num % 5 === 0;
+  const multiplesOfFive = trippledNumbers.filter(isMultipleOfFive);
+  return multiplesOfFive;
 }
 
 export {
