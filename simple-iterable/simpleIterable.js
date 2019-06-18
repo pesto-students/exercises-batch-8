70cf144a9dc99bd1627b167799d070d80c006d16
Rakesh Tembhurne <tj2point0@gmail.com>
2019-06-19 00:45:56 +0530

    Refactored simple-iterable

--- simple-iterable/simpleIterable.js
+++ simple-iterable/simpleIterable.js
@@ -1,27 +1,6 @@
 function simpleIterable() {
   const numbers = [1, 2, 3, 4, 5];
-
-  return {
-    [Symbol.iterator]() {
-      let index = 0;
-      return ({
-        next() {
-          if (index < numbers.length) {
-            const nextValue = {
-              value: numbers[index],
-              done: false,
-            };
-            index += 1;
-            return nextValue;
-          }
-          return ({
-            value: undefined,
-            done: true,
-          });
-        },
-      });
-    },
-  };
+  return numbers[Symbol.iterator]();
 }
 
 export {
