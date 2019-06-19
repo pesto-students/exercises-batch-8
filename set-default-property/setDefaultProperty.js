65610ff250a4055a30ce8817adb605e57cc43a49
Pradyumna Newalkar <contactpradyumna1@gmail.com>
2019-06-19 19:36:49 +0000

    added solution for set-default-property

--- set-default-property/setDefaultProperty.js
+++ set-default-property/setDefaultProperty.js
@@ -1,6 +1,14 @@
 
-function setDefaultProperty(...args) {
-  return args;
+function setDefaultProperty(actualObj, defaultValue) {
+  const defaultHandler = {
+    get(obj, prop) {
+      if (prop in obj) {
+        return obj[prop];
+      }
+      return defaultValue;
+    },
+  };
+  return new Proxy(actualObj, defaultHandler);
 }
 
 export {
