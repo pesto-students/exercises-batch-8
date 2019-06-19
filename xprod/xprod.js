53a4b73f1392c1616f489217d0ceaa3668e0d1b7
Pradyumna Newalkar <contactpradyumna1@gmail.com>
2019-06-19 18:45:51 +0000

    Added solution for xprod

--- xprod/xprod.js
+++ xprod/xprod.js
@@ -1,6 +1,15 @@
 
-function xprod(...args) {
-  return args;
+function xprod(arr1, arr2) {
+  return arr1.map(
+    itemArr1 => (
+      arr2.map(
+        itemArr2 => [itemArr1, itemArr2],
+      )
+    ),
+  ).reduce(
+    (result, val) => [...result, ...val],
+    [],
+  );
 }
 
 export {
