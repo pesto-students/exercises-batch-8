1d694178a750e1268cbef2b63ba1e7534d39821d
sourabhmodi20051 <sourabhmodi20051@gmail.com>
2019-06-19 07:36:19 +0530

    Solved Fibonacciter

--- fibonacci-iter/fibonacciIter.js
+++ fibonacci-iter/fibonacciIter.js
@@ -1,7 +1,24 @@
+const fibonacciIter = {
+  fib: [1, 1],
+  index: -1,
+  getNextFib() {
+    if (this.fib[this.index] === undefined) {
+      this.fib[this.index] = this.fib[this.index - 1] + this.fib[this.index - 2];
+    }
+    this.index += 1;
+    return this.fib[this.index - 1];
+  },
 
-function fibonacciIter(...args) {
-  return args;
-}
+  [Symbol.iterator]() {
+    return {
+      next: () => ({
+        value: this.getNextFib(),
+        done: false,
+      }),
+    };
+  },
+};
+
 
 export {
   fibonacciIter,
