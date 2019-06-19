9ea905634bb36ce7e948d70499b5e5bd3d593a96
Abhinav Sharma <abhi18av@outlook.com>
2019-06-19 18:50:30 +0530

    completed known-props

--- known-prop/knownProp.js
+++ known-prop/knownProp.js
@@ -1,6 +1,13 @@
 
-function knownProp(...args) {
-  return args;
+function knownProp(object) {
+  return new Proxy(object, {
+    get(obj, keyname) {
+      if (!obj.hasOwnProperty(keyname)) {
+        throw TypeError('Unknown property');
+      }
+      return obj[keyname];
+    },
+  });
 }
 
 export {
