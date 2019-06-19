6f5fb1c1e00438ebbaa485b62fe216265ccc0414
sourabhmodi20051 <sourabhmodi20051@gmail.com>
2019-06-20 01:05:56 +0530

    Negetive Index

--- negative-index/negativeIndex.js
+++ negative-index/negativeIndex.js
@@ -1,7 +1,30 @@
 
-function negativeIndex(...args) {
-  return args;
+function negativeIndex(maybeArray) {
+  if (!(maybeArray instanceof Array)) {
+    throw new TypeError('Only arrays are supported')
+  }
+  var handler = {
+    get: (array, prop) => {
+      const numberProp = Number(prop);
+      if (numberProp < 0) {
+        return array[array.length + numberProp];
+      }
+      return array[numberProp];
+    },
+    set: (array, prop, value) => {
+      const numberProp = Number(prop);
+      if (numberProp < 0) {
+        array[array.length + numberProp] = numberProp;
+      } else {
+        array[numberProp] = value;
+      }
+      return true;
+    },
+  }
+  const negativeIndexProxy = new Proxy(maybeArray, handler);
+  return negativeIndexProxy;
 }
+
 
 export {
   negativeIndex,
