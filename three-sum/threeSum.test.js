fa88b84bb30ac54637ed246c6109e2e2368ae039
sourabhmodi20051 <sourabhmodi20051@gmail.com>
2019-06-19 14:37:31 +0530

    Solved three sum

--- three-sum/threeSum.test.js
+++ three-sum/threeSum.test.js
@@ -4,7 +4,7 @@ describe('threeSum', () => {
   it('should return first triplet adding up to a given target sum.', () => {
     expect(threeSum([10, 12, 12, 15, 12], 37)).toEqual([10, 12, 15]);
     expect(threeSum([4, 3, 2, 1, 0, -1, -2], 3)).toEqual([4, 1, -2]);
-    expect(threeSum([5, 3, 2, 1, 0, -1, -2], 3)).toEqual([3, 1, -1]);
+    expect(threeSum([5, 3, 2, 1, 0, -1, -2], 3)).toEqual([5, 0, -2]);
   });
 
   it('should return null when no such triplets found', () => {
@@ -12,8 +12,8 @@ describe('threeSum', () => {
   });
 
   it('should throw when given invalid inputs', () => {
-    expect(threeSum([], 35)).toThrow();
-    expect(threeSum([1, 2, 3], 'lol')).toThrow();
-    expect(threeSum([1, 2], 5)).toThrow();
+    expect(() => threeSum([], 35)).toThrow();
+    expect(() => threeSum([1, 2, 3], 'lol')).toThrow();
+    expect(() => threeSum([1, 2], 5)).toThrow();
   });
 });
