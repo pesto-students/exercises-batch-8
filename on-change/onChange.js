d92dab1b3cf0512e7c7375eed7068a158c07b568
kunnu01 <kunalbhatia2409@gmail.com>
2019-06-20 01:49:54 +0530

    Solved on-change

--- on-change/onChange.js
+++ on-change/onChange.js
@@ -2,16 +2,28 @@
 function onChange(objToWatch, onChangeFunction) {
   const handler = {
     get(target, prop) {
-      onChangeFunction();
-      if (typeof target[prop] === 'function' && prop === 'sort') {
-        onChangeFunction();
+      try {
+        if (typeof target[prop] === 'function' && prop === 'sort') {
+          onChangeFunction();
+        }
+        return new Proxy(target[prop], handler);
+      } catch (e) {
+        return Reflect.get(target, prop);
       }
-      return new Proxy(target[prop], handler);
     },
-    set(target, prop) {
+    set(target, prop, value) {
       onChangeFunction();
-      return Reflect.set(target, prop);
+      return Reflect.set(target, prop, value);
     },
+    defineProperty(target, prop, descriptor) {
+      onChangeFunction();
+      return Reflect.defineProperty(target, prop, descriptor);
+    },
+    deleteProperty(target, prop) {
+      onChangeFunction();
+      return Reflect.deleteProperty(target, prop);
+    },
+
   };
   return new Proxy(objToWatch, handler);
 }
