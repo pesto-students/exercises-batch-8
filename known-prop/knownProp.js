e9a2cd9aed3be9d95386c9705cf35064ce0d0076
sourabhmodi20051 <sourabhmodi20051@gmail.com>
2019-06-20 00:15:32 +0530

    Solved Known Property Proxy

--- known-prop/knownProp.js
+++ known-prop/knownProp.js
@@ -1,6 +1,15 @@
 
-function knownProp(...args) {
-  return args;
+function knownProp(object) {
+  const handler = {
+    get: (obj, prop) => {
+      if (prop in obj) {
+        return obj[prop];
+      }
+      throw new TypeError('Unknown property');
+    },
+  };
+  const knowPropertyProxy = new Proxy(object, handler);
+  return knowPropertyProxy;
 }
 
 export {
