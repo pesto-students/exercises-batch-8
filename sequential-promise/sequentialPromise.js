bc09cb10235f13f02d0fdc50146ecbde20b2280b
sourabhmodi20051 <sourabhmodi20051@gmail.com>
2019-06-19 13:25:30 +0530

    Sequential Promise Solved

--- sequential-promise/sequentialPromise.js
+++ sequential-promise/sequentialPromise.js
@@ -1,6 +1,9 @@
-
-function sequentialPromise(...args) {
-  return args;
+function sequentialPromise(functions, currentFunction = 0, args) {
+  if (functions.length === currentFunction) {
+    return Promise.resolve(args);
+  }
+  return functions[currentFunction](args)
+    .then(res => sequentialPromise(functions, currentFunction + 1, res));
 }
 
 export {
