84eba6b7a142dcac2261fbaa5c9a3eba870223b1
saurabh shetty <saurabh.shetty100@gmail.com>
2019-06-19 19:49:01 +0530

    solved arrayCubeRoot

--- ackermann/ackermann.js
+++ ackermann/ackermann.js
@@ -1,6 +1,4 @@
-// A(m,n) = n+1             when m = 0
-// A(m,n) = A(m-1,1)        when m > 0 and n = 0
-// A(m,n) = A(m-1,A(m,n-1)) when m > 0 and n > 0
+
 const ackermannFunction = (m, n) => {
   if (m === 0) {
     return n + 1;
