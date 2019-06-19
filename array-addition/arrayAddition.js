74f6c95da996fded44b02dc254cdfd8abc134f38
Abhinav Sharma <abhi18av@outlook.com>
2019-06-19 07:10:09 +0530

    WIP array-addition problem

--- array-addition/arrayAddition.js
+++ array-addition/arrayAddition.js
@@ -1,6 +1,30 @@
+// WIP
 
-function arrayAddition(...args) {
-  return args;
+
+function resizeSmallerArray(arr1, arr2) {
+    let array1 = arr1;
+    let array2 = arr2;
+    const sizeArray1 = array1.length;
+    const sizeArray2 = array2.length;
+    
+    if (sizeArray1 < sizeArray2) {
+        let sizeDiff = sizeArray2 - sizeArray1;
+        for (let i = 0; i < sizeDiff; i++) {
+            array1.push(0);
+        }
+        return array1;
+    } 
+
+    let sizeDiff = sizeArray1 - sizeArray2;
+    for (let i = 0; i < sizeDiff; i++) {
+            array2.push(0);
+    }
+    return array2;
+    
+}
+
+function arrayAddition(array1, array2) {
+  
 }
 
 export {
