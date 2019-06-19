c9b899df7c888e01eec21d89fb7bd4c4fa15bbac
Pradyumna Newalkar <contactpradyumna1@gmail.com>
2019-06-19 00:52:41 +0000

    Added solution for range-iter

--- range-iter/rangeIter.js
+++ range-iter/rangeIter.js
@@ -1,6 +1,33 @@
+function isNumber(arg) {
+  return typeof arg === 'number' && !Number.isNaN(arg);
+}
 
-function rangeIter(...args) {
-  return args;
+function rangeIter(start, end) {
+  if (!isNumber(start)) {
+    throw new TypeError(`${start} is not a number`);
+  }
+  if (!isNumber(end)) {
+    throw new TypeError(`${end} is not a number`);
+  }
+  let curr = start;
+  return {
+    [Symbol.iterator]() {
+      return this;
+    },
+    next() {
+      const val = curr;
+      curr += 1;
+      if (val <= end) {
+        return {
+          value: val,
+          done: false,
+        };
+      }
+      return {
+        done: true,
+      };
+    },
+  };
 }
 
 export {
