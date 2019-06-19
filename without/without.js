f9965710788e4efba20d6a955bd11a2661629c39
Abdul Khuddus <khuddus1@gmail.com>
2019-06-20 00:28:15 +0530

    Complete xprod

--- without/without.js
+++ without/without.js
@@ -1,6 +1,6 @@
 
-function without(...args) {
-  return args;
+function without(arr1, arr2) {
+  return arr1.reduce((acc, val1) => acc.filter(val2 => val1 !== val2), arr2);
 }
 
 export {
--- xprod/xprod.js
+++ xprod/xprod.js
@@ -1,6 +1,12 @@
 
-function xprod(...args) {
-  return args;
+function xprod(arr1, arr2) {
+  return arr1.reduce((acc1, val1) => {
+    const arrayOfVal1WithArr2 = arr2.reduce((acc2, val2) => {
+      acc2.push([val1, val2]);
+      return acc2;
+    }, []);
+    return [...acc1, ...arrayOfVal1WithArr2];
+  }, []);
 }
 
 export {
