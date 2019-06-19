e2fd4120faada5bec0c97d98769d0e29829b0a6c
aayushtiwari <arsenicaayush@gmail.com>
2019-06-19 18:10:13 +0530

    updated curry & alternatingCount

--- curry/curry.js
+++ curry/curry.js
@@ -1,8 +1,11 @@
-
-function curry(...args) {
-  return args;
+function curry(inputFunction, count = 0, args = []) {
+  const argsLength = inputFunction.length;
+  if (count === argsLength) {
+    return inputFunction.apply(null, args);
+  }
+  return function () {
+    return curry(inputFunction, count + arguments.length, [...args, ...arguments]);
+  };
 }
 
-export {
-  curry,
-};
+export { curry };
--- savings-account/SavingsAccount.js
+++ savings-account/SavingsAccount.js
@@ -1,6 +1,23 @@
 
-function savingsAccount(...args) {
-  return args;
+function SavingsAccount(accountNumber, email, firstName, lastName, products = []) {
+  const emailRegex = /^([a-zA-Z0-9]+@[a-z.]+)$/i;
+  const firstNameRegex = /^[A-Z]{3,20}/i;
+  const lastNameRegex = /[a-z]+$/i;
+  if (accountNumber.length !== 6) {
+    throw new Error('Account Number must be a 6-digit number');
+  } else if (!emailRegex.test(email)) {
+    throw new Error('Invalid e-mail');
+  } else if (!firstNameRegex.test(firstName)) {
+    throw new Error('First name must be between 3 and 20 characters long');
+  } else if (!lastNameRegex.test(lastName)) {
+    throw new Error('Last name must contain english alphabets only');
+  }
+
+  this._accountNumber = accountNumber;
+  this._email = email;
+  this._firstName = firstName;
+  this._lastName = lastName;
+  this.products = products;
 }
 
 export {
