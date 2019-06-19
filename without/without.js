c807e289078c4a25cbbfb3abf5c114e3bf087e6b
Pradyumna Newalkar <contactpradyumna1@gmail.com>
2019-06-19 19:06:28 +0000

    added solution for without

--- without/without.js
+++ without/without.js
@@ -1,6 +1,13 @@
-
-function without(...args) {
-  return args;
+function isEqual(item1, item2) {
+  if (typeof item1 === 'object' || typeof item2 === 'object') {
+    return item1.toString() === item2.toString();
+  }
+  return Object.is(item1, item2);
+}
+function without(arr1, arr2) {
+  return arr2.filter(
+    itemArr2 => !arr1.some(itemArr1 => isEqual(itemArr1, itemArr2)),
+  );
 }
 
 export {
