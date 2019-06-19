90a820cb18f6bfd8f45671d5098aea748542baac
Rakesh Tembhurne <tj2point0@gmail.com>
2019-06-20 02:05:39 +0530

    Completed xprod

--- xprod/xprod.js
+++ xprod/xprod.js
@@ -1,6 +1,13 @@
 
-function xprod(...args) {
-  return args;
+function xprod(firstArray, secondArray) {
+  const modified = firstArray.map((elemFirst) => {
+    const replacerArray = secondArray.map(elemSecond => [elemFirst, elemSecond]);
+    return replacerArray;
+  });
+
+  const flattened = modified.flat();
+
+  return flattened;
 }
 
 export {
