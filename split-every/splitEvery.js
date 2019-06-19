68255b002e2a820e0e1277904377f6f460338b8e
saurabh shetty <saurabh.shetty100@gmail.com>
2019-06-20 05:17:41 +0530

    solved splitevery

--- split-every/splitEvery.js
+++ split-every/splitEvery.js
@@ -1,6 +1,18 @@
 
-function splitEvery(...args) {
-  return args;
+function splitEvery(limit, arrayOrString) {
+  if (limit <= 0) {
+    throw new Error('Limit must be a positive number');
+  }
+  let index = 0;
+  const resultArray = [];
+  while (arrayOrString.length > index) {
+    resultArray.push([...arrayOrString.slice(index, index + limit)]);
+    index += limit;
+  }
+  if (typeof arrayOrString === 'string') {
+    return resultArray.map(arr => arr.join(''));
+  }
+  return resultArray;
 }
 
 export {
