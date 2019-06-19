40c26c55925d28711171e02a2ece5cfef454f97c
Radheshyam Etikala <radhey1851@gmail.com>
2019-06-20 00:33:25 +0530

    completed values, valuesIn

--- values-in/valuesIn.js
+++ values-in/valuesIn.js
@@ -1,6 +1,17 @@
 
-function valuesIn(...args) {
-  return args;
+function valuesIn(obj) {
+  const propertyValues = [];
+  const { hasOwnProperty } = Object.prototype;
+  const objPrototype = Object.getPrototypeOf(obj);
+
+  for (const property in obj) {
+    if (hasOwnProperty.call(obj, property)
+        || hasOwnProperty.call(objPrototype, property)) {
+      const value = obj[property];
+      propertyValues.push(value);
+    }
+  }
+  return propertyValues;
 }
 
 export {
