4f96876a9c9c7eed8c4897d81f2f2540bda4846e
Abdul Khuddus <khuddus1@gmail.com>
2019-06-20 05:17:29 +0530

    Complete unnest

--- unnest/unnest.js
+++ unnest/unnest.js
@@ -1,6 +1,13 @@
-
-function unnest(...args) {
-  return args;
+function unnest(nest) {
+  const convertToArray = (obj) => {
+    const objToArray = new Array(obj.length).fill('');
+    return objToArray.reduce((acc, _el, index) => [...acc, obj[index]], []);
+  };
+  const arrayToBeUnnested = Array.isArray(nest) ? nest : convertToArray(nest);
+  return arrayToBeUnnested.reduce((acc, element) => {
+    const arrayedElement = Array.isArray(element) ? element : [element];
+    return [...acc, ...arrayedElement];
+  }, []);
 }
 
 export {
