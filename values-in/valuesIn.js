638e06cfb2ea6323e3dd68c3308e662ea4be78fe
Abhinav Sharma <abhi18av@outlook.com>
2019-06-19 18:52:43 +0530

    completed values-in problem

--- values-in/valuesIn.js
+++ values-in/valuesIn.js
@@ -1,6 +1,10 @@
 
-function valuesIn(...args) {
-  return args;
+function valuesIn(object) {
+  const objectValues = [];
+  for (const key in object) {
+    objectValues.push(object[key]);
+  }
+  return objectValues;
 }
 
 export {
