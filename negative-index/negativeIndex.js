7b35f01df5dee7ca9bd3ebb742ece8cc03c1e9c8
kunnu01 <kunalbhatia2409@gmail.com>
2019-06-20 04:49:26 +0530

    Refactor negative-index

--- negative-index/negativeIndex.js
+++ negative-index/negativeIndex.js
@@ -7,29 +7,24 @@ function negativeIndex(array) {
 
   return new Proxy(array, {
     get(target, prop) {
-      try {
-        let indexToLook = prop;
-        if (indexToLook < 0) {
-          indexToLook = array.length + parseInt(indexToLook, 10);
-        }
-        return Reflect.get(target, indexToLook);
-      } catch (e) {
+      if (typeof prop === 'symbol') {
         return Reflect.get(target, prop);
       }
+      let indexToLook = prop;
+      if (indexToLook < 0) {
+        indexToLook = array.length + parseInt(indexToLook, 10);
+      }
+      return Reflect.get(target, indexToLook);
     },
     set(target, prop, value) {
-      try {
-        let indexToLook = prop;
-        if (indexToLook < 0) {
-          indexToLook = array.length + parseInt(indexToLook, 10);
-        }
-        if (Number(prop) > array.length) {
-          array.length = Number(prop) + 1;
-        }
-        return Reflect.set(target, indexToLook, value);
-      } catch (e) {
-        return Reflect.set(target, prop, value);
+      let indexToLook = prop;
+      if (indexToLook < 0) {
+        indexToLook = array.length + parseInt(indexToLook, 10);
       }
+      if (Number(prop) > array.length) {
+        array.length = Number(prop) + 1;
+      }
+      return Reflect.set(target, indexToLook, value);
     },
   });
 }
