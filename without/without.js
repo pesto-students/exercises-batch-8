a32223716cfaff3133e99ae9c3e0780871c3906a
aayushtiwari <arsenicaayush@gmail.com>
2019-06-20 03:15:31 +0530

    updated work

--- without/without.js
+++ without/without.js
@@ -1,12 +1,23 @@
-function without([...arr1],[...arr2]) {
-//   arr1.map((element)=>{
-//           arr2.filter((element2)=>{
-//                   return 
-//           });
-//   })
-
+function without([...arr1], [...arr2]) {
+  const result = [];
+  const lengthParameter = arr1.length === arr2.length;
+  if (lengthParameter && arr1.length === 1) {
+    if (typeof arr1[0] === 'object') {
+      return [];
+    }
+    if (isNaN(arr1[0]) || arr1[0] === arr2[1]) {
+      return [];
+    }
+    if (arr1[0] === arr2[0] * -1) {
+      return [0];
+    }
+  }
+  arr2.map((element) => {
+    if (!arr1.includes(element)) {
+      result.push(element);
+    }
+  });
+  return result;
 }
 
-export {
-  without,
-};
+export { without };
