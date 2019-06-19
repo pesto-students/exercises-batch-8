e7c36fd98e4528bde63866fe9fada75998f5d84a
kunnu01 <kunalbhatia2409@gmail.com>
2019-06-20 01:04:15 +0530

    WIP: on-change

--- on-change/onChange.js
+++ on-change/onChange.js
@@ -1,6 +1,19 @@
 
-function onChange(...args) {
-  return args;
+function onChange(objToWatch, onChangeFunction) {
+  const handler = {
+    get(target, prop) {
+      onChangeFunction();
+      if (typeof target[prop] === 'function' && prop === 'sort') {
+        onChangeFunction();
+      }
+      return new Proxy(target[prop], handler);
+    },
+    set(target, prop) {
+      onChangeFunction();
+      return Reflect.set(target, prop);
+    },
+  };
+  return new Proxy(objToWatch, handler);
 }
 
 export {
