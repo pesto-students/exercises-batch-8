cbd32fceaaff42517e542054bc87ce00aefbfea2
Pradyumna Newalkar <contactpradyumna1@gmail.com>
2019-06-19 01:50:00 +0000

    Added solution for iterable-protocol

--- iterable-protocol/iterableProtocol.test.js
+++ iterable-protocol/iterableProtocol.test.js
@@ -1,5 +1,11 @@
 describe('A simple iterable without items inside, implementing the right protocol', () => {
-  function iteratorFunction() {}
+  function iteratorFunction() {
+    return {
+      next: () => ({
+        done: true,
+      }),
+    };
+  }
 
   describe('the `iteratorFunction` needs to comply to the iterator protocol', () => {
     it('must return an object', () => {
@@ -17,7 +23,9 @@ describe('A simple iterable without items inside, implementing the right protoco
 
   let iterable;
   beforeEach(() => {
-    iterable = 'iterable';
+    iterable = {
+      [Symbol.iterator]: iteratorFunction,
+    };
   });
 
   describe('the iterable', () => {
@@ -31,7 +39,7 @@ describe('A simple iterable without items inside, implementing the right protoco
 
   describe('using the iterable', () => {
     it('it contains no values', () => {
-      let values;
+      let values = '';
       for (const value of iterable) {
         values += value;
       }
@@ -39,18 +47,18 @@ describe('A simple iterable without items inside, implementing the right protoco
     });
 
     it('has no `.length` property', () => {
-      const hasLengthProperty = iterable;
+      const hasLengthProperty = iterable.length !== undefined;
       expect(hasLengthProperty).toBe(false);
     });
 
     describe('can be converted to an array', () => {
       it('using `Array.from()`', () => {
-        const arr = iterable;
+        const arr = Array.from(iterable);
         expect(Array.isArray(arr)).toBe(true);
       });
 
       it('where `.length` is still 0', () => {
-        const arr = iterable;
+        const arr = Array.from(iterable);
         const {
           length,
         } = arr;
