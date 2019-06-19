1c502a09eb67dbc1fee076e0b76b0dd581c24d98
kunnu01 <kunalbhatia2409@gmail.com>
2019-06-20 00:48:51 +0530

    Solved negative-index

--- negative-index/negativeIndex.js
+++ negative-index/negativeIndex.js
@@ -1,6 +1,37 @@
+/* eslint-disable no-param-reassign */
 
-function negativeIndex(...args) {
-  return args;
+function negativeIndex(array) {
+  if (!Array.isArray(array)) {
+    throw new TypeError(`Expected array, recieved ${typeof array} (${array})`);
+  }
+
+  return new Proxy(array, {
+    get(target, prop) {
+      try {
+        let indexToLook = prop;
+        if (indexToLook < 0) {
+          indexToLook = array.length + parseInt(indexToLook, 10);
+        }
+        return Reflect.get(target, indexToLook);
+      } catch (e) {
+        return Reflect.get(target, prop);
+      }
+    },
+    set(target, prop, value) {
+      try {
+        let indexToLook = prop;
+        if (indexToLook < 0) {
+          indexToLook = array.length + parseInt(indexToLook, 10);
+        }
+        if (Number(prop) > array.length) {
+          array.length = Number(prop) + 1;
+        }
+        return Reflect.set(target, indexToLook, value);
+      } catch (e) {
+        return Reflect.set(target, prop, value);
+      }
+    },
+  });
 }
 
 export {
