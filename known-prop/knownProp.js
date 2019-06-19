bb97405837e8627ca70eff74724f28d48bba3334
kunnu01 <kunalbhatia2409@gmail.com>
2019-06-20 00:20:04 +0530

    Solved knownProp

--- known-prop/knownProp.js
+++ known-prop/knownProp.js
@@ -1,6 +1,14 @@
 
-function knownProp(...args) {
-  return args;
+function knownProp(object) {
+  return new Proxy(object, {
+    get(target, key) {
+      // if (target.hasOwnProperty(key)) {
+      if (Object.prototype.hasOwnProperty.call(target, key)) {
+        return target[key];
+      }
+      throw new TypeError(`Unknown property: ${key}`);
+    },
+  });
 }
 
 export {
