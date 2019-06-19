e78f6bac076bfd296cc0c02a3e717fb4ea53dda5
Rakesh Tembhurne <tj2point0@gmail.com>
2019-06-19 20:37:55 +0530

    Complet limit-function-call-count

--- limit-function-call-count/limitFunctionCallCount.js
+++ limit-function-call-count/limitFunctionCallCount.js
@@ -1,6 +1,14 @@
 
-function limitFunctionCallCount(...args) {
-  return args;
+function limitFunctionCallCount(fn, limit) {
+  let counter = 0;
+
+  return function fnLimiter(...args) {
+    if (counter >= limit) {
+      return null;
+    }
+    counter += 1;
+    return fn.apply(this, args);
+  };
 }
 
 export {
