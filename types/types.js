b787e8309a210b7c054b7212b62c1371d6b64812
Abhinav Sharma <abhi18av@outlook.com>
2019-06-20 04:59:04 +0530

    completed types problem

--- types/types.js
+++ types/types.js
@@ -1,8 +1,33 @@
+import { isUndefined, isNull } from 'util';
 
-function types(...args) {
-  return args;
+function type(data) {
+  if (isNull(data)) {
+    return 'Null';
+  }
+  if (isUndefined(data)) {
+    return 'Undefined';
+  }
+  if (typeof data === 'string') {
+    return 'String';
+  }
+  if (Array.isArray(data)) {
+    return 'Array';
+  }
+  if (typeof data === 'function') {
+    return 'Function';
+  }
+  if (typeof data === 'boolean') {
+    return 'Boolean';
+  }
+  if (data instanceof RegExp) {
+    return 'RegExp';
+  }
+  if (typeof data === 'object') {
+    return 'Object';
+  }
+  return 'Number';
 }
 
 export {
-  types,
+  type,
 };
