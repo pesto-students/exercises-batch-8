40a510414e072df2d7edffa364d152cd616be9b1
Radheshyam Etikala <radhey1851@gmail.com>
2019-06-19 08:06:32 +0530

    completed simpleIter

--- simple-iterable/simpleIterable.js
+++ simple-iterable/simpleIterable.js
@@ -1,7 +1,21 @@
 
-function simpleIterable(...args) {
-  return args;
-}
+const simpleIterable = () => ({
+  [Symbol.iterator]() {
+    let i = 1;
+    const iterator = {
+      next() {
+        if (i < 6) {
+          const valueObj = { value: i, done: false };
+          i += 1;
+          return valueObj;
+        }
+        return { done: true };
+      },
+    };
+    return iterator;
+  },
+});
+
 
 export {
   simpleIterable,
