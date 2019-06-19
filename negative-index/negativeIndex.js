9378b2668ca2f36c7eb1da78bf19f7c2584b3d73
Jaspreet Singh <erjaspreet57@gmail.com>
2019-06-20 00:50:41 +0530

    Solving negative-index

--- negative-index/negativeIndex.js
+++ negative-index/negativeIndex.js
@@ -1,6 +1,33 @@
+const get = (array, indexKey) => {
+  const index = parseInt(indexKey, 10);
+  if (index < 0) {
+    return array[array.length + index];
+  }
+  return array[index];
+};
 
-function negativeIndex(...args) {
-  return args;
+const set = (array, indexKey, value) => {
+  const index = parseInt(indexKey, 10);
+  if (index < 0) {
+    // eslint-disable-next-line no-param-reassign
+    array[array.length + index] = value;
+  }
+  // eslint-disable-next-line no-param-reassign
+  array[index] = value;
+  return array;
+};
+
+function negativeIndex(array) {
+  if (!Array.isArray(array)) {
+    throw new TypeError('Only arrays are supported');
+  }
+
+  const handler = {
+    get,
+    set,
+  };
+  // TODO: make proxy behave like plain array too.
+  return new Proxy(array, handler);
 }
 
 export {
