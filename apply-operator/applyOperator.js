94a2ab4489ab64a24dde61157bb11a7ea8638c4e
Rakesh Tembhurne <tj2point0@gmail.com>
2019-06-20 00:02:46 +0530

    Added unfinished changes to apply-operator

--- apply-operator/applyOperator.js
+++ apply-operator/applyOperator.js
@@ -1,6 +1,49 @@
 
-function applyOperator(...args) {
-  return args;
+const operators = {
+  ADDITION: '+',
+  SUBTRACTION: '-',
+  MULTIPLICATION: '*',
+  DIVISION: '/',
+  MODULUS: '%',
+};
+
+function applyOperator(operator, ...numbers) {
+  if (typeof operator === 'undefined' || !Array.isArray(numbers)) {
+    throw new Error('Invalid parameters.');
+  }
+
+  // if (numbers.length === 1) {
+  //   return numbers[0];
+  // }
+
+  let result;
+  switch (operator) {
+    case operators.ADDITION:
+      result = numbers.reduce((acc, num) => acc + num, 0);
+      break;
+
+    case operators.SUBTRACTION:
+      result = numbers.reduce((acc, num) => acc - num, 0);
+      break;
+
+    case operators.MULTIPLICATION:
+      result = numbers.reduce((acc, num) => acc * num, 1);
+      break;
+
+    case operators.DIVISION:
+      result = numbers.reduce((acc, num) => acc / num, 1);
+      result = parseFloat(result.toFixed(4));
+      break;
+
+    case operators.MODULUS:
+      result = numbers.reduce((acc, num) => acc % num, 0);
+      break;
+
+    default:
+      break;
+  }
+
+  return result;
 }
 
 export {
