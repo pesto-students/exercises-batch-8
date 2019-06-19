55d303bef7a07924b64354c0e1d18956140300eb
Radheshyam Etikala <radhey1851@gmail.com>
2019-06-19 06:49:38 +0530

    Completed aperture

--- aperture/aperture.js
+++ aperture/aperture.js
@@ -1,6 +1,11 @@
 
-function aperture(...args) {
-  return args;
+function aperture(windowLength, array) {
+  const tuples = [];
+  for (let i = 0; i + windowLength <= array.length; i += 1) {
+    const tuple = array.slice(i, i + windowLength);
+    tuples.push(tuple);
+  }
+  return tuples;
 }
 
 export {
