05c99864e2ab230836021ca7ee14111ef1cdac77
sourabhmodi20051 <sourabhmodi20051@gmail.com>
2019-06-19 12:56:19 +0530

    Solved queue

--- queue/Queue.js
+++ queue/Queue.js
@@ -1,8 +1,70 @@
+class LinkedListNode {
+  constructor(value, next) {
+    this.value = value;
+    this.next = next;
+  }
+}
 
-function queue(...args) {
-  return args;
+class Queue {
+  constructor() {
+    this.linkedList = {
+      head: null,
+      last: null,
+    };
+  }
+
+  enqueue(element) {
+    const node = new LinkedListNode(element, null);
+    if (this.linkedList.head === null) {
+      this.linkedList.head = node;
+      this.linkedList.last = node;
+    } else {
+      this.linkedList.last.next = node;
+    }
+  }
+
+  dequeue() {
+    const returnValue = this.peek();
+    if (this.linkedList.head != null) {
+      this.linkedList.head = this.linkedList.head.next;
+    }
+    return returnValue;
+  }
+
+  peek() {
+    if (this.linkedList.head == null) {
+      return null;
+    }
+    return this.linkedList.head.value;
+  }
+
+  isEmpty() {
+    return this.linkedList.head === null;
+  }
+
+  [Symbol.iterator]() {
+    let traverser = this.linkedList.head;
+    return {
+      next: () => {
+        if (traverser === null) {
+          traverser = this.linkedList.head;
+          return { value: undefined, done: true };
+        }
+        const currentItem = traverser;
+        traverser = traverser.next;
+        return { value: currentItem.value, done: false };
+      },
+    };
+  }
+
+  toString(stringifier) {
+    if (stringifier === undefined) {
+      return [...this].join(',');
+    }
+    return [...this].map(element => stringifier(element)).join(',');
+  }
 }
 
 export {
-  queue,
+  Queue,
 };
