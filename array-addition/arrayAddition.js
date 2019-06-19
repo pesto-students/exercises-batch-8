058d70110cad52a31964395bff308eabfd8c93a2
kunnu01 <kunalbhatia2409@gmail.com>
2019-06-20 00:01:07 +0530

    WIP: arrayAddition

--- array-addition/arrayAddition.js
+++ array-addition/arrayAddition.js
@@ -1,6 +1,13 @@
 
-function arrayAddition(...args) {
-  return args;
+function arrayAddition(array1, array2) {
+  const instanceOfArray = array => array instanceof Array;
+  if (array1.length === 0 || array2.length === 0) {
+    throw new Error('Array is empty');
+  }
+  if (!instanceOfArray(array1) || !instanceOfArray(array2)) {
+    throw new Error('Arguments must be array');
+  }
+  return 0;
 }
 
 export {
