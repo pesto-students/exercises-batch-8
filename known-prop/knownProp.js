6049b3430e1c6838bc4415f1d64dacb5bc0d4011
prateekinstillion <Prateek Madaan>
2019-06-20 00:30:53 +0530

    Solved the known prop problem

--- known-prop/knownProp.js
+++ known-prop/knownProp.js
@@ -1,8 +1,16 @@
+const handler = {
+  get(target, property) {
+    if (property in target) {
+      return target[property];
+    }
+    const error = new TypeError(`Unknown property: ${property}`);
+    throw error;
+  },
+};
 
-function knownProp(...args) {
-  return args;
+function knownProp(originalObject) {
+  return new Proxy(originalObject, handler);
 }
-
 export {
   knownProp,
 };
