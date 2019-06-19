f7ecf1ea544a128a5c7623b418b253357e1b47ee
saurabh shetty <saurabh.shetty100@gmail.com>
2019-06-19 06:14:42 +0530

    solve ackermann

--- ackermann/ackermann.js
+++ ackermann/ackermann.js
@@ -1,6 +1,20 @@
-
-function ackermann(...args) {
-  return args;
+// A(m,n) = n+1             when m = 0
+// A(m,n) = A(m-1,1)        when m > 0 and n = 0
+// A(m,n) = A(m-1,A(m,n-1)) when m > 0 and n > 0
+const ackermannFunction = (m, n) => {
+  if (m === 0) {
+    return n + 1;
+  }
+  if (m > 0 && n === 0) {
+    return ackermannFunction(m - 1, 1);
+  }
+  if (m > 0 && n > 0) {
+    return ackermannFunction(m - 1, ackermannFunction(m, n - 1));
+  }
+  return null;
+};
+function ackermann({ m, n }) {
+  return ackermannFunction(m, n);
 }
 
 export {
