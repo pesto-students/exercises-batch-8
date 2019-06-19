b7868b07da1a55cf1302505f37adbe8c86014d61
sourabhmodi20051 <sourabhmodi20051@gmail.com>
2019-06-19 13:52:25 +0530

    Sleep Promise solved

--- sleep-promise/sleepPromise.js
+++ sleep-promise/sleepPromise.js
@@ -1,8 +1,15 @@
 
-function sleepPromise(...args) {
-  return args;
+function sleep(time) {
+  const sleepPromise = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, time);
+  });
+  const sleepPromiseThennableFunction = value => sleepPromise.then(() => value);
+  sleepPromiseThennableFunction.then = value => sleepPromise.then(value);
+  return sleepPromiseThennableFunction;
 }
 
 export {
-  sleepPromise,
+  sleep,
 };
