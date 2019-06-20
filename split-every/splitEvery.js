40ed4bd95f6eaa59bb7d3b109ae46198af807516
Jaspreet Singh <erjaspreet57@gmail.com>
2019-06-20 20:21:53 +0530

    Solve split-every

--- split-every/splitEvery.js
+++ split-every/splitEvery.js
@@ -1,6 +1,28 @@
+const slice = (elements, fromIndex, toIndex) => elements.slice(fromIndex, toIndex);
 
-function splitEvery(...args) {
-  return args;
+function splitEvery(len, elements) {
+  if (!Number.isInteger(len)) {
+    throw new Error(`${len} is not integer`);
+  }
+  if (len <= 0) {
+    throw new Error(`${len} is not positive integer`);
+  }
+
+  let startIndex = 0;
+  let endIndex = startIndex + len;
+
+  const result = [];
+
+  while (startIndex < elements.length) {
+    const nextList = slice(elements, startIndex, endIndex);
+    if (nextList.length) {
+      result.push(nextList);
+    }
+
+    startIndex = endIndex;
+    endIndex = startIndex + len;
+  }
+  return result;
 }
 
 export {
