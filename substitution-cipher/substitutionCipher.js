5002889f5d3f380062f7c1dafa936494f43fa31f
Jaspreet Singh <erjaspreet57@gmail.com>
2019-06-20 20:34:11 +0530

    Solve substitution-clipher

--- substitution-cipher/substitutionCipher.js
+++ substitution-cipher/substitutionCipher.js
@@ -1,6 +1,20 @@
+const nextNumberInRange = (min, max, prevNumber, step) => {
+  const next = prevNumber + step;
+  if (next > max) {
+    return (next % max) + min;
+  }
+  return next;
+}
 
-function substitutionCipher(...args) {
-  return args;
+function substitutionCipher(text) {
+  const characters = text.split('');
+
+  const encodedCharacters = [];
+  for (const character of characters) {
+    if (character === '.' || character === ' ' || character === '\n') {
+      encodedCharacters.push(character);
+    }
+  }
 }
 
 export {
