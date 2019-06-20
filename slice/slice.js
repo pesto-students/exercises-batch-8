34fff3e0af1134d878a426e860e056d82be8032c
Radheshyam Etikala <radhey1851@gmail.com>
2019-06-20 07:19:12 +0530

    Completed Slice

--- slice/slice.js
+++ slice/slice.js
@@ -1,6 +1,29 @@
 
 function slice(start, end, iterable) {
-  return iterable.slice(start, end);
+  if (typeof start !== 'number') {
+    if (Number.isNaN(start)) {
+      throw new TypeError('Received NaN, expected a number');
+    }
+    throw new TypeError(`Received ${start} of type ${typeof start}, expected a number`);
+  }
+  if (typeof end !== 'number' || Number.isNaN(end)) {
+    return '';
+  }
+  const { length } = iterable;
+  const properStart = start < 0 ? length + start : start;
+  const properEnd = end < 0 ? length + end : end;
+  const properIterable = typeof iterable === 'string' ? iterable.split('') : iterable;
+
+  const slicedIterable = [];
+  for (const [idx, el] of properIterable.entries()) {
+    if (idx >= properStart && idx < properEnd) {
+      slicedIterable.push(el);
+    }
+  }
+  if (typeof iterable !== 'string') {
+    return slicedIterable;
+  }
+  return slicedIterable.join('');
 }
 
 export {
