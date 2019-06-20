e384d8bee12045a95ee022f4e1e6ce5398b3a9a0
aayushtiwari <arsenicaayush@gmail.com>
2019-06-20 17:47:27 +0530

    updated

--- set-default-property/setDefaultProperty.js
+++ set-default-property/setDefaultProperty.js
@@ -1,8 +1,18 @@
-
-function setDefaultProperty(...args) {
-  return args;
+function setDefaultProperty(inputObj) {
+  const variable = {
+    get(target, trap) {
+      return target.hasOwnProperty(trap) ? target[trap] : 'defaultValue';
+    },
+    set: function name(target, obj) {
+      const objArray = Object.entries(obj);
+      const key = objArray[0][0];
+      const value = objArray[0][1];
+      target.setAttribute(key, value);
+      return target.value;
+    },
+  };
+  const setDefaultProxy = new Proxy(inputObj, variable);
+  return setDefaultProxy;
 }
 
-export {
-  setDefaultProperty,
-};
+export { setDefaultProperty };
