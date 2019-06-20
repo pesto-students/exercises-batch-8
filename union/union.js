5f5a9ff3f9bff8c386bf9ba47761209c256d7e1a
Sebin Benjamin <sebinbenjamin@gmail.com>
2019-06-20 17:45:31 +0530

    Completed exercise 'union'

--- union/union.js
+++ union/union.js
@@ -1,6 +1,24 @@
+/* eslint-disable arrow-body-style */
+/* eslint-disable nonblock-statement-body-position */
+// eslint-disable-next-line arrow-body-style
+const findStrictIndexOf = (array, targetElement) => {
+  let indexOfTarget = -1;
+  array.forEach((currElement, index) => {
+    if (Object.is(currElement, targetElement)) {
+      indexOfTarget = index;
+    }
+  });
+  return indexOfTarget;
+};
 
-function union(...args) {
-  return args;
+const isDuplicate = (element, currentIndex, array) => {
+  // element here shows 0 in place of -0. Hence using array[currentIndex]
+  return findStrictIndexOf(array, array[currentIndex]) === currentIndex;
+};
+
+function union(arrayA, arrayB) {
+  const combinedArray = [...arrayA, ...arrayB];
+  return combinedArray.filter(isDuplicate);
 }
 
 export {
