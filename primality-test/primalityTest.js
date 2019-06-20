65cbb0bef2f118b7ff90105ed1486a0ceb31c06f
Jaspreet Singh <erjaspreet57@gmail.com>
2019-06-21 01:03:06 +0530

    Solve primality-test

--- primality-test/primalityTest.js
+++ primality-test/primalityTest.js
@@ -1,6 +1,17 @@
 
-function primalityTest(...args) {
-  return args;
+function primalityTest(number) {
+  const absoluteNumber = Math.abs(number);
+
+  if (absoluteNumber < 2) {
+    return false;
+  }
+
+  for (let i = 2; i <= Math.sqrt(absoluteNumber); i += 1) {
+    if ((absoluteNumber % i) === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 export {
