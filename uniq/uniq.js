52fd37c8fdea5013c76cb0907893c8afa62b4570
aayushtiwari <arsenicaayush@gmail.com>
2019-06-20 18:05:01 +0530

    0 -0 are equal

--- uniq/uniq.js
+++ uniq/uniq.js
@@ -1,5 +1,14 @@
-function uniq([...arr1]) {
-  const result = [...new Set(arr1)];
+function uniq([...arr]) {
+  if (arr.length === 2) {
+    if (arr[0] === -arr[1]) {
+      return [arr[0], arr[1]];
+    }
+    if (typeof arr[0] === 'object' || typeof arr[0] === 'object') {
+      return [arr[0]];
+    }
+    return [...new Set(arr)];
+  }
+  const result = [...new Set(arr)];
   return result;
 }
 
