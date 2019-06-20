e0cc27ac1997342272382534efc8ce1dbc744f3e
Abdul Khuddus <khuddus1@gmail.com>
2019-06-20 08:23:39 +0530

    Complete theatre

--- theatre/theatre.js
+++ theatre/theatre.js
@@ -1,6 +1,51 @@
 
-function theatre(...args) {
-  return args;
+function theatre(bills) {
+  return bills.reduce((acc, bill) => {
+    if (bill === 50) {
+      if ((acc.twenties * 20 + acc.tens * 10) >= 40) {
+        let balance = bill - 10;
+        while (acc.twenties > 0 && balance >= 20) {
+          acc.twenties -= 1;
+          balance -= 20;
+        }
+        while (acc.tens > 0 && balance >= 10) {
+          acc.tens -= 1;
+          balance -= 10;
+        }
+        if (balance === 0) {
+          acc.fifties += 1;
+        }
+        return acc;
+      }
+      acc.status = acc.status && false;
+      return acc;
+    }
+    if (bill === 20) {
+      if ((acc.tens * 10) >= 10) {
+        let balance = bill - 10;
+        while (acc.tens > 0 && balance >= 10) {
+          acc.tens -= 1;
+          balance -= 10;
+        }
+        if (balance === 0) {
+          acc.twenties += 1;
+        }
+        return acc;
+      }
+      acc.status = acc.status && false;
+      return acc;
+    }
+    if (bill === 10) {
+      acc.tens += 1;
+      return acc;
+    }
+    return acc;
+  }, {
+    tens: 0,
+    twenties: 0,
+    fifties: 0,
+    status: true,
+  }).status;
 }
 
 export {
