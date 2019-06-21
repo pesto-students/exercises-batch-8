f1751387bac884db5ec3acaad286c1bcf440977e
Jaspreet Singh <erjaspreet57@gmail.com>
2019-06-21 06:07:41 +0530

    Solve is-odd

--- is-odd/isOdd.js
+++ is-odd/isOdd.js
@@ -1,6 +1,7 @@
 
-function isOdd(...args) {
-  return args;
+function isOdd(number) {
+  // eslint-disable-next-line no-bitwise
+  return Boolean(number & 1); // odd number has first bit on
 }
 
 export {
