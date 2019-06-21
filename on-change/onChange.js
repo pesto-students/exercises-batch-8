4df92e1955d0823234a45c976ba621a8d22dba83
saurabh shetty <saurabh.shetty100@gmail.com>
2019-06-21 08:33:38 +0530

    solved a few problems

--- on-change/onChange.js
+++ on-change/onChange.js
@@ -1,8 +1,25 @@
+//  wip
+/* eslint-disable no-param-reassign */
+function onChange(object, func) {
+  const handler = {
+    set(target, prop, value) {
+      target[prop] = value;
+      func();
+      return true;
+    },
+    defineProperty(target, prop, value) {
+      target[prop] = value;
+      func();
+      return true;
+    },
+    deleteProperty(target, prop) {
+      delete target[prop];
+      func();
+      return true;
+    },
+  };
 
-function onChange(...args) {
-  return args;
+  return new Proxy(object, handler);
 }
 
-export {
-  onChange,
-};
+export { onChange };
