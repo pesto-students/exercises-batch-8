ce40b6e3294c3a8c4ab1097ed5565f0591f95a4f
Jaspreet Singh <erjaspreet57@gmail.com>
2019-06-21 06:09:29 +0530

    Solve is-even

--- is-even/isEven.js
+++ is-even/isEven.js
@@ -1,6 +1,7 @@
 
-function isEven(...args) {
-  return args;
+function isEven(number) {
+  // eslint-disable-next-line no-bitwise
+  return !(number & 1); // odd number has first bit on
 }
 
 export {
