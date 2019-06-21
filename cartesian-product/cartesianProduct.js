9b50110f6d36c9a05cd779b16d0686b301a7270f
Pradyumna Newalkar <contactpradyumna1@gmail.com>
2019-06-21 18:29:23 +0000

    added solution for cartesian-product

--- cartesian-product/cartesianProduct.js
+++ cartesian-product/cartesianProduct.js
@@ -1,6 +1,15 @@
 
-function cartesianProduct(...args) {
-  return args;
+function cartesianProduct(set1, set2) {
+  if (set1 == null || set2 == null) {
+    return null;
+  }
+  return set1.reduce(
+    (product, elementSet1) => [
+      ...product,
+      ...set2.map(elementSet2 => [elementSet1, elementSet2]),
+    ],
+    [],
+  );
 }
 
 export {
