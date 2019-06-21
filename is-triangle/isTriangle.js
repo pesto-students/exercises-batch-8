4d4f8a56f03aca1e840a5a2ca962834dedff20c1
Jaspreet Singh <erjaspreet57@gmail.com>
2019-06-21 20:56:33 +0530

    Solve is-triangle

--- is-triangle/isTriangle.js
+++ is-triangle/isTriangle.js
@@ -1,6 +1,11 @@
-
-function isTriangle(...args) {
-  return args;
+function isTriangle(side1, side2, side3) {
+  // sum of two sides should be greater than third side
+  // and this condition should be true for all pair of sides
+  return (
+    side1 + side2 > side3
+    && side1 + side3 > side2
+    && side2 + side3 > side1
+  );
 }
 
 export {
