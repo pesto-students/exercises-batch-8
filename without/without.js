72e948f40231aadbd9a5fb3e4c706716ba1857b0
kunnu01 <kunalbhatia2409@gmail.com>
2019-06-23 02:50:44 +0530

    Solved without

--- without/without.js
+++ without/without.js
@@ -1,11 +1,34 @@
+const isNegativeZero = x => x === 0 && 1 / x < 0;
+const containsNegativeZero = array => array.some(num => isNegativeZero(num));
+const containsIsNaN = array => array.some(num => Number.isNaN(num));
 
-// function without(arrayA, arrayB) {
-//   const nonSimilarElements = arrayB.filter((ele) => {
-//     return arrayA.indexOf(ele) < 0 && Math.sign(ele) === Math.sign(arrayA.indexOf(ele));
-//   });
-//   return nonSimilarElements;
-// }
+const arraysAreEqual = (first, second) => (first.length === second.length
+    && first.every((value, index) => value === second[index]));
 
-// export {
-//   without,
-// };
+const containsArray = (el, container) => container.some(array => arraysAreEqual(array, el));
+
+function without(excluder, main) {
+  const excluded = main.filter((el) => {
+    const hasNegativeZero = (el === 0 && (isNegativeZero(el) || containsNegativeZero(excluder)));
+    if (hasNegativeZero) {
+      return true;
+    }
+
+    const hasNaN = Number.isNaN(el) && containsIsNaN(excluder);
+    if (hasNaN) {
+      return false;
+    }
+
+    if (Array.isArray(el) && containsArray(el, excluder)) {
+      return false;
+    }
+
+    return (excluder.indexOf(el) === -1);
+  });
+
+  return excluded;
+}
+
+export {
+  without,
+};
