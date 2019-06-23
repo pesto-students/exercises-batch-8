45a8aa76a9a711513c578b7222733163806c19e8
sourabhmodi20051 <sourabhmodi20051@gmail.com>
2019-06-23 23:51:52 +0530

    Solved Animal Sort

--- animal-sort/animalSort.js
+++ animal-sort/animalSort.js
@@ -1,8 +1,15 @@
-
-function animalSort(...args) {
-  return args;
+function animalSortPredicate() {
+  return (a, b) => {
+    if (a.numberOfLegs === b.numberOfLegs) {
+      return a.name.localeCompare(b.name);
+    }
+    return a.numberOfLegs - b.numberOfLegs;
+  };
 }
 
-export {
-  animalSort,
-};
+function animalSort(animals) {
+  const sortedAnimals = animals.sort(animalSortPredicate());
+  return sortedAnimals;
+}
+
+export { animalSort };
