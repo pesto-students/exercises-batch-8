3d70dd01c3fe52d31e402f5f7ab6948b3885b19b
sourabhmodi20051 <sourabhmodi20051@gmail.com>
2019-06-24 04:02:32 +0530

    Array Cube Root Solved

--- array-cube-root-to-json/arrayCubeRootToJson.js
+++ array-cube-root-to-json/arrayCubeRootToJson.js
@@ -1,8 +1,26 @@
-
-function arrayCubeRootToJson(...args) {
-  return args;
+function validate(array) {
+  if (!(array instanceof Array)) {
+    throw new Error(`Expected Array got ${array}`);
+  }
+  for (const element of array) {
+    if (typeof element !== 'string' && typeof element !== 'number') {
+      throw new Error(`Expected numbers got ${element}`);
+    }
+    if (Number.isNaN(Number(element))) {
+      throw new Error(`Expected numbers got ${element}`);
+    }
+  }
 }
 
-export {
-  arrayCubeRootToJson,
-};
+function arrayCubeRootToJson(array) {
+  validate(array);
+  return array.reduce(
+    (acc, element) => ({
+      ...acc,
+      [String(element)]: Math.cbrt(Number(element)),
+    }),
+    {},
+  );
+}
+
+export { arrayCubeRootToJson };
