5d64c87a3a54e9c40755945b808fa9441dbf7c1e
saurabh shetty <saurabh.shetty100@gmail.com>
2019-06-24 09:32:56 +0530

    solved cartesianProduct

--- cartesian-product/cartesianProduct.js
+++ cartesian-product/cartesianProduct.js
@@ -1,6 +1,11 @@
 
-function cartesianProduct(...args) {
-  return args;
+function cartesianProduct(array1, array2) {
+  const pairs = [];
+  if (!Array.isArray(array1) || !Array.isArray(array2)) {
+    return null;
+  }
+  array1.map(value1 => array2.map(value2 => pairs.push([value1, value2])));
+  return pairs;
 }
 
 export {
