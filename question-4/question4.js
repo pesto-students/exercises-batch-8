a6c97fbe353dd42e85a9b552c88df7b04bdca531
Sebin Benjamin <sebinbenjamin@gmail.com>
2019-06-25 04:35:09 +0530

    completed question-4

--- question-4/question4.js
+++ question-4/question4.js
@@ -1,8 +1,16 @@
+/* eslint-disable no-var */
+/* eslint-disable no-unused-vars */
+var window;
 
-function question4(...args) {
-  return args;
+function question4() {
+  const table = window.document.getElementById('sampleTable');
+  const nextRowIndex = table.rows.length;
+  const row = table.insertRow(nextRowIndex);
+  row.insertCell(0).innerText = `Row${nextRowIndex + 1} cell1`;
+  row.insertCell(1).innerText = `Row${nextRowIndex + 1} cell2`;
 }
 
-export {
-  question4,
+const insertRow = (event) => {
+  event.preventDefault();
+  question4();
 };
