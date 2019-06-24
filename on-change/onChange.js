46b9205d069b9f3ee8872164a988b37cc4e386b1
sourabhmodi20051 <sourabhmodi20051@gmail.com>
2019-06-24 06:27:44 +0530

    WIP On Change

--- on-change/onChange.js
+++ on-change/onChange.js
@@ -1,8 +1,27 @@
-
-function onChange(...args) {
-  return args;
+function onChange(object, callback) {
+  const handler = {
+    get: (target, prop) => {
+      const getResult = Reflect.get(target, prop);
+      callback();
+      return getResult;
+    },
+    set: (target, prop, value) => {
+      const setResult = Reflect.set(target, prop, value);
+      callback();
+      return setResult;
+    },
+    deleteProperty: (target, prop, value) => {
+      callback();
+      const deletePropertyResult = Reflect.deleteProperty(target, prop, value);
+      return deletePropertyResult;
+    },
+    defineProperty: (target, prop, value) => {
+      const definePropertyResult = Reflect.defineProperty(target, prop, value);
+      callback();
+      return definePropertyResult;
+    },
+  };
+  return new Proxy(object, handler);
 }
 
-export {
-  onChange,
-};
+export { onChange };
