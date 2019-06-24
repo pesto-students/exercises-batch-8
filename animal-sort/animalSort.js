3689b079233d0eaebb5ab01a11ef353765658fd1
saurabh shetty <saurabh.shetty100@gmail.com>
2019-06-24 10:38:59 +0530

    solved animalSort

--- animal-sort/animalSort.js
+++ animal-sort/animalSort.js
@@ -1,6 +1,11 @@
 
-function animalSort(...args) {
-  return args;
+function animalSort(animals) {
+  return animals.sort((animal1, animal2) => {
+    if (animal1.numberOfLegs - animal2.numberOfLegs !== 0) {
+      return animal1.numberOfLegs - animal2.numberOfLegs;
+    }
+    return animal1.name > animal2.name;
+  });
 }
 
 export {
