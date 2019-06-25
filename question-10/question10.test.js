8c62d87bc4fc544e258de78b27a20c22df5ae107
prateekinstillion <Prateek Madaan>
2019-06-25 18:27:28 +0530

    Solved first two

---
+++ question-10/question10.test.js
@@ -0,0 +1,5 @@
+describe("question10", () => {
+  it("should access the elements in the DOM", () => {
+    expect(true).toBe(true);
+  });
+});
---
+++ question-9/question9.test.js
@@ -0,0 +1,5 @@
+describe("question9", () => {
+  it("should highlight the bold items", () => {
+    expect(true).toBe(true);
+  });
+});
