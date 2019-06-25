67b3cfb37e78c6edd6c3d3a251bc5ddc3e1d855f
Jaspreet Singh <erjaspreet57@gmail.com>
2019-06-26 01:51:13 +0530

    Solve beginner-exercises

--- beginner-exercises/src/components/03-FavoriteMovie.js
+++ beginner-exercises/src/components/03-FavoriteMovie.js
@@ -34,6 +34,7 @@ class FavoriteMovie extends Component {
     this.state = { movie: '' };
 
     // Warning! If we don't bind this method - we would not be able to update state.
+    this.onMovieChange = this.onMovieChange.bind(this);
   }
 
   /*
@@ -48,15 +49,22 @@ class FavoriteMovie extends Component {
 
   /* eslint-disable no-unused-vars, react/no-unused-state */
   onMovieChange(event) {
-    // Huh... There's something wrong here...
-    this.setState({ badAttribute: 'ChangeME!' });
+    const { value } = event.target;
+    this.setState({ movie: value });
   }
 
   render() {
+    const { movie } = this.state;
     return (
       <div>
-        <p>My favorite movie is <span style={{ color: 'blue' }}>{this.state.movie}</span></p>
-        <input type="text" name="name" onChange={this.onMovieChange} />
+        <p>
+          My favorite movie is
+          <span style={{ color: 'blue' }}>{movie}</span>
+        </p>
+        <input type="text" name="movie" onChange={this.onMovieChange} />
+        {movie.length === 0
+          && <p>Hey there. Enter your favorite movie.</p>
+        }
       </div>
     );
   }
--- beginner-exercises/src/components/04-GroceryList.js
+++ beginner-exercises/src/components/04-GroceryList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 /*
   In this exercises, you'll will make a reactive grocery list.
@@ -24,11 +25,34 @@ class GroceryList extends React.Component {
     super(props);
     this.state = {
       groceries: [{ name: 'Apples' }, { name: 'KitKat' }, { name: 'Red Bull' }],
+      groceryName: '',
     };
+
+    this.addGroceryItem = this.addGroceryItem.bind(this);
+    this.onGroceryNameChange = this.onGroceryNameChange.bind(this);
+    this.clearList = this.clearList.bind(this);
+  }
+
+  onGroceryNameChange(event) {
+    this.setState({ groceryName: event.target.value });
+  }
+
+  addGroceryItem() {
+    const { groceries, groceryName } = this.state;
+    this.setState({
+      groceries: [...groceries, { name: groceryName }],
+      groceryName: '',
+    });
+  }
+
+  clearList() {
+    this.setState({
+      groceries: [],
+    });
   }
 
   render() {
-    const { groceries } = this.state;
+    const { groceries, groceryName } = this.state;
     /*
       Properties are a way to pass parameters to your React components.
       We mentioned this in the third exercise. Properties are to React
@@ -37,13 +61,21 @@ class GroceryList extends React.Component {
       Below you can see how to pass properties to child components.
       We have defined a `grocery` property for each `GroceryListItem`.
     */
-    const groceriesComponents = groceries.map(item => ( // eslint-disable-line no-unused-vars
-      <GroceryListItem grocery={item} />
+    const groceriesComponents = groceries.map((item, i) => ( // eslint-disable-line no-unused-vars
+      <GroceryListItem key={item.name} grocery={item} />
     ));
     // Hint: Don't forget about putting items into `ul`
     return (
       <div>
-        Put your code here
+        <ul>
+          {groceriesComponents}
+        </ul>
+
+        <p>Input New Grocery Name</p>
+        <input type="text" name="grocery" value={groceryName} onChange={this.onGroceryNameChange} />
+        <button id="add-button" type="button" onClick={this.addGroceryItem}>Add</button>
+         &nbsp;
+        <button id="clear-button" type="button" onClick={this.clearList}>Clear List</button>
       </div>
     );
   }
@@ -55,17 +87,54 @@ class GroceryList extends React.Component {
 class GroceryListItem extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      purchased: false,
+    };
+    this.togglePurchase = this.togglePurchase.bind(this);
+    this.getColor = this.getColor.bind(this);
+  }
+
+  getColor() {
+    const { purchased } = this.state;
+    if (purchased) {
+      return 'red';
+    }
+    return 'black';
+  }
+
+  togglePurchase() {
+    const { purchased } = this.state;
+    this.setState({
+      purchased: !purchased,
+    });
   }
 
   render() {
+    const { grocery: { name } } = this.props;
     return (
       <li>
-        Put your code here.
+        <button
+          type="button"
+          className="grocery-item-name"
+          onClick={this.togglePurchase}
+          style={{ color: this.getColor() }}
+        >
+          {
+            name
+          }
+        </button>
       </li>
     );
   }
 }
 
 // Do prop validation here using the package `prop-types`
+
+
+GroceryListItem.propTypes = {
+  grocery: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default GroceryList;
