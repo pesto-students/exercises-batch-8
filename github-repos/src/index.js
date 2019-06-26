87192f0abafd43572c7f781bd1cbe296aa95c8a0
sourabhmodi20051 <sourabhmodi20051@gmail.com>
2019-06-27 05:14:40 +0530

    Github Repo WIP

--- github-repos/src/index.js
+++ github-repos/src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootReducer = (state = {}, action) => {
+  switch (action.type) {
+    case 'GITHUB_REPOS':
+      return { ...state, repos: action.repos };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
 registerServiceWorker();
+
+export default store;
