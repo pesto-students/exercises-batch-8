f73fefec558ea4ac3d82b358656b3685a0e4d367
Jaspreet Singh <erjaspreet57@gmail.com>
2019-06-26 21:41:40 +0530

    Solved cache-function

--- cache-function/cacheFunction.js
+++ cache-function/cacheFunction.js
@@ -1,7 +1,16 @@
+const cacheFunction = (cb) => {
+  const cache = new Map();
+  return (...args) => {
+    const serializedArgs = JSON.stringify(args);
+    if (cache.has(serializedArgs)) {
+      return cache.get(serializedArgs);
+    }
 
-function cacheFunction(...args) {
-  return args;
-}
+    const result = cb(...args);
+    cache.set(serializedArgs, result);
+    return result;
+  };
+};
 
 export {
   cacheFunction,
