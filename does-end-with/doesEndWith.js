df86c5acce4e20fe3f74464e678f14dd49de3c66
saurabh shetty <saurabh.shetty100@gmail.com>
2019-06-26 09:06:30 +0530

    solved endsWith and dupLetters

--- does-end-with/doesEndWith.js
+++ does-end-with/doesEndWith.js
@@ -1,6 +1,13 @@
 
-function doesEndWith(...args) {
-  return args;
+function doesEndWith(endsWith, stringOrArray) {
+  const lastElements = stringOrArray
+    .slice(stringOrArray.length - endsWith.length, stringOrArray.length);
+  if (Array.isArray(stringOrArray)) {
+    const stringifiedLastElements = lastElements.toString();
+    const stringifiedEndsWith = endsWith.toString();
+    return stringifiedLastElements === stringifiedEndsWith;
+  }
+  return lastElements === endsWith;
 }
 
 export {
--- duplicate-letters/duplicateLetters.js
+++ duplicate-letters/duplicateLetters.js
@@ -1,6 +1,24 @@
 
-function duplicateLetters(...args) {
-  return args;
+function duplicateLetters(string) {
+  const repetitions = {};
+  // eslint-disable-next-line guard-for-in
+  for (const character of string.split('')) {
+    if (repetitions[character] === undefined) {
+      repetitions[character] = 1;
+    } else {
+      repetitions[character] += 1;
+    }
+  }
+  if (Object.values(repetitions).every(noOfRepititions => noOfRepititions === 1)) {
+    return false;
+  }
+  const maxRepetitions = Object.keys(repetitions).reduce((max, chara) => {
+    if (repetitions[chara] > max) {
+      return repetitions[chara];
+    }
+    return max;
+  }, 1);
+  return maxRepetitions;
 }
 
 export {
