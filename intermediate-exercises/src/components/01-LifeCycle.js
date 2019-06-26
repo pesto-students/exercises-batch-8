b36e2ceb09d3accaee85e3778ff0ec7bbe7c3a2d
Rakesh Tembhurne <tj2point0@gmail.com>
2019-06-27 00:11:48 +0530

    WIP Refactored code of stop watch intermediate-exercises

--- intermediate-exercises/src/components/01-LifeCycle.js
+++ intermediate-exercises/src/components/01-LifeCycle.js
@@ -1,13 +1,5 @@
 import React from 'react';
 
-/*
-  Tasks for this exercise are in comments inside the component class code.
-  In this exercise lifecycle methods will be used to provide convenient debug
-  messages in developer's console.
-  There are more lifecycle methods available.
-  Those three presented are commonly used.
- */
-
 class LifeCycle extends React.Component {
   constructor(props) {
     super(props);
@@ -34,7 +26,6 @@ class LifeCycle extends React.Component {
     this.setState({ name: 'India' });
   }
 
-  // Task 4: Create a button which triggers ComponentDidUpdate lifecycle method on click.
   render() {
     return (
       <div>
