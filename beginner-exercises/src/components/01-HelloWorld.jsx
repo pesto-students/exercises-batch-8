60bc60f01faf2e81554908bb7dcbcd0c3bd8c957
saurabh shetty <saurabh.shetty100@gmail.com>
2019-06-26 23:03:23 +0530

    removed comments from react app

--- beginner-exercises/src/components/01-HelloWorld.jsx
+++ beginner-exercises/src/components/01-HelloWorld.jsx
@@ -1,11 +1,5 @@
 import React from 'react';
 
-// Task: Render "Hello World" text.
-// All components *must* have a `render` method defined.
-
-// To define a component's render method, we use syntax called JSX. As you
-// can see it looks similar to HTML. JSX gets converted to
-// JavaScript code. It is here just for readability purposes.
 const HelloWorld = () => <h1>Hello World</h1>;
 
 export default HelloWorld;
--- beginner-exercises/src/components/04-GroceryList.jsx
+++ beginner-exercises/src/components/04-GroceryList.jsx
@@ -1,23 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-/*
-  In this exercises, you'll will make a reactive grocery list.
-
-  Task 1: Fill the `return` of `GroceryList` render method. It should return
-        a list of `GroceryListItem`. You need to display the groceries names
-        using `this.props` in `GroceryListItem`. We already prepared the variable
-        `groceriesComponents` inside `render` method containing a list of these items for you.
-
-
-  Task 2: Create an input box and a button. User should be able to add more grocery items and click
-          the `Add` button to add it to the list displaying the item.
-
-  Task 3: Create a button to clear the whole list.
-
-  Task 4: Clicking on a grocery item should change its color to red. Clicking again should change
-          it back to black. Red means the item has been purchased.
-
-*/
 
 class GroceryList extends React.Component {
   constructor(props) {
@@ -61,14 +43,6 @@ class GroceryList extends React.Component {
   }
   render() {
     const { groceries } = this.state;
-    /*
-      Properties are a way to pass parameters to your React components.
-      We mentioned this in the third exercise. Properties are to React
-      components what attributes are to HTML elements.
-
-      Below you can see how to pass properties to child components.
-      We have defined a `grocery` property for each `GroceryListItem`.
-    */
     const groceriesComponents = groceries.map((item, index) => (
       <GroceryListItem
         color={item.purchased ? 'red' : 'black'}
@@ -78,7 +52,6 @@ class GroceryList extends React.Component {
         clickEventFunction={this.purchaseItem}
       />
     ));
-    // Hint: Don't forget about putting items into `ul`
     return (
       <div>
         {this.state.groceries.length === 0 ? 'List is Empty' : null}
@@ -91,9 +64,6 @@ class GroceryList extends React.Component {
   }
 }
 
-// Render grocery name from component's properties.
-// If you have a problem, check `this.props` in the console.
-/* eslint-disable react/no-multi-comp, no-useless-constructor */
 
 const GroceryListItem = props => (
   <li>
@@ -108,7 +78,6 @@ const GroceryListItem = props => (
   </li>
 );
 
-// Do prop validation here using the package `prop-types`
 GroceryListItem.propTypes = {
   grocery: PropTypes.shape({
     name: PropTypes.string.isRequired,
