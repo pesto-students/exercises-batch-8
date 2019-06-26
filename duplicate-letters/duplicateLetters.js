3957c247d7a2b83e76765b5fd3efddc5df8c630f
Radheshyam Etikala <radhey1851@gmail.com>
2019-06-26 08:30:39 +0530

    completed duplicateLetters

--- duplicate-letters/duplicateLetters.js
+++ duplicate-letters/duplicateLetters.js
@@ -1,6 +1,18 @@
 
-function duplicateLetters(...args) {
-  return args;
+function duplicateLetters(string) {
+  const charCountStore = new Map();
+  for (const char of string) {
+    if (charCountStore.has(char)) {
+      const charCount = charCountStore.get(char);
+      charCountStore.set(char, charCount + 1);
+    } else {
+      charCountStore.set(char, 1);
+    }
+  }
+  if (charCountStore.size === string.length) {
+    return false;
+  }
+  return Math.max(...charCountStore.values());
 }
 
 export {
