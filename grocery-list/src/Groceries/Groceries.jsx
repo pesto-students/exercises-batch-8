87845fa2711b489b8f35e7765767d3356740985a
saurabh shetty <saurabh.shetty100@gmail.com>
2019-06-27 10:07:47 +0530

    added delete in groceries

--- grocery-list/src/Groceries/Groceries.jsx
+++ grocery-list/src/Groceries/Groceries.jsx
@@ -5,7 +5,7 @@ class Groceries extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      groceryName: '',
+      groceryName: ''
     };
     this.handleGroceryNameChange = this.handleGroceryNameChange.bind(this);
     this.addItem = this.addItem.bind(this);
@@ -19,19 +19,41 @@ class Groceries extends Component {
   addItem() {
     this.props.addItem(this.state.groceryName);
   }
-
+  updateItem(newName, index) {
+    this.props.updateItem(newName, index);
+  }
   clearItems() {
     this.props.clearItems();
   }
+  deleteItem(index) {
+    this.props.deleteItem(index);
+  }
 
+  purchaseItem(index) {
+    this.props.purchaseItem(index);
+  }
   render() {
     const { groceries } = this.props;
+    console.log(groceries);
     return (
       <div>
         <h1>Groceries</h1>
         <ul>
-          {groceries.map(grocery => (
-            <li>{grocery}</li>
+          {groceries.map((grocery, index) => (
+            <li>
+              <button
+                style={{
+                  color: grocery.purchased ? 'red' : 'black',
+                  background: 'white',
+                  border: 'none',
+                  fontSize: '1em'
+                }}
+                onClick={() => this.purchaseItem(index)}
+              >
+                {grocery.itemName}
+              </button>{' '}
+              <button onClick={() => this.deleteItem(index)}>x</button>
+            </li>
           ))}
         </ul>
         <input
@@ -45,11 +67,14 @@ class Groceries extends Component {
   }
 }
 const mapStateToProps = state => ({
-  groceries: state.groceries,
+  groceries: state.groceries
 });
 const mapDispatchToProps = dispatch => ({
   addItem: itemName => dispatch({ type: 'ADD_ITEM', itemName }),
+  purchaseItem: index => dispatch({ type: 'PURCHASE_ITEM', index }),
+  editItem: (newName, index) => dispatch({ type: 'EDIT_ITEM', index, newName }),
   clearItems: () => dispatch({ type: 'CLEAR_ITEMS' }),
+  deleteItem: index => dispatch({ type: 'DELETE_ITEM', index }),
 });
 const GroceriesWithState = connect(
   mapStateToProps,
--- grocery-list/src/redux/GroceriesReducer.js
+++ grocery-list/src/redux/GroceriesReducer.js
@@ -1,9 +1,26 @@
-const GroceriesReducer = (state = { groceries: ['aa', 'bb'] }, action) => {
-  console.log('inside reducer', state, action);
+const GroceriesReducer = (
+  state = { groceries: [{ itemName: 'aa' }, { itemName: 'bb' }] },
+  action
+) => {
   switch (action.type) {
     case 'ADD_ITEM': {
       const updatedGroceries = state.groceries;
-      updatedGroceries.push(action.itemName);
+      updatedGroceries.push({ itemName: action.itemName });
+      return { ...state, groceries: updatedGroceries };
+    }
+    case 'PURCHASE_ITEM': {
+      const updatedGroceries = state.groceries;
+      updatedGroceries[action.index].purchased = !updatedGroceries[action.index]
+        .purchased;
+      return { ...state, groceries: updatedGroceries };
+    }
+    case 'EDIT_ITEM': {
+      const updatedGroceries = state.groceries;
+      updatedGroceries[action.index].itemName = action.newName;
+      return { ...state, groceries: updatedGroceries };
+    }
+    case 'DELETE_ITEM': {
+      const updatedGroceries = state.groceries.filter((grocery, index) => index !== action.index);
       return { ...state, groceries: updatedGroceries };
     }
     case 'CLEAR_ITEMS':
