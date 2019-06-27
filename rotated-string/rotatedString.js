3e1c53b550c7bd6edc993850948c18fb02d3e361
Rakesh Tembhurne <tj2point0@gmail.com>
2019-06-27 19:11:21 +0530

    Complete rotated-string

--- rotated-string/rotatedString.js
+++ rotated-string/rotatedString.js
@@ -1,6 +1,16 @@
 
-function rotatedString(...args) {
-  return args;
+
+function rotatedString(string, reversedString) {
+  const fnRotate = (characters) => {
+    characters.push(characters.shift());
+    return characters;
+  };
+
+  const characters = string.split('');
+  const reversed = fnRotate(fnRotate(characters));
+  const strReversed = reversed.join('');
+
+  return strReversed === reversedString;
 }
 
 export {
