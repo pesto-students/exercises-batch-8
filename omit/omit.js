3a23ee073b9c8f3c25595093f25d228441e6decb
saurabh shetty <saurabh.shetty100@gmail.com>
2019-06-28 00:54:23 +0530

    partial omit

--- omit/omit.js
+++ omit/omit.js
@@ -1,8 +1,11 @@
-
-function omit(...args) {
-  return args;
+function omit(keysToFilter, object) {
+  return Object.keys(object).reduce((acc, key) => {
+    const updatedAcc = acc;
+    if (!keysToFilter.includes(key)) {
+      updatedAcc[key] = object[key];
+    }
+    return updatedAcc;
+  }, {});
 }
 
-export {
-  omit,
-};
+export { omit };
