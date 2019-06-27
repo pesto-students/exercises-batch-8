1f897fd4aa1fceab226a5142c54876bdf793d075
prateekinstillion <Prateek Madaan>
2019-06-28 02:00:42 +0530

    Solved the omit problem

--- omit/omit.js
+++ omit/omit.js
@@ -1,8 +1,17 @@
+const getDesiredProperties = (obj, isDesired) =>
+  Object.entries(obj).reduce((acc, [key, value]) => {
+    if (isDesired(key)) {
+      acc[key] = value;
+    }
+    return acc;
+  }, {});
+function omit(listOfPropsToOmit, obj) {
+  const isDesired = prop => !listOfPropsToOmit.includes(prop);
 
-function omit(...args) {
-  return args;
+  const prototypeProps = Object.getPrototypeOf(obj);
+  return {
+    ...getDesiredProperties(obj, isDesired),
+    ...getDesiredProperties(prototypeProps, isDesired)
+  };
 }
-
-export {
-  omit,
-};
+export { omit };
--- setup-router/src/App.js
+++ setup-router/src/App.js
@@ -1,11 +1,11 @@
-import React, { Component, Fragment } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
+import React, { Component, Fragment } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
-import './styles/App.css';
+import "./styles/App.css";
 
 class App extends Component {
   render() {
@@ -13,9 +13,11 @@ class App extends Component {
       <Router>
         <Fragment>
           <Navbar />
-          <Route exact path="/" component={Home} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/contact" component={Contact} />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/contact" component={Contact} />
+          </Switch>
         </Fragment>
       </Router>
     );
