a071f2aed76c18c600f8856dac389cf8f5fcdae8
saurabh shetty <saurabh.shetty100@gmail.com>
2019-06-28 00:40:32 +0530

    solved setup Router

--- setup-router/src/App.js
+++ setup-router/src/App.js
@@ -1,20 +1,22 @@
 import React, { Component, Fragment } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
-
 import './styles/App.css';
 
 class App extends Component {
   render() {
     return (
       <Fragment>
-        <Navbar />
-        <Home />
-        <About />
-        <Contact />
+        <BrowserRouter>
+          <Navbar />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/" exact component={Home} />
+        </BrowserRouter>
       </Fragment>
     );
   }
