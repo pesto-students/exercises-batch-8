b7dcc625a14780b5f4166c6a5c269b1bd7858ec4
sourabhmodi20051 <sourabhmodi20051@gmail.com>
2019-06-28 00:44:04 +0530

    Implement Router

--- setup-router/src/components/Navbar.js
+++ setup-router/src/components/Navbar.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 
 import '../styles/App.css';
+import { NavLink } from 'react-router-dom';
 
 export default class Navbar extends Component {
   render() {
     return (
       <ul className="nav">
-        <li className="active">Home</li>
-        <li>About Us</li>
-        <li>Contact</li>
+        <li className="active">
+          <NavLink to="/home">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/about">About Us</NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact">Contact</NavLink>
+        </li>
       </ul>
     );
   }
--- setup-router/src/index.js
+++ setup-router/src/index.js
@@ -1,11 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from "./App";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root'),
+import registerServiceWorker from "./registerServiceWorker";
+import { Route, BrowserRouter as Router } from "react-router-dom";
+
+const routing = (
+  <Router>
+    <div>
+      <Route path="/" component={App} />
+      <Route path="/home" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/contact" component={Contact} />
+    </div>
+  </Router>
 );
+
+ReactDOM.render(routing, document.getElementById("root"));
 registerServiceWorker();
