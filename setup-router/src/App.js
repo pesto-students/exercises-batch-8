c4468d70982aed81368ca419e773ed37511b2a5e
Radheshyam Etikala <radhey1851@gmail.com>
2019-06-28 00:59:15 +0530

    complete setup-router

--- setup-router/src/App.js
+++ setup-router/src/App.js
@@ -1,21 +1,28 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
+import Error404 from './components/Error404';
 
 import './styles/App.css';
 
 class App extends Component {
   render() {
     return (
-      <Fragment>
+      <Router>
+
         <Navbar />
-        <Home />
-        <About />
-        <Contact />
-      </Fragment>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route component={Error404} />
+        </Switch>
+
+      </Router>
     );
   }
 }
