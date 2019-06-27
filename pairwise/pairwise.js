158ebf835a9e3cdb405cd6c3d167fb6802949bd3
Rakesh Tembhurne <tj2point0@gmail.com>
2019-06-27 19:45:12 +0530

    Complete pairwise

--- pairwise/pairwise.js
+++ pairwise/pairwise.js
@@ -1,6 +1,24 @@
 
-function pairwise(...args) {
-  return args;
+function pairwise(numbers, sum) {
+  const indices = [];
+
+  numbers.map((leftNumber, leftIndex) => {
+    numbers.map((rightNumber, rightIndex) => {
+      if (leftNumber + rightNumber === sum
+        && leftIndex !== rightIndex
+        && !indices.includes(leftIndex)
+        && !indices.includes(rightIndex)
+      ) {
+        indices.push(leftIndex, rightIndex);
+      }
+      return null;
+    });
+    return null;
+  });
+
+  const sumOfIndices = indices.reduce((a, b) => a + b, 0);
+
+  return sumOfIndices;
 }
 
 export {
