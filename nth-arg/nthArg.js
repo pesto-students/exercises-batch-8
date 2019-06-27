394270b908185b936be0ed88591c260ad27cd22c
Jaspreet Singh <erjaspreet57@gmail.com>
2019-06-28 03:14:23 +0530

    Solved nth-arg

--- nth-arg/nthArg.js
+++ nth-arg/nthArg.js
@@ -1,7 +1,29 @@
+const positiveArg = (argNum) => {
+  const argsReceived = [];
+  const curried = (...args) => {
+    args.forEach(arg => argsReceived.push(arg));
+    if (argsReceived.length >= argNum) {
+      return argsReceived[argNum - 1];
+    }
+    return curried;
+  };
 
-function nthArg(...args) {
-  return args;
-}
+  Object.defineProperty(curried, 'length', { value: argNum });
+  return curried;
+};
+
+const negativeArg = (argNum) => {
+  const handler = (...args) => args[args.length + argNum];
+  Object.defineProperty(handler, 'length', { value: 1 });
+  return handler;
+};
+
+const nthArg = (num) => {
+  if (num >= 0) {
+    return positiveArg(num + 1);
+  }
+  return negativeArg(num);
+};
 
 export {
   nthArg,
