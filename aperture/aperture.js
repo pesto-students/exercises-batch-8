ccf65c33313dfed7f0b1bb26ff5e1f5baa095662
Abhinav Sharma <abhi18av@outlook.com>
2019-06-28 18:35:35 +0530

    made progress with many problems

--- aperture/aperture.js
+++ aperture/aperture.js
@@ -1,6 +1,17 @@
 
-function aperture(...args) {
-  return args;
+function aperture(n, array) {
+  if (n > array.length) {
+    return [];
+  }
+  let count = 0;
+  const transformedArray = [];
+
+  array.forEach((element, index) => {
+    if (index < array.length - n + 1) {
+      transformedArray.push(array.slice(index, index + n))
+    }
+  });
+  return transformedArray;
 }
 
 export {
--- array-addition/arrayAddition.js
+++ array-addition/arrayAddition.js
@@ -1,32 +1,30 @@
-// WIP
+const isNumber = num => typeof num === 'number';
 
-
-function resizeSmallerArray(arr1, arr2) {
-    let array1 = arr1;
-    let array2 = arr2;
-    const sizeArray1 = array1.length;
-    const sizeArray2 = array2.length;
-    
-    if (sizeArray1 < sizeArray2) {
-        let sizeDiff = sizeArray2 - sizeArray1;
-        for (let i = 0; i < sizeDiff; i++) {
-            array1.push(0);
-        }
-        return array1;
-    } 
-
-    let sizeDiff = sizeArray1 - sizeArray2;
-    for (let i = 0; i < sizeDiff; i++) {
-            array2.push(0);
+function addArrays(array1, array2) {
+  let index = 0;
+  const summationArray = array1.map((el) => {
+    const secondArrayEl = array2[index];
+    if (!isNumber(el)) {
+      throw new Error(`Expected numerical values. Received ${typeof el}.`);
     }
-    return array2;
-    
+    if (!isNumber(secondArrayEl)) {
+      throw new Error(`Expected numerical values. Received ${typeof secondArrayEl}.`);
+    }
+    index += 1;
+    return el + secondArrayEl;
+  });
+  const extraElements = array2.slice(index);
+  return summationArray.concat(extraElements);
 }
 
 function arrayAddition(array1, array2) {
-  
+  if (array1.length === 0 || array2.length === 0) {
+    throw new Error('Received empty array.');
+  }
+  if (array1.length <= array2.length) {
+    return addArrays(array1, array2);
+  }
+  return addArrays(array2, array1);
 }
 
-export {
-  arrayAddition,
-};
+export { arrayAddition };
