ea8f0077a8f4f12608f7f51fb76225cb52f767de
Abhinav Sharma <abhi18av@outlook.com>
2019-06-28 18:18:14 +0530

    completed all exercises

--- sleep-promise/sleepPromise.js
+++ sleep-promise/sleepPromise.js
@@ -1,8 +1,20 @@
 
-function sleepPromise(...args) {
-  return args;
+function sleepPromise(delay) {
+  return new Promise(resolve => setTimeout(resolve, delay));
+}
+
+function sleep(delay) {
+  const newSleepPromise = sleepPromise(delay);
+
+  function functionThen(value) {
+    return newSleepPromise.then(() => value);
+  }
+
+  functionThen.then = args => newSleepPromise.then(args);
+  functionThen.catch = Promise.resolve().catch;
+  return functionThen;
 }
 
 export {
-  sleepPromise,
+  sleep,
 };
