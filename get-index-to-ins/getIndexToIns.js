bb6f6516521652b81e36e97c0c0d2cc455f068bf
Jaspreet Singh <erjaspreet57@gmail.com>
2019-06-29 00:32:38 +0530

    Solving get-index-to-ins

--- get-index-to-ins/getIndexToIns.js
+++ get-index-to-ins/getIndexToIns.js
@@ -1,6 +1,20 @@
+function getIndexToIns(array, value) {
+  const sorted = array.sort();
 
-function getIndexToIns(...args) {
-  return args;
+  // Using binary search algorithm
+  let left = 0;
+  let right = array.length - 1;
+  let mid = Number.parseInt(array.length / 2, 10);
+  console.log(mid);
+  while (left < right) {
+    if (value > mid) {
+      left = mid;
+    } else {
+      right = mid;
+    }
+    mid = Number.parseInt((left + right) / 2, 10);
+    console.log(left, right);
+  }
 }
 
 export {
