a872a88255aa085a086e946fd702dff3c11fe637
prateekinstillion <Prateek Madaan>
2019-06-29 00:49:56 +0530

    Solved the reject problem

--- reject/reject.js
+++ reject/reject.js
@@ -1,6 +1,8 @@
-
-function reject(...args) {
-  return args;
+function reject(checkFunction, values) {
+  if (Array.isArray(values) && values.length === 0) {
+    return [];
+  }
+  return values.filter(val => !checkFunction(val));
 }
 
 export {
