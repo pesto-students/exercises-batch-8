b9cd351c176979c2959535a38deace75d7247abe
Radheshyam Etikala <radhey1851@gmail.com>
2019-06-29 04:30:15 +0530

    completed test-basics

--- test-basics/src/App.js
+++ test-basics/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import './styles/App.css';
 
@@ -9,19 +10,44 @@ class App extends Component {
     input: '',
     mainColor: 'blue',
   }
+
+  handleClick = () => {
+    this.setState({ ...this.state, on: !this.state.on });
+  }
+
+  handleChange = (e) => {
+    this.setState({ ...this.state, input: e.currentTarget.value });
+  }
+
+  handleStrings = str => str.length > 0;
+
   render() {
+    const { input, on, mainColor } = this.state;
     return (
       <div className="App">
-        Welcome to React
+        <h1 className={mainColor}>Welcome to React</h1>
+
+        <p className="button-state">{on ? 'Yes!' : 'No!'}</p>
+        <button onClick={this.handleClick}>Click me</button>
+
+        <h2>{input}</h2>
+        <input type="text" value={input} onChange={this.handleChange} />
       </div>
     );
   }
 }
 
+
 export class Link extends Component {
   render() {
-    return null;
+    const { address, hide } = this.props;
+    return hide ? null : <a href={address}>link</a>;
   }
 }
+
+Link.propTypes = {
+  address: PropTypes.string.isRequired,
+  hide: PropTypes.bool.isRequired,
+};
 
 export default App;
