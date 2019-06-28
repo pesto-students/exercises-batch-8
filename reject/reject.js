c051216b8788cffeefafba7a958f192a15525aa6
prateekinstillion <Prateek Madaan>
2019-06-29 02:05:34 +0530

    Solved the test basics

--- reject/reject.js
+++ reject/reject.js
@@ -1,8 +1,5 @@
-function reject(checkFunction, values) {
-  if (Array.isArray(values) && values.length === 0) {
-    return [];
-  }
-  return values.filter(val => !checkFunction(val));
+function reject(...args) {
+  return args;
 }
 
 export {
