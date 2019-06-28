d7e5a6f73bcd31a0ec173d36bc1db314d89fc5a5
Jaspreet Singh <erjaspreet57@gmail.com>
2019-06-28 20:45:47 +0530

    Solved diff-array

--- diff-array/diffArray.js
+++ diff-array/diffArray.js
@@ -1,6 +1,17 @@
 
-function diffArray(...args) {
-  return args;
+function diffArray(array1, array2) {
+  const diff = [];
+
+  array1.forEach((element) => {
+    const indexInArray2 = array2.indexOf(element);
+    if (indexInArray2 > -1) {
+      array2.splice(indexInArray2, 1);
+    } else {
+      diff.push(element);
+    }
+  });
+
+  return diff.concat(array2);
 }
 
 export {
