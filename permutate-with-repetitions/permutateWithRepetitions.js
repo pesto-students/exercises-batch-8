eed4dc6b67ebdfbaeaae303ee09b94da2a411d0d
Abhinav Sharma <abhi18av@outlook.com>
2019-06-28 17:40:44 +0530

    completed all exercises

--- permutate-with-repetitions/permutateWithRepetitions.js
+++ permutate-with-repetitions/permutateWithRepetitions.js
@@ -1,24 +1,21 @@
-function alienAlphabet(stringArray) {
-  var array = [];
-  for (var i = 0; i < stringArray.length - 1; i++) {
-    array.push([stringArray[i], stringArray[i + 1]]);
-  }
-  var sortedOrderChar = [];
-  var distinctChar = {};
-  array.map(element => {
-    var char1 = element[0][0];
-    var char2 = element[1][0];
-    if (!distinctChar[char1]) {
-      sortedOrderChar.push(char1);
-      distinctChar[char1] = true;
-    }
+var resArray = [];
 
-    if (!distinctChar[char2]) {
-      sortedOrderChar.push(char2);
-      distinctChar[char2] = true;
-    }
-  });
-  return sortedOrderChar;
+function recursivePermutate(charArray, permutation) {
+  if (charArray.length === permutation.length) {
+    return resArray.push(permutation);
+  }
+  var permutationCopy = permutation.slice();
+  for (var i = 0; i < charArray.length; i++) {
+    permutationCopy.push(charArray[i]);
+    recursivePermutate(charArray, permutationCopy);
+    permutationCopy = permutation.slice();
+  }
 }
 
-export { alienAlphabet };
+function permutateWithRepetitions(charArray) {
+  resArray = [];
+  recursivePermutate(charArray, []);
+  return resArray;
+}
+
+export { permutateWithRepetitions };
