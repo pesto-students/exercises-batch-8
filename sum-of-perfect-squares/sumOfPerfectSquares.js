2955cbe3f7eae740d222d29af0fcb6801186ff70
Jaspreet Singh <erjaspreet57@gmail.com>
2019-06-28 10:30:23 +0530

    Solved sum-of-perfect-squares

--- sum-of-perfect-squares/sumOfPerfectSquares.js
+++ sum-of-perfect-squares/sumOfPerfectSquares.js
@@ -1,7 +1,25 @@
+// Recursive approach
+function sumOfPerfectSquares(num) {
+  // base cases
+  if (num <= 3) {
+    return num;
+  }
 
-function sumOfPerfectSquares(...args) {
-  return args;
+  // Maximum number of squares possible are n (1*1 + 1*1 + ..)
+  let lengthOfSquares = num;
+
+  // Go through all smaller numbers to recursively find minimum
+  for (let i = 1; i <= num; i += 1) {
+    const temp = i * i;
+    if (temp > num) {
+      break;
+    } else {
+      lengthOfSquares = Math.min(lengthOfSquares, 1 + sumOfPerfectSquares(num - temp));
+    }
+  }
+  return lengthOfSquares;
 }
+
 
 export {
   sumOfPerfectSquares,
