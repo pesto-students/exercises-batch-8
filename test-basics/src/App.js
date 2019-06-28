610b382ab0d735ac7cd18cdb0deb41b16f340fba
Jaspreet Singh <erjaspreet57@gmail.com>
2019-06-29 02:11:55 +0530

    Solved test-basic

--- test-basics/src/App.js
+++ test-basics/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
 
 import './styles/App.css';
 
@@ -9,10 +11,39 @@ class App extends Component {
     input: '',
     mainColor: 'blue',
   }
+
+  toggleState = () => {
+    const { on } = this.state;
+    this.setState({
+      on: !on,
+    });
+  }
+
+  handleChange = (event) => {
+    const { value } = event.target || event.currentTarget;
+    this.setState({
+      input: value,
+    });
+  }
+
+  handleStrings = (mayBeString) => {
+    return Boolean(typeof mayBeString === 'string' && mayBeString.length);
+  }
+
   render() {
+    const { mainColor, on, input } = this.state;
+    const appClass = classNames(
+      'App',
+      { [mainColor]: true },
+    );
     return (
-      <div className="App">
-        Welcome to React
+      <div className={appClass}>
+        <h1>Welcome to React</h1>
+        <h2>{input}</h2>
+        <input type="text" value={input} onChange={this.handleChange} />
+        <br />
+        <button type="button" className="btn" onClick={this.toggleState}>Toggle State</button>
+        <p className="button-state">{on ? 'Yes!' : 'No!'}</p>
       </div>
     );
   }
@@ -20,8 +51,22 @@ class App extends Component {
 
 export class Link extends Component {
   render() {
-    return null;
+    const { address, hide } = this.props;
+    if (hide) {
+      return null;
+    }
+    return <a href={address}>{address}</a>;
   }
 }
+
+Link.propTypes = {
+  address: PropTypes.string,
+  hide: PropTypes.bool,
+};
+
+Link.defaultProps = {
+  hide: false,
+  address: '',
+};
 
 export default App;
