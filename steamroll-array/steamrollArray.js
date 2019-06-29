df140f726e7152944765cfc296f6d9a5db43b6e2
Rakesh Tembhurne <tj2point0@gmail.com>
2019-06-29 09:06:18 +0530

    Complete steamroll-array

--- steamroll-array/steamrollArray.js
+++ steamroll-array/steamrollArray.js
@@ -1,6 +1,11 @@
 
-function steamrollArray(...args) {
-  return args;
+function steamrollArray(array) {
+  const containsNoArray = array.every(element => !Array.isArray(element));
+  if (containsNoArray) {
+    return array;
+  }
+
+  return steamrollArray([].concat(...array));
 }
 
 export {
