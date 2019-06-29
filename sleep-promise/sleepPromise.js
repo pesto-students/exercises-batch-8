ca3b36b8a9221dde39b7460dee78ea63f1339e54
prateekinstillion <Prateek Madaan>
2019-06-30 01:00:35 +0530

    Solved pending two questions

--- sleep-promise/sleepPromise.js
+++ sleep-promise/sleepPromise.js
@@ -1,6 +1,11 @@
+function sleep(delay) {
+  const sleepPromise = new Promise((resolve, reject) => setTimeout(resolve, delay));
 
-function sleep(time) {
-  return new Promise(resolve => setTimeout(resolve, time));
+  const promisifyFunction = value => sleepPromise.then(() => value);
+  promisifyFunction.then = (...rest) => sleepPromise.then(...rest);
+  promisifyFunction.catch = error => sleepPromise.catch(error);
+
+  return promisifyFunction;
 }
 
 export {
