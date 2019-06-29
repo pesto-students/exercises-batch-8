36311d675b9b121cb6624a1e788d7edecac22158
Jaspreet Singh <erjaspreet57@gmail.com>
2019-06-29 21:44:38 +0530

    Solved is-palindrome

--- is-palindrome/isPalindrome.js
+++ is-palindrome/isPalindrome.js
@@ -1,6 +1,11 @@
 
-function isPalindrome(...args) {
-  return args;
+function isPalindrome(mayBePalindrome) {
+  const stringForm = String(mayBePalindrome);
+  const charsToBeTested = stringForm.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+  const chars = charsToBeTested.split('');
+  const reversed = chars.reverse().join('');
+  // isPalindrome
+  return charsToBeTested === reversed;
 }
 
 export {
