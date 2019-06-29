af66fdc91708d9ad12cdd099eafcfb0a69a478f8
Jaspreet Singh <erjaspreet57@gmail.com>
2019-06-29 23:50:53 +0530

    Solved sum-all

--- sum-all/sumAll.js
+++ sum-all/sumAll.js
@@ -1,6 +1,13 @@
 
-function sumAll(...args) {
-  return args;
+function sumAll(elements) {
+  const min = Math.min(...elements);
+  const max = Math.max(...elements);
+
+  let sum = 0;
+  for (let ele = min; ele <= max; ele += 1) {
+    sum += ele;
+  }
+  return sum;
 }
 
 export {
