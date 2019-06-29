0d81bd98ebda49f4829fe807fe0f45624340068b
prateekinstillion <Prateek Madaan>
2019-06-30 00:48:08 +0530

    completed day 5 segment 2

--- fix-eslint-errors/exercise-2.test.js
+++ fix-eslint-errors/exercise-2.test.js
@@ -1,8 +1,7 @@
-function noop() {}
+function noop() { }
 
 if (typeof console === 'undefined') {
-  // eslint-disable-next-line no-undef
-  window.console = {
+  this.window.console = {
     warn: noop,
     error: noop,
   };
@@ -14,63 +13,57 @@ console.info = noop;
 let asserted;
 
 function createCompareFn(spy) {
-  // eslint-disable-next-line consistent-return
   const hasWarned = (msg) => {
     let count = spy.calls.count();
-    let args;
-    function containsMsg(arg) {
-      return arg.toString().indexOf(msg) > -1;
-    }
-    while (count) {
-      count -= 1;
-      args = spy.calls.argsFor(count);
+    while (count--) {
+      const args = spy.calls.argsFor(count)
       if (args.some(containsMsg)) {
         return true;
       }
     }
-  };
+
+    function containsMsg(arg) {
+      return arg.toString().indexOf(msg) > -1
+    }
+  }
 
   return {
-    compare: (msg) => {
-      asserted = asserted.concat(msg);
-      const warned = Array.isArray(msg)
+    compare: msg => {
+      asserted = asserted.concat(msg)
+      var warned = Array.isArray(msg)
         ? msg.some(hasWarned)
-        : hasWarned(msg);
+        : hasWarned(msg)
       return {
         pass: warned,
         message: warned
-          ? `Expected message "${msg}" not to have been warned`
-          : `Expected message "${msg}" to have been warned`,
-      };
-    },
-  };
+          ? 'Expected message "' + msg + '" not to have been warned'
+          : 'Expected message "' + msg + '" to have been warned'
+      }
+    }
+  }
 }
 
 // define custom matcher for warnings
 beforeEach(() => {
-  asserted = [];
-  // eslint-disable-next-line no-undef
-  spyOn(console, 'warn');
-  // eslint-disable-next-line no-undef
-  spyOn(console, 'error');
-  // eslint-disable-next-line no-undef
+  asserted = []
+  spyOn(console, 'warn')
+  spyOn(console, 'error')
   jasmine.addMatchers({
     toHaveBeenWarned: () => createCompareFn(console.error),
-    toHaveBeenTipped: () => createCompareFn(console.warn),
-  });
-});
+    toHaveBeenTipped: () => createCompareFn(console.warn)
+  })
+})
 
-afterEach((done) => {
-  const warned = msg => asserted.some(assertedMsg => msg.toString().indexOf(assertedMsg) > -1);
-  let count = console.error.calls.count();
-  let args;
-  while (count) {
-    count -= 1;
-    args = console.error.calls.argsFor(count);
+afterEach(done => {
+  const warned = msg => asserted.some(assertedMsg => msg.toString().indexOf(assertedMsg) > -1)
+  let count = console.error.calls.count()
+  let args
+  while (count--) {
+    args = console.error.calls.argsFor(count)
     if (!warned(args[0])) {
-      done.fail(`Unexpected console.error message: ${args[0]}`);
-      return;
+      done.fail(`Unexpected console.error message: ${args[0]}`)
+      return
     }
   }
-  done();
-});
+  done()
+})
