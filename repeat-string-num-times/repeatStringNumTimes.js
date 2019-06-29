5c76db72f19e090983c7404a9607af63c9668aa3
Jaspreet Singh <erjaspreet57@gmail.com>
2019-06-29 22:43:34 +0530

    Solved repeat-string-num-times

--- repeat-string-num-times/repeatStringNumTimes.js
+++ repeat-string-num-times/repeatStringNumTimes.js
@@ -1,6 +1,12 @@
-
-function repeatStringNumTimes(...args) {
-  return args;
+function repeatStringNumTimes(str, times) {
+  if (times < 0) {
+    return '';
+  }
+  let result = '';
+  for (let i = 0; i < times; i += 1) {
+    result += str;
+  }
+  return result;
 }
 
 export {
