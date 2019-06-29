ae78509426ff788726df606af04245e5e7919801
Rakesh Tembhurne <tj2point0@gmail.com>
2019-06-29 08:04:39 +0530

    Complete rot13

--- rot13/rot13.js
+++ rot13/rot13.js
@@ -1,6 +1,27 @@
+function rot13(string) {
+  const getChar = num => String.fromCharCode(num);
 
-function rot13(...args) {
-  return args;
+  const START = 'A'.charCodeAt();
+  const END = 'Z'.charCodeAt();
+  const SPACER = 13;
+  const characters = string.split('');
+
+  const replacedChars = characters.map((char) => {
+    const charCode = char.charCodeAt();
+
+    if (charCode >= START && charCode <= END) {
+      const newCharCode = charCode - SPACER;
+
+      if (newCharCode < START) {
+        return getChar(charCode + SPACER);
+      }
+      return getChar(newCharCode);
+    }
+
+    return char;
+  });
+
+  return replacedChars.join('');
 }
 
 export {
