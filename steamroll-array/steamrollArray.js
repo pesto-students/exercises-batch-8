70b1db8eb061a3333e9305e981d6621fc34b0d96
Jaspreet Singh <erjaspreet57@gmail.com>
2019-06-29 23:38:07 +0530

    Solved steamroll-array

--- steamroll-array/steamrollArray.js
+++ steamroll-array/steamrollArray.js
@@ -1,6 +1,11 @@
 
-function steamrollArray(...args) {
-  return args;
+function steamrollArray(arr) {
+  return arr.reduce((acc, val) => {
+    if (Array.isArray(val)) {
+      return acc.concat(steamrollArray(val));
+    }
+    return acc.concat(val);
+  }, []);
 }
 
 export {
