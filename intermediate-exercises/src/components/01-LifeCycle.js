158468c8113ac145c3b5480cf4df1d24cf2b0ade
Abhinav Sharma <abhi18av@outlook.com>
2019-07-01 04:47:28 +0530

    wip intermediate-exercises

--- intermediate-exercises/src/components/01-LifeCycle.js
+++ intermediate-exercises/src/components/01-LifeCycle.js
@@ -1,39 +1,49 @@
-import React from 'react';
+import React from "react";
+
+/*
+  Tasks for this exercise are in comments inside the component class code.
+  In this exercise lifecycle methods will be used to provide convenient debug
+  messages in developer's console.
+  There are more lifecycle methods available.
+  Those three presented are commonly used.
+ */
 
 class LifeCycle extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { name: 'Good Morning, Pesto!' };
+    this.state = { name: "Pesto" };
   }
 
+  // This code will be called when the component finishes mounting
+  // (so it is visible for a user).
   componentDidMount() {
     console.log("I'm mounted!");
   }
 
   componentDidUpdate(prevProps, prevState) {
+    // Task 2: Display a message "Updated!" in developer's console
+    //         when the component updates.
+    //         Here you also need to use the console.log function.
+    //         Notice that in this lifecycle method you have an access
+    //         to previous values of properties and state.
+    console.log("Updated!");
     console.log({ prevProps, prevState });
-    console.log('Updated!');
   }
 
   componentWillUnmount() {
-    console.log('Good night, Pesto!');
+    console.log("Good night, Pesto!");
   }
 
+  triggerUpdate() {
+    this.setState({ name: "Change" });
+  }
 
-changeTime() { 
-  return this.setState( {name: "Abhinav Sharma"});
-}
-
-
-
+  // Task 4: Create a button which triggers ComponentDidUpdate lifecycle method on click.
   render() {
-    const { name } = this.state;
     return (
       <div>
-        <p>{name}</p>
-        <button type="button" onClick={this.changeTime()}>
-          Update Component
-        </button>
+        <p>Good morning, {this.state.name}!</p>
+        <button onClick={() => this.triggerUpdate()}>Trigger</button>
       </div>
     );
   }
--- intermediate-exercises/src/components/02-StopWatch.js
+++ intermediate-exercises/src/components/02-StopWatch.js
@@ -1,50 +1,62 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
+/*
+ * Exercise 2:
+ *
+ *  Create a `StopWatch` component that has a Start/Stop button and a Clear
+ *  button.
+ *
+ *  Pressing Start will start a timer and the lapsed time in
+ *  milliseconds should be displayed above the buttons.
+ *
+ *  Once started the Start button should change to Stop. Clicking Stop
+ *  will stop the timer but lapsed time will be preserved.
+ *
+ *  Clicking Start again will resume the timer from where it left off.
+ *
+ *  Clicking Clear will stop the timer if it's running and reset the lapsed time to 0.
+ */
 
 class StopWatch extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      time: 0,
-      isTimerOn: false,
-    }
-    this.startTimer = this.startTimer.bind(this);
-    this.stopTimer = this.stopTimer.bind(this);
-    this.resetTimer = this.resetTimer.bind(this);
+      timer: 0,
+      stopTimer: true,
+      clearTimer: false
+    };
+    let timerVal;
   }
+
+  componentDidMount() {
+    this.setState({ timeElapsed: 0 });
+  }
+
+  incrementTimer() {
+    const currentTime = this.state.timer + 1;
+    this.setState({ timer: currentTime });
+  }
+
   startTimer() {
-    return this.setState({
-      isTimerOn: true,
-    }),
-    this.timer = setInterval(() => {
-        this.setState({
-          time: this.state.time + 1,
-        });
-      }, 1000)
+    this.timerVal = setInterval(this.incrementTimer.bind(this), 1000);
   }
-  
+
   stopTimer() {
-    this.setState({
-      isTimerOn: false,
-    });
-    clearInterval(this.timer);
+    clearInterval(this.timerVal);
   }
-  
+
   resetTimer() {
-    this.setState({
-      time : 0,
-      isTimerOn : false,
-    });
-    clearInterval(this.timer);
+    clearInterval(this.timerVal);
+    this.setState({ timer: 0 });
   }
 
   render() {
-    const { time, isTimerOn } = this.state;
     return (
       <div>
-        <h3> {time} </h3>
-        <button type="button" onClick={isTimerOn ? this.stopTimer : this.startTimer}> {isTimerOn ? 'Stop Time' : 'Start Timer'}</button>
-        <button type="button" onClick={this.resetTimer} > Reset</button>
+        <h1>Timer : {this.state.timer}</h1>
+        <button onClick={() => this.startTimer()}>Start</button>
+        <button onClick={() => this.stopTimer()}>Stop</button>
+        <button onClick={() => this.resetTimer()}>Clear</button>
       </div>
     );
   }
