0358ed4a964558b520202cf3e3da6e48a5a65440
Abhinav Sharma <abhi18av@outlook.com>
2019-07-01 03:37:31 +0530

    completed the express-forms problem

---
+++ express-forms/expressForms.test.js
@@ -0,0 +1,5 @@
+describe('express-forms', () => {
+  test('mark express forms as done', () => {
+    expect(true).toBe(true);
+  });
+});
--- express-forms/src/index.js
+++ express-forms/src/index.js
@@ -1,18 +1,34 @@
 const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.redirect('/form-with-get');
-});
+app.use(bodyParser.urlencoded({ extended: true }));
+const publicPath = path.join(__dirname, './views');
+app.set('view engine', 'ejs');
+app.use(express.static(publicPath));
 
-app.get('/form-with-get', (req, res) => {
-  return res.render('form-with-get');
-});
+
+app.get('/', (req, res) => res.redirect('/form-with-get'));
+
+app.get('/form-with-get', (req, res) => res.render('form-with-get'));
 
 app.get('/form-with-post', (req, res) => {
-  return res.render('form-with-post');
+  res.render('form-with-post');
 });
+
+app.get('/submit-form-with-get', (req, res) => {
+  const { first, last } = req.query;
+
+  res.send({ first, last });
+});
+
+app.post('/submit-form-with-post', (req, res) => {
+  console.log(req.body);
+  res.send(JSON.stringify(req.body));
+});
+
 
 app.listen(3000, () => {
   console.log('Server is listening on port 3000');
