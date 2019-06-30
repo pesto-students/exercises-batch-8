5a3d6b838cf231387c70d5a40959ed35bd22020a
prateekinstillion <Prateek Madaan>
2019-06-30 11:46:48 +0530

    Solved some pending problems

--- boolean-expression-evaluator/booleanExpressionEvaluator.js
+++ boolean-expression-evaluator/booleanExpressionEvaluator.js
@@ -1,6 +1,14 @@
 
 function booleanExpressionEvaluator(...args) {
-  return args;
+  if (typeof args !== 'string') {
+    throw new Error(`Expected string, received ${typeof args}`);
+  }
+  // now we come here this means args is  a string 
+  const booleanSymbols = ['&', '|', '^'];
+  if (booleanSymbols.some(symbol => !args.includes(symbol))) {
+    throw new Error('Must have atleast one of the boolean symbols');
+  }
+  return Boolean(args);
 }
 
 export {
