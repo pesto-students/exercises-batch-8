7814cddec673b81d06807c3589ba2817c34f727e
Jaspreet Singh <erjaspreet57@gmail.com>
2019-06-30 22:51:50 +0530

    Solve object-pairs

--- object-pairs/objectPairs.js
+++ object-pairs/objectPairs.js
@@ -1,6 +1,12 @@
 
-function objectPairs(...args) {
-  return args;
+function objectPairs(object) {
+  const result = [];
+  for (const key in object) {
+    if (Object.prototype.hasOwnProperty.call(object, key)) {
+      result.push([key, object[key]]);
+    }
+  }
+  return result;
 }
 
 export {
