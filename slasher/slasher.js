a548dd0ef883efa4a73221b342d403c02e147959
Jaspreet Singh <erjaspreet57@gmail.com>
2019-06-30 23:19:00 +0530

    Solve slasher

--- slasher/slasher.js
+++ slasher/slasher.js
@@ -1,6 +1,9 @@
-
-function slasher(...args) {
-  return args;
+function slasher(array, elementsToChop) {
+  const copy = [...array];
+  for (let i = 0; i < elementsToChop; i += 1) {
+    copy.shift();
+  }
+  return copy;
 }
 
 export {
