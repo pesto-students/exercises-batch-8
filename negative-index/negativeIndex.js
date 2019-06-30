bee73db25aac8a0cc09663ac42146c240e0aef2d
prateekinstillion <Prateek Madaan>
2019-06-30 08:54:04 +0530

    Solved more problems

--- negative-index/negativeIndex.js
+++ negative-index/negativeIndex.js
@@ -1,6 +1,31 @@
 
-function negativeIndex(...args) {
-  return args;
+function negativeIndex(array) {
+  if (!Array.isArray(array)) {
+    const error = new TypeError(`Expected array, received ${typeof array}(${array})`);
+    throw error;
+  }
+  return new Proxy(array, {
+    get(target, property) {
+      if (typeof property === 'symbol') {
+        return Reflect.get(target, property);
+      }
+      let arrayIndex = property;
+      if (arrayIndex < 0) {
+        arrayIndex = array.length + parseInt(arrayIndex, 10);
+      }
+      return Reflect.get(target, arrayIndex);
+    },
+    set(target, property, value) {
+      let arrayIndex = property;
+      if (arrayIndex < 0) {
+        arrayIndex = array.length + parseInt(arrayIndex, 10);
+      }
+      if (+property > array.length) {
+        array.length = +property + 1;
+      }
+      return Reflect.set(target, arrayIndex, value);
+    }
+  });
 }
 
 export {
--- on-change/onChange.js
+++ on-change/onChange.js
@@ -1,7 +1,32 @@
-function onChange(...args) {
-  return args;
+
+function onChange(originalObject, func) {
+  const handler = {
+    get(target, property) {
+      try {
+        if (typeof target[property] === 'function' && property === 'sort') {
+          func();
+        }
+        return new Proxy(originalObject[property], handler);
+      } catch (error) {
+        return Reflect.get(target, property);
+      }
+    },
+    set(target, property, value) {
+      func();
+      return Reflect.set(target, property, value);
+    },
+    defineProperty(target, property, attributes) {
+      func();
+      return Reflect.defineProperty(target, property, attributes);
+    },
+    deleteProperty(target, property) {
+      func();
+      return Reflect.deleteProperty(target, property);
+    },
+  };
+  return new Proxy(originalObject, handler);
 }
 
 export {
   onChange,
-};
+}
