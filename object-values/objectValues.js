d20c55404e88d52122121de09d2232932ace5691
Jaspreet Singh <erjaspreet57@gmail.com>
2019-06-30 22:52:54 +0530

    Solve object-values

--- object-values/objectValues.js
+++ object-values/objectValues.js
@@ -1,6 +1,12 @@
 
-function objectValues(...args) {
-  return args;
+function objectValues(object) {
+  const result = [];
+  for (const key in object) {
+    if (Object.prototype.hasOwnProperty.call(object, key)) {
+      result.push(object[key]);
+    }
+  }
+  return result;
 }
 
 export {
