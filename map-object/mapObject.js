ceca17166ca28df9504296ca2b4e5b587a835313
Rakesh Tembhurne <tj2point0@gmail.com>
2019-06-30 10:16:13 +0530

    Complete map-object

--- map-object/mapObject.js
+++ map-object/mapObject.js
@@ -1,6 +1,12 @@
 
-function mapObject(...args) {
-  return args;
+function mapObject(object, fnModifier) {
+  const alterObject = { ...object };
+
+  for (const key of Object.keys(object)) {
+    alterObject[key] = fnModifier(alterObject[key]);
+  }
+
+  return alterObject;
 }
 
 export {
