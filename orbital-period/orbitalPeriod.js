b91880089ac759dac6cb745d82a1ee708b41db34
Jaspreet Singh <erjaspreet57@gmail.com>
2019-06-30 23:15:20 +0530

    Solve orbital-period

--- orbital-period/orbitalPeriod.js
+++ orbital-period/orbitalPeriod.js
@@ -1,6 +1,25 @@
+const GM = 398600.4418; // in km3s-2
+const earthRadius = 6367.4447; // in km
 
-function orbitalPeriod(...args) {
-  return args;
+function square(value) {
+  return value ** 2;
+}
+
+function cube(value) {
+  return value ** 3;
+}
+
+function toOrbitalPeriod(avgAlt) {
+  const radius = avgAlt + earthRadius;
+  const value = Math.sqrt((4 * square(Math.PI) * cube(radius)) / GM);
+  return Math.round(value);
+}
+
+function orbitalPeriod(objects) {
+  return objects.map(({ name, avgAlt }) => ({
+    name,
+    orbitalPeriod: toOrbitalPeriod(avgAlt),
+  }));
 }
 
 export {
