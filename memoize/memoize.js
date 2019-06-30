014554f890a990e5cfd73bef4f5ba5418ca02464
Jaspreet Singh <erjaspreet57@gmail.com>
2019-06-30 23:30:22 +0530

    Solve memoize

--- memoize/memoize.js
+++ memoize/memoize.js
@@ -1,7 +1,17 @@
 
-function memoize(...args) {
-  return args;
-}
+const memoize = (cb) => {
+  const cache = new Map();
+  return (...args) => {
+    const serializedArgs = JSON.stringify(args);
+    if (cache.has(serializedArgs)) {
+      return cache.get(serializedArgs);
+    }
+
+    const result = cb(...args);
+    cache.set(serializedArgs, result);
+    return result;
+  };
+};
 
 export {
   memoize,
