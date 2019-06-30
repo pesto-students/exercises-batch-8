84e42e4cdbe226c2ae9f80ca7e52111e54aa2e30
Abhinav Sharma <abhi18av@outlook.com>
2019-07-01 03:46:50 +0530

    filtered name recursive solved

--- filtered-names-recursive/filteredNamesRecursive.js
+++ filtered-names-recursive/filteredNamesRecursive.js
@@ -1,6 +1,25 @@
 
-function filteredNamesRecursive(...args) {
-  return args;
+const fs = require('fs');
+const path = require('path');
+
+const hasExtn = extn => fileName => fileName.endsWith(`.${extn}`);
+
+function filteredNamesRecursive(directoryPath, fileExtn) {
+  const extnValidator = hasExtn(fileExtn);
+
+  const fsDirectObjects = fs.readdirSync(directoryPath, { withFileTypes: true });
+  const directories = fsDirectObjects.filter(obj => obj.isDirectory()).map(obj => obj.name);
+  const files = fsDirectObjects.filter(obj => obj.isFile()).map(obj => obj.name);
+
+  let fileNames = directories.reduce((acc, dirName) => {
+    const subDirPath = path.join(directoryPath, dirName);
+    const subDirFiles = filteredNamesRecursive(subDirPath, fileExtn);
+    return [...acc, ...subDirFiles];
+  }, []);
+
+  fileNames = [...files, ...fileNames];
+
+  return fileNames.filter(extnValidator);
 }
 
 export {
