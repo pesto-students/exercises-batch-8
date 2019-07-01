6e0e2955f8514099923aba24a744c4399c179b2c
sourabhmodi20051 <sourabhmodi20051@gmail.com>
2019-07-02 01:00:47 +0530

    Implemented using local cache

--- simple-post/frontend/src/App.js
+++ simple-post/frontend/src/App.js
@@ -1,38 +1,57 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component, Fragment, useEffect, useState } from 'react';
 
 import Post from './components/Post';
 import './App.css';
 
-class App extends Component {
-  state = {
-    posts: [],
-  };
+function App() {
+  const [posts, savePosts] = useState({ 1: [] });
+  const [currentPage, saveCurrentPage] = useState(1);
 
-  render() {
-    return (
-      <div>
-        <h2>Posts</h2>
-        <hr />
-        <br />
-        <button onClick={this.handlePrevClick}>
-          Previous
-        </button>
-        <button onClick={this.handleNextClick}>
-          Next
-        </button>
-        <div>
-          {this.state.posts.map((post) => {
-            return (
-              <Fragment key={post.id}>
-                <Post post={post} />
-                <hr />
-              </Fragment>
-            );
-          })}
-        </div>
-      </div>
-    );
+  useEffect(() => {
+    console.log(currentPage);
+    console.log(posts[currentPage]);
+    if (posts[currentPage].length === 0) {
+      fetch(`http://localhost:3001/posts/${currentPage}`, {
+        headers: new Headers({ 'pesto-password': 'darth vader' }),
+      })
+        .then(response => response.json())
+        .then(data => savePosts({ ...posts, [currentPage]: data.data }));
+    }
+  }, [currentPage]);
+
+  function previousPage() {
+    const previousPageNumber = currentPage - 1;
+    if (posts[previousPageNumber] === undefined) {
+      posts[previousPageNumber] = [];
+    }
+    saveCurrentPage(previousPageNumber);
   }
+
+  function nextPage() {
+    const nextPageNumber = currentPage + 1;
+    if (posts[nextPageNumber] === undefined) {
+      posts[nextPageNumber] = [];
+    }
+    saveCurrentPage(nextPageNumber);
+  }
+
+  return (
+    <div>
+      <h2>Posts</h2>
+      <hr />
+      <br />
+      {currentPage <= 1 ? null : <button onClick={previousPage}>Previous</button>}
+      <button onClick={nextPage}>Next</button>
+      <div>
+        {posts[currentPage].map(post => (
+          <Fragment key={post.id}>
+            <Post post={post} />
+            <hr />
+          </Fragment>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
