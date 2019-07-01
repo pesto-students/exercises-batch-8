3d0920a28b02678f1c163fb027768b03dbe19d06
kunnu01 <kunalbhatia2409@gmail.com>
2019-07-02 04:05:30 +0530

    WIP: Simple Post

--- simple-post/frontend/src/App.js
+++ simple-post/frontend/src/App.js
@@ -4,9 +4,59 @@ import Post from './components/Post';
 import './App.css';
 
 class App extends Component {
-  state = {
-    posts: [],
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+    };
+
+    if (!this.getPageNo()) {
+      this.setPageNo(1);
+    }
+  }
+
+  
+  componentDidMount() {
+    this.getPosts();
+  }
+  
+  getPageNo = () => {
+    return Number.parseInt(localStorage.getItem('page'), 10);
+  }
+
+  getURL = () => {
+    const apiUrl = `http://localhost:3001/posts/${this.getPageNo()}`;
+    return apiUrl;
+  }
+  
+  setPageNo = (page) => {
+    localStorage.setItem('page', page);
+  }
+
+  handleNextClick = () => {
+      this.setPageNo(this.getPageNo() + 1);
+      this.getPosts();
+  }
+
+  handlePrevClick = () => {
+      this.setPageNo(this.getPageNo() - 1);
+      this.getPosts();
+  }
+
+  getPosts = () => {
+    let headers = {
+      method: 'GET',
+      headers: new Headers({'pesto-password': 'darth vader'}),
+    };
+    fetch(this.getURL(), headers)
+      .then(res => {
+        return res.json();
+      })
+      .then(res => this.setState({ posts: res.data }))
+      .catch(err => {
+        console.log(err);
+      })
+  }
 
   render() {
     return (
@@ -14,10 +64,10 @@ class App extends Component {
         <h2>Posts</h2>
         <hr />
         <br />
-        <button onClick={this.handlePrevClick}>
+        <button onClick={this.handlePrevClick} disabled={this.getPageNo()===1}>
           Previous
         </button>
-        <button onClick={this.handleNextClick}>
+        <button onClick={this.handleNextClick} disabled={this.getPageNo()===10}>
           Next
         </button>
         <div>
