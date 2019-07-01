67a6dbccb8e1badd76219d580a3faeac3acf8ae1
prateekinstillion <Prateek Madaan>
2019-07-02 00:52:35 +0530

    Solved the lastIndexOf

--- last-index-of/lastIndexOf.js
+++ last-index-of/lastIndexOf.js
@@ -1,8 +1,20 @@
+const createErrorMessage = (message, isTypeError = false) => {
+  if (isTypeError) {
+    const error = new TypeError(message);
+    throw error;
+  }
+  throw new Error(message);
+};
 
-function lastIndexOf(...args) {
-  return args;
+function lastIndexOf(elementWhoseLastIndexIsRequired, array) {
+  if (!Array.isArray(array)) {
+    return createErrorMessage(`Expected array, got ${typeof numbers}`, true);
+  }
+  if (!array.includes(elementWhoseLastIndexIsRequired)) {
+    return -1;
+  }
+  return array.lastIndexOf(elementWhoseLastIndexIsRequired);
 }
 
-export {
-  lastIndexOf,
-};
+// console.log(lastIndexOf(20, [20, "a", "b", null, 20, 19]));
+export { lastIndexOf };
