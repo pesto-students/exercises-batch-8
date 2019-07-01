d31c9797a8d27f7b6431018153b1827a1b9d3a8b
Rakesh Tembhurne <tj2point0@gmail.com>
2019-07-01 10:35:18 +0530

    Complete add-big-integers

--- add-big-integers/addBigIntegers.js
+++ add-big-integers/addBigIntegers.js
@@ -1,6 +1,48 @@
 
-function addBigIntegers(...args) {
-  return args;
+const fnSumOfLongIntegerStrings = (intFirst, intSecond) => {
+  const [
+    firstIntDigits,
+    secondIntDigits,
+  ] = [intFirst.split('').reverse(), intSecond.split('').reverse()];
+
+  const DECIMAL = 10;
+  const sum = [];
+  let carryForward = 0;
+
+  for (const [index, digitChar] of firstIntDigits.entries()) {
+    const secondDigit = (typeof secondIntDigits[index] !== 'undefined')
+      ? secondIntDigits[index]
+      : 0;
+    const sumOfDigits = Number(digitChar) + Number(secondDigit) + Number(carryForward);
+    const lastDigit = sumOfDigits % 10;
+
+    sum.unshift(`${lastDigit}`);
+    carryForward = parseInt(sumOfDigits / 10, DECIMAL);
+  }
+  if (carryForward) {
+    sum.unshift(`${carryForward}`);
+  }
+
+  return sum.join('');
+};
+
+function addBigIntegers(numberString) {
+  const numbers = numberString.split(/\r|\r\n|\n/);
+  const [totalNumbersToAdd, ...restNumbers] = numbers;
+  const numbersToAdd = restNumbers.slice(0, totalNumbersToAdd);
+
+  const lengthWiseNumbersToAdd = numbersToAdd.sort((a, b) => b.length > a.length);
+  const sumOfIntegers = lengthWiseNumbersToAdd.reduce((sumAccumulator, stringOfDigits) => {
+    const paramsToPass = [];
+    if ((Number(sumAccumulator) > Number(stringOfDigits))) {
+      paramsToPass.push(sumAccumulator, stringOfDigits);
+    } else {
+      paramsToPass.push(stringOfDigits, sumAccumulator);
+    }
+    return fnSumOfLongIntegerStrings(...paramsToPass);
+  }, '');
+
+  return sumOfIntegers;
 }
 
 export {
