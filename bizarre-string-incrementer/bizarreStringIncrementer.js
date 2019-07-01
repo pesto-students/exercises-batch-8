494ce3b5823cf77f03de460d87c534bc530a1371
Abhinav Sharma <abhi18av@outlook.com>
2019-07-01 07:47:12 +0530

    completed form validation exercise

--- bizarre-string-incrementer/bizarreStringIncrementer.js
+++ bizarre-string-incrementer/bizarreStringIncrementer.js
@@ -1,6 +1,14 @@
 
-function bizarreStringIncrementer(...args) {
-  return args;
+function bizarreStringIncrementer(string) {
+  const endingNumberPattern = /\d+$/;
+  const number = string.match(endingNumberPattern)[0];
+  let numberPlusOne = parseInt(number, 10) + 1;
+  numberPlusOne = String(numberPlusOne);
+  if (numberPlusOne.length < number.length) {
+    numberPlusOne = number.substring(0, number.length - numberPlusOne.length) + numberPlusOne;
+  }
+  numberPlusOne = string.substring(0, numberPlusOne.length + 1) + numberPlusOne;
+  return numberPlusOne;
 }
 
 export {
