264ac06a3d65273fedee33c60612d1d3359c536b
sourabhmodi20051 <sourabhmodi20051@gmail.com>
2019-07-02 05:20:56 +0530

    Session Storage

--- session-storage/check.js
+++ session-storage/check.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 let value = '';
-
+let current;
 window.setInterval(() => {
   if (value === '' || value === null) {
     value = sessionStorage.getItem('world');
@@ -9,3 +9,19 @@ window.setInterval(() => {
   }
 }, 200);
 
+function onKeyValue() {
+  const key = document.getElementById('key').value;
+  const value = document.getElementById('value').value;
+  sessionStorage.setItem(key, value);
+  const existingElement = document.getElementById(key);
+  console.log(existingElement);
+  if (existingElement === null) {
+    const node = document.createElement('LI');
+    node.setAttribute('id', key);
+    const textnode = document.createTextNode(key + ' ' + value);
+    node.appendChild(textnode);
+    document.getElementById('sessionStorageList').appendChild(node);
+  } else {
+    document.getElementById(key).innerText = key + ' ' + value;
+  }
+}
