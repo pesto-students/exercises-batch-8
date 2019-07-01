2a0d063019c788263afce854334df943d44411e9
Rakesh Tembhurne <tj2point0@gmail.com>
2019-07-02 00:38:27 +0530

    WIP update-object

--- update-object/updateObject.js
+++ update-object/updateObject.js
@@ -1,7 +1,17 @@
 
-function updateObject(...args) {
-  return args;
+function updateObject(replacerIndex, newValue, array) {
+  const newArray = array.slice();
+  let index = replacerIndex;
+  if (replacerIndex < 0) {
+    index = newArray.length + replacerIndex;
+  }
+  if (index > (newArray.length - 1) || index < 0) {
+    return array;
+  }
+  newArray[index] = newValue;
+  return newArray;
 }
+
 
 export {
   updateObject,
