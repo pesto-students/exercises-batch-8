b797f532929c9282876ba0980976a88edb312ed3
Radheshyam Etikala <radhey1851@gmail.com>
2019-07-02 05:20:56 +0530

    completed sessionStorage

--- session-storage/check.js
+++ session-storage/check.js
@@ -1,11 +1,41 @@
 /* eslint-disable */
-let value = '';
+let value = "";
+
+const btn = document.getElementsByClassName("btn")[0];
+
+const updateLiveData = () => {
+  const liveData = document.getElementById("live-data");
+  liveData.innerHTML = '';
+  const entries = Object.entries(sessionStorage);
+  entries.forEach(entry => {
+    const newLi = document.createElement("li");
+    newLi.innerHTML = `${entry[0]}: ${entry[1]} 
+    <button onclick="deleteEntry('${entry[0]}')">x</button>`
+    liveData.appendChild(newLi);
+  });
+};
+
+const deleteEntry = (key) => {
+  sessionStorage.removeItem(key);
+  updateLiveData();
+}
+
+btn.onclick = e => {
+  e.preventDefault();
+  const key = document.getElementById("key").value;
+  value = document.getElementById("value").value;
+  sessionStorage.setItem(key, value);
+
+  updateLiveData();
+};
 
 window.setInterval(() => {
-  if (value === '' || value === null) {
-    value = sessionStorage.getItem('world');
+  if (value === "" || value === null) {
+    value = sessionStorage.getItem("world");
   } else {
-    document.getElementsByClassName('status')[0].innerText = 'Value add successful';
+    document.getElementsByClassName("status")[0].innerText =
+      "Value add successful";
   }
 }, 200);
 
+updateLiveData();
