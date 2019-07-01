cc33e48acdbfe766bcbd689512e5beccf3412516
Radheshyam Etikala <radhey1851@gmail.com>
2019-07-02 01:41:23 +0530

    WIP simple-post

--- simple-post/frontend/src/App.js
+++ simple-post/frontend/src/App.js
@@ -4,9 +4,63 @@ import Post from './components/Post';
 import './App.css';
 
 class App extends Component {
-  state = {
+  state: {
     posts: [],
+    currPageNo: JSON.parse(sessionStorage.getItem('currPageNo')) || 1,
   };
+
+  headers = {
+    'Content-Type': 'application/json',
+    'pesto-password': 'darth vader',
+  };
+
+  componentDidMount() {
+    console.log("ComponentDidMount");
+    const { currPageNo: pageNum } = this.state;
+    const url = `http://localhost:3001/posts/${pageNum}`;
+    const { headers } = this;
+    fetch(url, { headers })
+      .then(res => res.json())
+      .then(response => {
+        this.setState({ posts: [...response.data] });
+      })
+  }
+
+  updatePosts = (pageNum) => {
+    const url = `http://localhost:3001/posts/${pageNum}`;
+    const { headers } = this;
+
+    fetch(url, { headers })
+    .then(res => res.json())
+    .then(response => {
+      this.setState({ posts: [...response.data] });
+    });
+  }
+
+  handleNextClick = () => {
+    const { currPageNo } = this.state;
+    const nextPageNumber = currPageNo + 1;
+    sessionStorage.setItem('currPageNo', nextPageNumber);
+    this.setState({
+      ...this.state,
+      currPageNo: nextPageNumber,
+    });
+    this.updatePosts(nextPageNumber);
+  }
+
+  handlePrevClick = () => {
+    const { currPageNo } = this.state;
+    if (currPageNo !== 1) {
+      const prevPageNumber = currPageNo - 1;
+      sessionStorage.setItem('currPageNo', prevPageNumber);
+      this.setState({
+        ...this.state,
+        currPageNo: prevPageNumber,
+      })
+      this.updatePosts(prevPageNumber);
+    }
+    
+  }
 
   render() {
     return (
