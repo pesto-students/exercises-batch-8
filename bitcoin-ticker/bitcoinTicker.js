d847123543503df250e17aa46cbef46170048214
kunnu01 <kunalbhatia2409@gmail.com>
2019-07-03 04:53:09 +0530

    Solve bitcoin-ticker

--- bitcoin-ticker/bitcoinTicker.js
+++ bitcoin-ticker/bitcoinTicker.js
@@ -1,6 +1,32 @@
+const http = require('https');
 
-function bitcoinTicker(...args) {
-  return args;
+const options = {
+  hostname: 'api.coinmarketcap.com',
+  path: '/v2/ticker/',
+  method: 'GET',
+};
+
+function bitcoinTicker({ limit }) {
+  return new Promise((resolve) => {
+    http.get(options, (res) => {
+      res.setEncoding('utf8');
+      let rawData = '';
+      res.on('data', (chunk) => {
+        rawData += chunk;
+      });
+      res.on('end', () => {
+        const jsonData = JSON.parse(rawData);
+        const coins = jsonData.data;
+        const topCoins = [];
+        for (const coin in coins) {
+          if (coins[coin].rank <= limit) {
+            topCoins.push(coins[coin].name);
+          }
+        }
+        resolve(jsonData);
+      });
+    });
+  });
 }
 
 export {
