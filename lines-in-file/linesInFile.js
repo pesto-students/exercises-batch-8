4b19bbd8515ffee70472fd8875e1786d8d3b93d0
Rakesh Tembhurne <tj2point0@gmail.com>
2019-07-03 02:36:36 +0530

    Complete lines-in-file

--- lines-in-file/linesInFile.js
+++ lines-in-file/linesInFile.js
@@ -1,6 +1,10 @@
+const fs = require('fs');
 
-function linesInFile(...args) {
-  return args;
+function linesInFile(filePath) {
+  const fileContents = fs.readFileSync(filePath, 'utf-8');
+  const count = (fileContents.match(/\n/g) || []).length;
+
+  return count - 1;
 }
 
 export {
