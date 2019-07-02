f237eb146e5b6286055cdbd7af06e7b49f6019d8
Rakesh Tembhurne <tj2point0@gmail.com>
2019-07-02 10:24:39 +0530

    Complete time-was

--- time-was/timeWas.js
+++ time-was/timeWas.js
@@ -1,6 +1,51 @@
 
-function timeWas(...args) {
-  return args;
+function timeWas(timeToCompare, newNow) {
+  const NOW = 1516816290;
+  const SECONDS = 1000;
+  const MINUTES = SECONDS * 60;
+  const HOURS = MINUTES * 60;
+  const DAYS = HOURS * 24;
+  const WEEKS = DAYS * 7;
+  const MONTHS = DAYS * 30;
+  const YEARS = MONTHS * 12;
+
+  const timeDifference = newNow ? (newNow - timeToCompare) : (NOW - timeToCompare);
+
+  if (timeDifference < MINUTES) {
+    const seconds = timeDifference / SECONDS;
+    if (seconds <= 1) {
+      return 'just now';
+    }
+    return `${seconds} seconds ago`;
+  }
+
+  if (timeDifference < HOURS) {
+    const minutes = timeDifference / MINUTES;
+    return `${minutes} minute${minutes !== 1 ? 's' : ''} ago`;
+  }
+
+  if (timeDifference < DAYS) {
+    const hours = timeDifference / HOURS;
+    return `${hours} hours ago`;
+  }
+
+  if (timeDifference < WEEKS) {
+    const days = timeDifference / DAYS;
+    return `${days} days ago`;
+  }
+
+  if (timeDifference < MONTHS) {
+    const weeks = timeDifference / WEEKS;
+    return `${weeks} weeks ago`;
+  }
+
+  if (timeDifference < YEARS) {
+    const months = timeDifference / MONTHS;
+    return `${months} months ago`;
+  }
+
+  const years = timeDifference / YEARS;
+  return `${years} years ago`;
 }
 
 export {
