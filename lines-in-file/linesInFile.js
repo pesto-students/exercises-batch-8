3d4e72cd34bebfeadc990d6c252e8878db106c89
saurabh shetty <saurabh.shetty100@gmail.com>
2019-07-03 02:27:19 +0530

    partially solved lines-in-file

--- lines-in-file/linesInFile.js
+++ lines-in-file/linesInFile.js
@@ -1,6 +1,12 @@
 
-function linesInFile(...args) {
-  return args;
+const fs = require('fs');
+
+function linesInFile(filename) {
+  let fileContent = fs.readFileSync(filename, 'utf-8');
+  if (fileContent[fileContent.length - 1] === '\n') {
+    fileContent = fileContent.slice(0, fileContent.length - 1);
+  }
+  return fileContent.split('\n').length;
 }
 
 export {
