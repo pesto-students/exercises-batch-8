4cf5b5d11f18e22c562263ff1ca62c7c84506c04
Abdul Khuddus <khuddus1@gmail.com>
2019-07-03 05:07:31 +0530

    Partially complete bitcoin ticker

--- bitcoin-ticker/bitcoinTicker.js
+++ bitcoin-ticker/bitcoinTicker.js
@@ -1,8 +1,19 @@
+const https = require('https');
 
-function bitcoinTicker(...args) {
-  return args;
+function bitcoinTicker() {
+  const bitcoinTickerPromise = new Promise((resolve) => {
+    https.get('https://api.coinmarketcap.com/v2/ticker/', (res) => {
+      let body = '';
+      res.on('data', (chunk) => {
+        body += chunk;
+      });
+      res.on('end', () => {
+        resolve(body);
+      });
+    });
+  });
+  return bitcoinTickerPromise;
 }
-
 export {
   bitcoinTicker,
 };
