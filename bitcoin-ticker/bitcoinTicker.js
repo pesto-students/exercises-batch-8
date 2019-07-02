9ac576bc3883a6fdde268d30a48bd4d6b6ef43c5
Abhinav Sharma <abhi18av@outlook.com>
2019-07-03 05:09:55 +0530

    completed the bitcoin-ticker problem

--- bitcoin-ticker/bitcoinTicker.js
+++ bitcoin-ticker/bitcoinTicker.js
@@ -1,8 +1,30 @@
+/* eslint-disable dot-notation */
+const https = require('https');
 
-function bitcoinTicker(...args) {
-  return args;
+function bitcoinTicker(obj) {
+  let queryParams = '?';
+  for (const key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      queryParams += `${key}=${obj[key]}`;
+    }
+  }
+  return new Promise((resolve, reject) => {
+    https
+      .get(`https://api.coinmarketcap.com/v2/ticker/${queryParams}`, (res) => {
+        res.setEncoding('utf8');
+        let body = '';
+        res.on('data', (chunk) => {
+          body += chunk;
+        });
+        res.on('end', () => {
+          console.log(body);
+          resolve(JSON.parse(body));
+        });
+      })
+      .on('error', (e) => {
+        reject(e.name);
+      });
+  });
 }
 
-export {
-  bitcoinTicker,
-};
+export { bitcoinTicker };
