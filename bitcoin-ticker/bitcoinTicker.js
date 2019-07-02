6c628faecc5085ec4012a8c8d5817392aa70a573
saurabh shetty <saurabh.shetty100@gmail.com>
2019-07-03 05:07:19 +0530

    wip bitcoin ticker and csvtojson

--- bitcoin-ticker/bitcoinTicker.js
+++ bitcoin-ticker/bitcoinTicker.js
@@ -1,6 +1,17 @@
+import { promisify } from 'util';
 
-function bitcoinTicker(...args) {
-  return args;
+const https = require('https');
+
+const url = 'https://api.coinmarketcap.com/v2/ticker/';
+
+async function bitcoinTicker({ limit }) {
+  const get = promisify(https.get);
+  await get(`${url}?limit=${limit}`, 'utf-8', (resp) => {
+    console.log(Object.keys(resp));
+    resp.on('data', (chunk) => {
+      console.log(chunk)
+    });
+  });
 }
 
 export {
--- csv-to-json/csvToJson.js
+++ csv-to-json/csvToJson.js
@@ -1,6 +1,19 @@
+const fs = require('fs');
 
-function csvToJson(...args) {
-  return args;
+function csvToJson(csvFilePath) {
+  const fileContent = fs.readFileSync(csvFilePath, 'utf-8');
+  const newlineSeparatedFileContent = fileContent.split('\n');
+  // console.log(newlineSeparatedFileContent);
+  const keys = newlineSeparatedFileContent[0];
+  const dataArray = newlineSeparatedFileContent.slice(1);
+  console.log(dataArray);
+  dataArray.reduce((acc, hero, index) => {
+    // const heroObject = {}
+    // keys.forEach(key => {]})
+    // heroObject[keys[index]] = 
+    return acc;
+  }, [])
+  return csvFilePath;
 }
 
 export {
