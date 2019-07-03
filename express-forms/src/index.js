d2d1ca979250e522410a5423e18dda5d5dd45368
saurabh shetty <saurabh.shetty100@gmail.com>
2019-07-04 01:58:58 +0530

    solved express-forms

--- express-forms/src/index.js
+++ express-forms/src/index.js
@@ -1,18 +1,26 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.redirect('/form-with-get');
-});
 
-app.get('/form-with-get', (req, res) => {
-  return res.render('form-with-get');
-});
+app.use(bodyParser.json());
 
-app.get('/form-with-post', (req, res) => {
-  return res.render('form-with-post');
-});
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) => res.redirect('/form-with-get'));
+
+app.get('/form-with-get', (req, res) => res.render('form-with-get'));
+
+app.get('/form-with-post', (req, res) => res.render('form-with-post'));
+
+app.get('/submit-form-with-get', (req, res) => res.send(req.query));
+
+app.post('/submit-form-with-post', (req, res) => res.send(req.body));
+
 
 app.listen(3000, () => {
   console.log('Server is listening on port 3000');
