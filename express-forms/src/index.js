8d34124968c015a2dd2782ddf9712f1fc291ce77
sourabhmodi20051 <sourabhmodi20051@gmail.com>
2019-07-04 01:35:33 +0530

    Express forms apis added

--- express-forms/src/index.js
+++ express-forms/src/index.js
@@ -1,17 +1,40 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-  return res.redirect('/form-with-get');
+app.get('/', (req, res) => res.redirect('/form-with-get'));
+
+app.get('/form-with-get', (req, res) => res.render('form-with-get'));
+
+app.get('/form-with-post', (req, res) => res.render('form-with-post'));
+
+app.get('/submit-form-with-get', (req, res) => {
+  const firstname = req.query.first;
+  const lastname = req.query.last;
+  if (firstname === undefined) {
+    res.status(400);
+    res.send('first is required');
+  } else if (lastname === undefined) {
+    res.status(400);
+    res.send('last is required');
+  }
+  res.send({ firstName: firstname, lastName: lastname });
 });
 
-app.get('/form-with-get', (req, res) => {
-  return res.render('form-with-get');
-});
-
-app.get('/form-with-post', (req, res) => {
-  return res.render('form-with-post');
+app.post('/submit-form-with-post', (req, res) => {
+  const firstname = req.body.first;
+  const lastname = req.body.last;
+  if (firstname === undefined) {
+    res.status(400);
+    res.send('first is required');
+  } else if (lastname === undefined) {
+    res.status(400);
+    res.send('last is required');
+  }
+  res.send({ firstName: firstname, lastName: lastname });
 });
 
 app.listen(3000, () => {
