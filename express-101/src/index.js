8984e6d355db0885b5d343b8798f012a71b09dc9
Jaspreet Singh <erjaspreet57@gmail.com>
2019-07-04 00:47:55 +0530

    Solve express-101

--- express-101/src/index.js
+++ express-101/src/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 
+const port = 3000;
 const app = express();
 
-// 1) Create Routes
+app.get('/', (req, res) => res.send('Hey, I am server response'));
+app.get('/movie/:title', (req, res) => {
+  const { title } = req.params;
+  res.send(`My favorite movie is ${title}`);
+});
 
-// 2) Start server on port 3000
+// eslint-disable-next-line no-console
+app.listen(port, () => console.log(`Server listening on port: ${port}`));
 
 module.exports = app;
