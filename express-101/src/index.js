6da0f4209ac6e39accbdac1c193a8163976cdf08
saurabh shetty <saurabh.shetty100@gmail.com>
2019-07-04 00:41:20 +0530

    added test for for express101

--- express-101/src/index.js
+++ express-101/src/index.js
@@ -7,7 +7,8 @@ app.get('/', (req, res) => {
   res.send('Hey, I am server response');
 });
 app.get('/movie/:movieTitle', (req, res) => {
-  res.send(`My favorite movie is ${req.params.movieTitle}`);
+  const { movieTitle } = req.params;
+  res.send(`My favorite movie is ${movieTitle}`);
 });
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
