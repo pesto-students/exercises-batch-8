5fa2b69d653a848e9c84da8d1b821f89cf6e50df
saurabh shetty <saurabh.shetty100@gmail.com>
2019-07-03 07:44:51 +0530

    solved csvToJson

--- csv-to-json/csvToJson.js
+++ csv-to-json/csvToJson.js
@@ -3,18 +3,20 @@ const fs = require('fs');
 function csvToJson(csvFilePath) {
   const fileContent = fs.readFileSync(csvFilePath, 'utf-8');
   const newlineSeparatedFileContent = fileContent.split('\n');
-  // console.log(newlineSeparatedFileContent);
-  const keys = newlineSeparatedFileContent[0];
-  const dataArray = newlineSeparatedFileContent.slice(1);
-  console.log(keys.length);
-  dataArray.reduce((acc, hero, index) => {
-    // const heroObject = {}
-    keys.forEach((key, index) => {
-      console.log(index);
-    })
+  const keys = newlineSeparatedFileContent[0].split(',');
+  const dataArray = newlineSeparatedFileContent.slice(1).reduce((acc, hero) => {
+    acc.push(hero.split(','));
     return acc;
-  }, [])
-  return csvFilePath;
+  }, []);
+  const herosArray = dataArray.reduce((acc, hero) => {
+    const heroObject = {};
+    keys.forEach((key, keyIndex) => {
+      heroObject[key] = hero[keyIndex];
+    });
+    acc.push(heroObject);
+    return acc;
+  }, []);
+  return herosArray;
 }
 
 export {
