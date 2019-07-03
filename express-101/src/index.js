ac57911d637629715fc46254c1f41f6fa9aa742e
prateekinstillion <Prateek Madaan>
2019-07-04 00:54:21 +0530

    Solved the templatingEngine

--- express-101/src/index.js
+++ express-101/src/index.js
@@ -2,8 +2,11 @@ const express = require('express');
 
 const app = express();
 
-// 1) Create Routes
+app.get('/', (req, res) => res.send('Hey, I am server response'));
+app.get('/movie/:title', (req, res) => {
+    const { title } = req.params;
+    res.send(`My favorite movie is ${title}`);
+});
 
-// 2) Start server on port 3000
-
-module.exports = app;
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server running on port: ${port}`));
--- templating-engine/src/index.js
+++ templating-engine/src/index.js
@@ -2,12 +2,14 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 const languages = ['JavaScript', 'Haskell', 'Python'];
 
-app.get('/', (req, res) => {
-  return res.send(`Hello ${languages[0]}`); // Use res.render() to render the ejs file instead of sending text response
-});
+// Index Page
+app.get('/', (req, res) => res.render('../views/pages/index', { languages }));
 
-app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
 });
