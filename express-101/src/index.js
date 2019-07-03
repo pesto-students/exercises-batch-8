966bf6dc79c960d032b0d50cb6ad65f8474f7044
Abdul Khuddus <khuddus1@gmail.com>
2019-07-04 00:47:18 +0530

    Complete express 101

--- express-101/src/index.js
+++ express-101/src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
 const app = express();
-
-// 1) Create Routes
-
-// 2) Start server on port 3000
-
+app.get('/', (req, res) => {
+  res.send('Hey, I am server response');
+});
+app.get('/movie/:title', (req, res) => {
+  res.send(`My favorite movie is ${req.params.title}`);
+});
+app.listen(3000);
 module.exports = app;
