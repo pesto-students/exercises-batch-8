80b49a64b6bb590d7453a2f1b4a95f4d306c0e2e
Rakesh Tembhurne <tj2point0@gmail.com>
2019-07-04 01:49:43 +0530

    Complete express-forms

--- express-forms/src/index.js
+++ express-forms/src/index.js
@@ -1,17 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.redirect('/form-with-get');
-});
+app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get('/', (req, res) => res.redirect('/form-with-get'));
 
 app.get('/form-with-get', (req, res) => {
-  return res.render('form-with-get');
+  res.render('form-with-get');
 });
 
-app.get('/form-with-post', (req, res) => {
-  return res.render('form-with-post');
+app.get('/submit-form-with-get', (req, res) => {
+  res.json(req.query);
+});
+
+app.get('/form-with-post', (req, res) => res.render('form-with-post'));
+
+app.post('/submit-form-with-post', (req, res) => {
+  res.json(req.body);
 });
 
 app.listen(3000, () => {
