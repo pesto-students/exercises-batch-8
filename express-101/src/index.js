3a3afdf5108fe6dcd42c3ff25f63205fa9cfd8c5
Rakesh Tembhurne <tj2point0@gmail.com>
2019-07-04 00:46:05 +0530

    Add route to home page

--- express-101/src/index.js
+++ express-101/src/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 
 const app = express();
+const port = 3000;
 
 // 1) Create Routes
-
+app.get('/', (req, res) => {
+  res.send('Hey, I am server response');
+});
 // 2) Start server on port 3000
 
+app.listen(port, () => console.log(`App listening on port ${port}!`));
 module.exports = app;
