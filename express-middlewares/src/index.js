e9ca57884e67e5f79f8721de955ac5327bb0f701
kunnu01 <kunalbhatia2409@gmail.com>
2019-07-04 05:26:42 +0530

    Refactor express-middlewares

--- express-middlewares/src/index.js
+++ express-middlewares/src/index.js
@@ -14,20 +14,14 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/', (req, res) => {
-  res.render('index', { data: 'Hello' });
-});
+app.get('/', (req, res) => res.render('index', { data: 'Hello' }));
 
 const languages = ['JavaScript', 'Python', 'TypeScript', 'Java', 'C'];
 
-app.get('/languages', (req, res) => {
-  res.json({ languages });
-});
+app.get('/languages', (req, res) => res.json({ languages }));
 
 const packages = ['Express', 'body-parser', 'create-react-app', 'bcrypt', 'passport'];
 
-app.get('/packages', (req, res) => {
-  res.json({ packages });
-});
+app.get('/packages', (req, res) => res.json({ packages }));
 
 app.listen(PORT, () => console.log(`Server is listening on Port: ${PORT}`));
