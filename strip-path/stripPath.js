2ce099a839dff76d268a4f9e9a5a32a9c1824c71
saurabh shetty <saurabh.shetty100@gmail.com>
2019-07-03 08:12:09 +0530

    solved homeDir and stripPath

--- strip-path/stripPath.js
+++ strip-path/stripPath.js
@@ -1,6 +1,12 @@
+const path = require('path');
 
-function stripPath(...args) {
-  return args;
+function stripPath(fullPath, subPath) {
+  if (!subPath) {
+    return path.normalize(fullPath);
+  }
+  const normalizedFullPath = path.normalize(fullPath);
+  const normalizedSubPath = path.normalize(subPath);
+  return normalizedFullPath.replace(normalizedSubPath, '').slice(1);
 }
 
 export {
