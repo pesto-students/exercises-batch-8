6492f80556fad6f2650b8903774e58e041a6899f
Abdul Khuddus <khuddus1@gmail.com>
2019-07-04 05:05:05 +0530

    Complete express middlewares

--- express-middlewares/expressMiddlewares.js
+++ express-middlewares/expressMiddlewares.js
@@ -1,8 +1,42 @@
+const express = require('express');
 
-function expressMiddlewares(...args) {
-  return args;
-}
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello');
+});
 
-export {
-  expressMiddlewares,
-};
+const languages = 'languages';
+app.get(`/${languages}`, (req, res) => {
+  res.set('middleware-header', languages);
+  const today = new Date();
+  const date = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
+  const time = `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
+  const dateTime = `${date} ${time}`;
+  console.log(`middleware-header = ${languages} requested on ${dateTime}`);
+  res.json([
+    'C',
+    'Java',
+    'Python',
+    'Java Script',
+    'Closure',
+  ]);
+});
+const packages = 'packages';
+app.get(`/${packages}`, (req, res) => {
+  res.set('middleware-header', packages);
+  const today = new Date();
+  const date = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
+  const time = `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
+  const dateTime = `${date} ${time}`;
+  console.log(`middleware-header = ${packages} requested on ${dateTime}`);
+  res.json([
+    'Nodemon',
+    'Express',
+    'body-parser',
+    'lodash',
+    'babel-core',
+  ]);
+});
+
+app.listen(3000);
+module.exports = app;
