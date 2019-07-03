22089872f8782040fa8687a930f2cfdc6f4867ae
prateekinstillion <Prateek Madaan>
2019-07-04 00:37:00 +0530

    Solved the express101

--- express-101/express101.js
+++ express-101/express101.js
@@ -1,8 +1,12 @@
+const express = require('express');
+const app = express();
 
-function express101(...args) {
-  return args;
-}
 
-export {
-  express101,
-};
+app.get('/', (req, res) => res.send(`Hey, I am server response`));
+app.get('/movie/:title', (req, res) => {
+  const { title } = req.params;
+  res.send(`My favorite movie is ${title}`);
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server running on port: ${port}`));
