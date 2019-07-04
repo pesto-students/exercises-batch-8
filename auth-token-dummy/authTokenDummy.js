57243d11ae8637e7b6d2b530ff670456ca9ef338
saurabh shetty <saurabh.shetty100@gmail.com>
2019-07-04 06:49:41 +0530

    wip auth

--- auth-token-dummy/authTokenDummy.js
+++ auth-token-dummy/authTokenDummy.js
@@ -1,8 +1,33 @@
 
-function authTokenDummy(...args) {
-  return args;
-}
+//WIP
+const express = require('express');
+const bodyParser = require('body-parser')
+const app = express();
+const Cryptr = require('cryptr');
 
-export {
-  authTokenDummy,
-};
+const cryptr = new Cryptr('Shhhh!!');
+
+const someDB = [];
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  if (!req.headers.authorization && req.originalUrl !== '/signup') {
+    res.status(403).send('Forbidden');
+  } else {
+  }
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.send(['list', 'of', 'movies', '!!']);
+});
+app.post('/signup', (req, res) => {
+  const { username, password } = req.body;
+  const encryptedPassword = cryptr.encrypt(password);
+  someDB.push({ username, encryptedPassword });
+  res.send(encryptedPassword);
+});
+app.listen(3000, () => {
+  console.log('Listening to 3000');
+});
