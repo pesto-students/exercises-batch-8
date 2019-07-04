376d72a20185dc7a7614e0cbe47c551bb4386957
Abdul Khuddus <khuddus1@gmail.com>
2019-07-04 05:32:29 +0530

    Fix: used actual express middle ware in program

--- express-middlewares/expressMiddlewares.js
+++ express-middlewares/expressMiddlewares.js
@@ -1,18 +1,25 @@
 const express = require('express');
 
 const app = express();
+
+app.use((req, res, next) => {
+  const trimmedUrl = req.originalUrl.substr(1);
+  res.set('middleware-header', trimmedUrl);
+  const today = new Date();
+  const date = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
+  const time = `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
+  const dateTime = `${date} ${time}`;
+  console.log(`middleware-header = ${trimmedUrl} requested on ${dateTime}`);
+
+  next();
+});
+
 app.get('/', (req, res) => {
   res.send('Hello');
 });
 
 const languages = 'languages';
 app.get(`/${languages}`, (req, res) => {
-  res.set('middleware-header', languages);
-  const today = new Date();
-  const date = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
-  const time = `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
-  const dateTime = `${date} ${time}`;
-  console.log(`middleware-header = ${languages} requested on ${dateTime}`);
   res.json([
     'C',
     'Java',
@@ -23,12 +30,6 @@ app.get(`/${languages}`, (req, res) => {
 });
 const packages = 'packages';
 app.get(`/${packages}`, (req, res) => {
-  res.set('middleware-header', packages);
-  const today = new Date();
-  const date = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
-  const time = `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
-  const dateTime = `${date} ${time}`;
-  console.log(`middleware-header = ${packages} requested on ${dateTime}`);
   res.json([
     'Nodemon',
     'Express',
