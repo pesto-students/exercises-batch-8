2554dce7769e960c4b2790f73a652dc6e2e9055d
Rakesh Tembhurne <tj2point0@gmail.com>
2019-07-06 01:02:37 +0530

    Complete titleizeo

--- titleize/titleize.js
+++ titleize/titleize.js
@@ -1,6 +1,25 @@
 
-function titleize(...args) {
-  return args;
+function titleize(sentence) {
+  if (!sentence) {
+    return sentence;
+  }
+
+  const characters = sentence.split('');
+  const titalized = characters.reduce((accumulator, character) => {
+    if (accumulator.lastChar === ''
+        || accumulator.lastChar === ' '
+        || accumulator.lastChar === '-'
+    ) {
+      accumulator.title += character.toUpperCase();
+    } else {
+      accumulator.title += character.toLowerCase();
+    }
+    accumulator.lastChar = character;
+
+    return accumulator;
+  }, { lastChar: '', title: '' });
+
+  return titalized.title;
 }
 
 export {
