48165d273c395bca4b570abfa43d67bbcbd8204c
prateekinstillion <Prateek Madaan>
2019-07-06 01:32:12 +0530

    Solved uniqueRandom, titleize in progress

--- titleize/titleize.js
+++ titleize/titleize.js
@@ -1,6 +1,12 @@
+const performTitleCase = sentence => {
+  return sentence.charAt(0).toUpperCase() + sentence.slice(1).toLowerCase();
+}
 
-function titleize(...args) {
-  return args;
+function titleize(sentence) {
+  if (!sentence.split) return sentence;
+  let result = [];
+  sentence.split(" ").forEach(word => result.push(performTitleCase(word)));
+  return sentence.join('');
 }
 
 export {
--- unique-random/uniqueRandom.js
+++ unique-random/uniqueRandom.js
@@ -1,8 +1,15 @@
-
-function uniqueRandom(...args) {
-  return args;
+function uniqueRandom(start, end) {
+  let previousUnique = 0;
+  return function generateUnique() {
+    let currentUnique = Math.floor(Math.random() * (end - start)) + start;
+    if (currentUnique === previousUnique) {
+      return generateUnique();
+    } else {
+      previousUnique = currentUnique;
+      return currentUnique;
+    }
+  }; //The maximum is exclusive and the minimum is inclusive
 }
-
 export {
   uniqueRandom,
 };
