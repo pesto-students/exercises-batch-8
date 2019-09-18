c50a4be0c9c3cccc882a86dc5853fe5ed9f89286
Jaspreet Singh <erjaspreet57@gmail.com>
2019-09-18 15:03:41 +0530

    Enhance solution every-nth

--- every-nth-test/everyNthTest.js
+++ every-nth-test/everyNthTest.js
@@ -1,6 +1,15 @@
 
 function everyNth(string, n) {
-  return string.split('').filter((a, i) => i % n === 0).join('');
+  // below solution looks good but not better runtime;
+  // return string.split('').filter((a, i) => i % n === 0).join('');
+
+  // Below will run only length/n times
+  const chars = string.split('');
+  let result = '';
+  for (let i = 0; i < chars.length; i += n) {
+    result += chars[i];
+  }
+  return result;
 }
 
 export {
